565fbdad6f6af81ecebcab1f0093d7ab26c50e5c
Matthew Guo <mg2239@cornell.edu>
2019-10-29 17:20:21 -0400

    Added Google authentication + adding user
    ### Summary
    Added Google authentication and endpoint to add user using user's email, phone number, password, and name.

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,14 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
 
 const app = express();
 const main = express();
 
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  databaseURL: 'https://carriage.firebaseio.com',
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://carriage-471ba.firebaseio.com',
 });
 
 main.use('/api', app);
@@ -16,4 +17,24 @@ exports.webAPI = functions.https.onRequest(main);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/addUser', (req, res) => {
+  const {
+    userEmail, userPhone, userPass, userName,
+  } = req.body;
+  admin.auth().createUser({
+    email: userEmail,
+    emailVerified: false,
+    phoneNumber: userPhone,
+    password: userPass,
+    displayName: userName,
+    disabled: false,
+  })
+    .then((userRecord) => {
+      res.send({ id: userRecord.uid });
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 });
