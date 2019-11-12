11a0a872f6b71dee93bb17e95411c440720841b1
Simran Puri <sp2262@cornell.edu>
2019-11-12 17:59:04 -0500

    created dummy table

--- config.js
+++ config.js
@@ -1,11 +1,7 @@
-const AWS = require('aws-sdk');
 require('dotenv').config();
 
-const config = new AWS.Config({
-  // hardcode the config for now, DO NOT PUSH
+module.exports = {
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: 'us-east-2',
-});
-
-module.exports = config;
+};
