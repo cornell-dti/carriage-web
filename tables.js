1f8414f6e9220e55c33d73021103f0fde4f9cdb2
Matthew Guo <mg2239@cornell.edu>
2019-11-18 12:21:25 -0500

    added location and driver params, started app interface

--- tables.js
+++ tables.js
@@ -3,28 +3,64 @@ const config = require('./config');
 
 AWS.config.update(config);
 
-var dynamodb = new AWS.DynamoDB();
+const dynamodb = new AWS.DynamoDB();
 
-var params = {
-    TableName : "Movies",
-    KeySchema: [       
-        { AttributeName: "year", KeyType: "HASH"},  //Partition key
-        { AttributeName: "title", KeyType: "RANGE" }  //Sort key
-    ],
-    AttributeDefinitions: [       
-        { AttributeName: "year", AttributeType: "N" },
-        { AttributeName: "title", AttributeType: "S" }
-    ],
-    ProvisionedThroughput: {       
-        ReadCapacityUnits: 10, 
-        WriteCapacityUnits: 10
-    }
+const locationParams = {
+  TableName: 'Locations',
+  KeySchema: [
+    { AttributeName: 'id', KeyType: 'HASH' },
+  ],
+  AttributeDefinitions: [
+    { AttributeName: 'id', AttributeType: 'N' },
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 10,
+    WriteCapacityUnits: 10,
+  },
 };
 
-dynamodb.createTable(params, function(err, data) {
-    if (err) {
-        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
-    } else {
-        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
-    }
+const driverParams = {
+  TableName: 'Drivers',
+  KeySchema: [
+    { AttributeName: 'id', KeyType: 'HASH' },
+  ],
+  AttributeDefinitions: [
+    { AttributeName: 'id', AttributeType: 'N' },
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 10,
+    WriteCapacityUnits: 10,
+  },
+};
+
+// const movieParams = {
+//   TableNames: 'Movies',
+//   KeySchema: [
+//     { AttributeName: 'year', KeyType: 'HASH' }, // Partition key
+//     { AttributeName: 'title', KeyType: 'RANGE' }, // Sort key
+//   ],
+//   AttributeDefinitions: [
+//     { AttributeName: 'year', AttributeType: 'N' },
+//     { AttributeName: 'title', AttributeType: 'S' },
+//   ],
+//   ProvisionedThroughput: {
+//     ReadCapacityUnits: 10,
+//     WriteCapacityUnits: 10,
+//   },
+// };
+
+dynamodb.createTable(driverParams, (err, data) => {
+  if (err) {
+    console.error('Unable to create table. Error JSON:', JSON.stringify(err, null, 2));
+  } else {
+    console.log('Created table. Table description JSON:', JSON.stringify(data, null, 2));
+  }
+});
+
+dynamodb.createTable(locationParams, (err, data) => {
+  if (err) {
+    console.error('Unable to create table. Error JSON:', JSON.stringify(err, null, 2));
+  } else {
+    console.log('Created table. Table description JSON:', JSON.stringify(data, null, 2));
+  }
 });
