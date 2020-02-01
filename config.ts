a29985489c95dfc2d113667f9595c083e32c61ad
Matthew Guo <mg2239@cornell.edu>
2020-02-01 16:27:18 -0500

    setup eslint to work with typescript

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: 'us-east-2',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: 'us-east-2',
 };
