6fe4df75b41bb111874cedf936cd17ab6f04ba08
Ishika Jain <ij36@cornell.edu>
2020-02-05 16:31:10 -0500

    Just landing page, no google Oauth

---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => (
+  <header id="showcase">
+    <h1>Welcome to Carriage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias minus
+      recusandae enim placeat, maiores atque!
+    </p>
+    <a href="#" className="button">
+      {" "}
+      Read More
+    </a>
+  </header>
+);
+
+export default Header;
---
+++ frontend/src/components/sectionA.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./sectionA.css";
+
+const SectionA = () => (
+  <section id="section-a">
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis unde
+      aliquid nisi excepturi, et quam. Maiores ut cupiditate officia
+      consectetur! Ab ullam dolores quia illum quisquam eaque voluptatum, minus
+      earum laboriosam iste neque eveniet vel voluptates hic a veniam velit
+      accusamus voluptate similique expedita reprehenderit laudantium laborum
+      possimus inventore. Perferendis veniam laboriosam quam corporis facere,
+      consequuntur in ullam numquam natus, impedit minima ducimus, repellendus
+      blanditiis adipisci! Sit laboriosam ea id enim quaerat doloribus
+      distinctio asperiores iure maiores? Ducimus laborum incidunt voluptatem
+      veniam aut recusandae culpa commodi voluptatibus est quam, delectus
+      deserunt voluptates d ignissimos necessitatibus modi placeat doloribus
+      nemo ratione vel.
+    </p>
+  </section>
+);
+export default SectionA;
