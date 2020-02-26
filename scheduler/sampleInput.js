31d30958ee95332b682e105b827b14ba1e7cfeb0
Sam <sam@developersam.com>
2020-02-26 11:44:52 -0500

    Linter fixes for scheduler code

--- scheduler/sampleInput.js
+++ scheduler/sampleInput.js
@@ -1,169 +1,169 @@
 // Example input
-var rideRequests = [
+const rideRequests = [
   {
-    ID: "001",
+    ID: '001',
     startLocation: 0,
     endLocation: 3,
-    startTime: "2019-12-13T07:30",
-    endTime: "2019-12-13T07:50",
+    startTime: '2019-12-13T07:30',
+    endTime: '2019-12-13T07:50',
     isScheduled: false,
-    riderID: "0",
+    riderID: '0',
     dateRequested: 34,
   },
   {
-    ID: "002",
+    ID: '002',
     startLocation: 1,
     endLocation: 2,
-    startTime: "2019-12-13T08:20",
-    endTime: "2019-12-13T08:34",
+    startTime: '2019-12-13T08:20',
+    endTime: '2019-12-13T08:34',
     isScheduled: false,
-    riderID: "1",
+    riderID: '1',
     dateRequested: 34,
   },
   {
-      ID: "003",
-      startLocation: 1,
-      endLocation: 2,
-      startTime: "2019-12-13T08:10",
-      endTime: "2019-12-13T08:25",
-      isScheduled: false,
-      riderID: "1",
-      dateRequested: 34,
-    },
-    {
-      ID: "004",
-      startLocation: 1,
-      endLocation: 2,
-      startTime: "2019-12-13T08:30",
-      endTime: "2019-12-13T08:35",
-      isScheduled: false,
-      riderID: "1",
-      dateRequested: 34,
-    }
-]
-
-var rideRequestsImpossible = [
+    ID: '003',
+    startLocation: 1,
+    endLocation: 2,
+    startTime: '2019-12-13T08:10',
+    endTime: '2019-12-13T08:25',
+    isScheduled: false,
+    riderID: '1',
+    dateRequested: 34,
+  },
   {
-    ID: "001",
+    ID: '004',
+    startLocation: 1,
+    endLocation: 2,
+    startTime: '2019-12-13T08:30',
+    endTime: '2019-12-13T08:35',
+    isScheduled: false,
+    riderID: '1',
+    dateRequested: 34,
+  },
+];
+
+const rideRequestsImpossible = [
+  {
+    ID: '001',
     startLocation: 0,
     endLocation: 3,
-    startTime: "2019-12-13T07:30",
-    endTime: "2019-12-13T07:50",
+    startTime: '2019-12-13T07:30',
+    endTime: '2019-12-13T07:50',
     isScheduled: false,
-    riderID: "0",
+    riderID: '0',
     dateRequested: 34,
   },
   {
-    ID: "002",
+    ID: '002',
     startLocation: 1,
     endLocation: 2,
-    startTime: "2019-12-13T08:20",
-    endTime: "2019-12-13T08:34",
+    startTime: '2019-12-13T08:20',
+    endTime: '2019-12-13T08:34',
     isScheduled: false,
-    riderID: "1",
+    riderID: '1',
     dateRequested: 34,
   },
   {
-      ID: "003",
-      startLocation: 1,
-      endLocation: 2,
-      startTime: "2019-12-13T08:10",
-      endTime: "2019-12-13T08:25",
-      isScheduled: false,
-      riderID: "1",
-      dateRequested: 34,
-    },
-    {
-      ID: "004",
-      startLocation: 1,
-      endLocation: 2,
-      startTime: "2019-12-13T08:22",
-      endTime: "2019-12-13T08:35",
-      isScheduled: false,
-      riderID: "1",
-      dateRequested: 34,
-    }
-]
+    ID: '003',
+    startLocation: 1,
+    endLocation: 2,
+    startTime: '2019-12-13T08:10',
+    endTime: '2019-12-13T08:25',
+    isScheduled: false,
+    riderID: '1',
+    dateRequested: 34,
+  },
+  {
+    ID: '004',
+    startLocation: 1,
+    endLocation: 2,
+    startTime: '2019-12-13T08:22',
+    endTime: '2019-12-13T08:35',
+    isScheduled: false,
+    riderID: '1',
+    dateRequested: 34,
+  },
+];
 
-var drivers = [
-    {
-        ID: "0",
-        name: "Brandon Brandoni",
-        startTime: "2019-12-13T08:00",
-        endTime: "2019-12-13T18:00",
-        breaks: {
-            Mon: {
-              breakStart: '12:00',
-              breakEnd: '12:30',
-            },
-            Tues: {
-              breakStart: '12:00',
-              breakEnd: '12:30',
-            },
-            Wed: {
-              breakStart: '12:00',
-              breakEnd: '13:30',
-            },
-            Thurs: {
-              breakStart: '12:00',
-              breakEnd: '12:30',
-            },
-            Fri: {
-              breakStart: '13:00',
-              breakEnd: '13:30',
-            },
-        },
-        vehicle: 1,
-        workPhoneNumer: 1231231234,
-        email: "email@email.com",
+const drivers = [
+  {
+    ID: '0',
+    name: 'Brandon Brandoni',
+    startTime: '2019-12-13T08:00',
+    endTime: '2019-12-13T18:00',
+    breaks: {
+      Mon: {
+        breakStart: '12:00',
+        breakEnd: '12:30',
+      },
+      Tues: {
+        breakStart: '12:00',
+        breakEnd: '12:30',
+      },
+      Wed: {
+        breakStart: '12:00',
+        breakEnd: '13:30',
+      },
+      Thurs: {
+        breakStart: '12:00',
+        breakEnd: '12:30',
+      },
+      Fri: {
+        breakStart: '13:00',
+        breakEnd: '13:30',
+      },
     },
-    {
-        ID: "1",
-        name: "Bobby Bobbers",
-        startTime: "2019-12-13T07:00",
-        endTime: "2019-12-13T17:00",
-        breaks: {
-            Mon: {
-              breakStart: '10:00',
-              breakEnd: '10:30',
-            },
-            Tues: {
-              breakStart: '10:00',
-              breakEnd: '10:30',
-            },
-            Wed: {
-              breakStart: '10:00',
-              breakEnd: '10:30',
-            },
-            Thurs: {
-              breakStart: '10:00',
-              breakEnd: '10:30',
-            },
-            Fri: {
-              breakStart: '11:00',
-              breakEnd: '11:30',
-            },
-        },
-        vehicle: 2,
-        workPhoneNumer: 1231231234,
-        email: "email@email.com",
+    vehicle: 1,
+    workPhoneNumer: 1231231234,
+    email: 'email@email.com',
+  },
+  {
+    ID: '1',
+    name: 'Bobby Bobbers',
+    startTime: '2019-12-13T07:00',
+    endTime: '2019-12-13T17:00',
+    breaks: {
+      Mon: {
+        breakStart: '10:00',
+        breakEnd: '10:30',
+      },
+      Tues: {
+        breakStart: '10:00',
+        breakEnd: '10:30',
+      },
+      Wed: {
+        breakStart: '10:00',
+        breakEnd: '10:30',
+      },
+      Thurs: {
+        breakStart: '10:00',
+        breakEnd: '10:30',
+      },
+      Fri: {
+        breakStart: '11:00',
+        breakEnd: '11:30',
+      },
     },
-]
+    vehicle: 2,
+    workPhoneNumer: 1231231234,
+    email: 'email@email.com',
+  },
+];
 
-var vehicles = [
-    {
-      ID: "1",
-      wheelchairAccessible: true,
-    },
-    {
-      ID: "2",
-      wheelchairAccessible: false,
-  }
-]
+const vehicles = [
+  {
+    ID: '1',
+    wheelchairAccessible: true,
+  },
+  {
+    ID: '2',
+    wheelchairAccessible: false,
+  },
+];
 
-module.exports = { 
-  rideRequests: rideRequests,
-  rideRequestsImpossible: rideRequestsImpossible,
-  drivers: drivers,
-  vehicles: vehicles 
+module.exports = {
+  rideRequests,
+  rideRequestsImpossible,
+  drivers,
+  vehicles,
 };
--- scheduler/scheduler.js
+++ scheduler/scheduler.js
@@ -1,81 +1,98 @@
 // Incredibly basic test of scheduling
-var sample = require('./sampleInput');
+const sample = require('./sampleInput');
 // Test on possible request set
-schedule = makeSchedule(sample.rideRequests, sample.drivers);
+let schedule = makeSchedule(sample.rideRequests, sample.drivers);
 console.log(schedule); // Should output valid schedule
 // Test on impossible request set
 schedule = makeSchedule(sample.rideRequestsImpossible, sample.drivers);
 console.log(schedule); // Should output NULL
 
-// Constructs and returns schedule if possible given requests and drivers. Returns NULL if impossible.
-// * NOTE: Currently schedules all drivers to rides with no breaks and work all day, assuming instant transportation to start location, 
-// *   and does not allow shared rides
-// * PRECONDITION: rideRequests is list of valid ride JSONs (for now must have startTime, endTime fields), 
-// *   driver is valid driver JSON (for now must have id field)
+/**
+ * NOTE:
+ * Currently schedules all drivers to rides with no breaks and work all day,
+ * assuming instant transportation to start location, and does not allow shared rides.
+ *
+ * @param {*} rideRequests list of valid ride JSONs (for now must have startTime, endTime fields),
+ * @param {*} drivers valid driver JSON (for now must have id field)
+ * @returns {*} schedule if possible given requests and drivers or NULL if impossible.
+ */
 function makeSchedule(rideRequests, drivers) {
-  stack = []; // stack of decision tree nodes, where each node is a growing schedule (schedule = list of scheduled ride JSONs)
-  rootNode = [];
+  // stack of decision tree nodes
+  // where each node is a growing schedule(schedule = list of scheduled ride JSONs)
+  const stack = [];
+  const rootNode = [];
   stack.push(rootNode);
-  while(stack.length > 0) {
-    node = stack.pop();
-    //console.log(node);
-    if(node.length == rideRequests.length){
+  while (stack.length > 0) {
+    const node = stack.pop();
+    // console.log(node);
+    if (node.length === rideRequests.length) {
       return node;
     }
     // Schedule next ride (directly dependent on node length)
-    request = rideRequests[node.length]; 
-    for(d in drivers) {
-      driver = drivers[d];
+    const request = rideRequests[node.length];
+    Object.keys(drivers).forEach((d) => {
+      const driver = drivers[d];
       // Given no conflict assigning request to driver, push corresponding child node to stack
-      if(!conflict(request, node, driver)){
+      if (!conflict(request, node, driver)) {
         // Copy current node to begin constructing child
-        childNode = copyNode(node);
+        const childNode = copyNode(node);
         // Schedule ride with driver and add to childNode
         childNode.push(assignRide(request, driver.ID));
         // Push childNode to stack
-        // TODO: optimize order of pushing nodes to stack to make greedy choices (ie push probabilistically better options later)
+        // TODO: optimize order of pushing nodes to stack to make greedy choices
+        // (ie push probabilistically better options later)
         stack.push(childNode);
       }
-    }
+    });
   }
   // Fail state reached
   return null;
 }
 
-// Returns true if there exists a conflict when assigning request (ride) to driver given the schedule in node (list of scheduled ride JSONs)
-// * PRECONDITION: request is a JSON object with startTime and endTime valid string date fields, node is a list of JSON ride objects where
-// *  each ride has startTime and endTime valid string date fields and a driverID field, and driver is a valid driver JSON object with startTime,
-// *  endTime valid string date fields and a string ID field
-// TODO: currently checks for conflicts with every ride, much more efficient models possible (track most recent ride for each driver)
-// NOTE: assumes implicit guarantee that for all JSONs: startTime > endTime
+/**
+ * TODO: currently checks for conflicts with every ride, much more efficient models possible
+ * (track most recent ride for each driver)
+ * NOTE: assumes implicit guarantee that for all JSONs: startTime > endTime
+ *
+ * @param {{ startTime: string, endTime: string }} request
+ * @param {*} node a list of JSON ride objects,
+ * where each ride has startTime and endTime valid string date fields and a driverID field
+ * @param {{ startTime: string, endTime: string; ID: string }} driver a valid driver JSON object.
+ * @returns {boolean} if there exists a conflict when assigning request (ride) to driver
+ * given the schedule in node (list of scheduled ride JSONs)
+ */
 function conflict(request, node, driver) {
-  for(r in node) { // iterate through all indices in node (list of scheduled rides defining this node's state)
-    ride = node[r];
-    if(ride.driverID === driver.ID && 
-        (new Date(request.startTime) < new Date(driver.startTime) || new Date(request.endTime) > new Date(driver.endTime)
-        || (new Date(request.startTime) > new Date(ride.startTime) && new Date(request.startTime) < new Date(ride.endTime)) 
-        || (new Date(request.endTime) > new Date(ride.startTime) && new Date(request.endTime) < new Date(ride.endTime)))){
-      return true;
-    }
-  }
-  return false;
+  return Object.keys(node).some((key) => {
+    const ride = node[key];
+    return ride.driverID === driver.ID
+      && (new Date(request.startTime) < new Date(driver.startTime)
+        || new Date(request.endTime) > new Date(driver.endTime)
+      || (new Date(request.startTime) > new Date(ride.startTime)
+        && new Date(request.startTime) < new Date(ride.endTime))
+        || (new Date(request.endTime) > new Date(ride.startTime)
+          && new Date(request.endTime) < new Date(ride.endTime)));
+  });
 }
 
-// Creates copy of request JSON object with driverID field set as input driver
-// * PRECONDITION: request is a JSON object and driver is a JSON object with ID field
+/**
+ * Creates copy of request JSON object with driverID field set as input driver
+ *
+ * @param {*} request a JSON object
+ * @param {*} driverID
+ */
 function assignRide(request, driverID) {
-  scheduled = JSON.parse(JSON.stringify(request));
+  const scheduled = JSON.parse(JSON.stringify(request));
   scheduled.driverID = driverID;
   return scheduled;
 }
 
-// Construct and returns a deep copy of the input node
-// * PRECONDITION: node is an array list of JSON objects that all must be deep-copied
-// * TODO: Inefficient, over-copying --> improve
+/**
+ * TODO: Inefficient, over-copying --> improve
+ *
+ * @template {T}
+ * @param {T[]} node an array list of JSON objects that all must be deep-copied.
+ * @returns {T[]} a deep copy of the input node.
+ */
 function copyNode(node) {
-  copy = [];
-  for(r in node){
-    copy.push(JSON.parse(JSON.stringify(node[r])));
-  }
-  return copy;
+  return Object.keys(node).map((key) => JSON.parse(JSON.stringify(node[key])));
 }
