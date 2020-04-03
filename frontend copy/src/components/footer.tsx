a330497bbba560caab8dc2e586cd4ec4e89107f2
Lucy <ljw222@cornell.edu>
2020-04-03 04:29:27 -0400

    fixed package.json

---
+++ frontend copy/src/components/footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    Some information about copyright can go here
+  </footer>
+);
+
+export default Footer;
