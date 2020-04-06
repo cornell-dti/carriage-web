e6a36479dfeeae19b335700ca1340b91c7fdc5d7
Lucy <ljw222@cornell.edu>
2020-04-06 10:47:07 -0400

    update default page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LandingPage from './components/landing';
-import Table from "./components/table";
+// import Table from "./components/table";
 
 import './App.css';
 
 const App = () => (
   <Router>
     <Switch>
-      <Route exact path="/" component={Table} />
+      <Route exact path="/" component={LandingPage} />
     </Switch>
   </Router>
 );
