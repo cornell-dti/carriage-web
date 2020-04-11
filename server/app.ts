2635723477ededb7506b119714ae221d9fb7cb78
Matthew Guo <mg2239@cornell.edu>
2020-04-11 16:40:40 -0400

    changed port to 3001 to not conflict w/ react

--- server/app.ts
+++ server/app.ts
@@ -10,7 +10,7 @@ import vehicle from './router/vehicle';
 import location from './router/location';
 import auth from './router/auth';
 
-const port = 3000;
+const port = 3001;
 
 const app = express();
 app.use(bodyparser.json());
