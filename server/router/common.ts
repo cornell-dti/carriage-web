6268c687eed4138c24821daaf2cff9a47f6e8b74
chansen424 <chansen424@yahoo.com>
2020-04-27 22:56:40 -0400

    Shorten line length for linter

--- server/router/common.ts
+++ server/router/common.ts
@@ -2,13 +2,13 @@ import { Request, Response } from 'express';
 import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 
 export const deleteByID =
-  (req: Request, res: Response, docClient: DocumentClient, table: string) => {
+  (req: Request, res: Response, client: DocumentClient, table: string) => {
     const { id } = req.params;
     const params = {
       TableName: table,
       Key: { id },
     };
-    docClient.delete(params, (err, _) => {
+    client.delete(params, (err, _) => {
       if (err) {
         res.send({ err });
       } else {
