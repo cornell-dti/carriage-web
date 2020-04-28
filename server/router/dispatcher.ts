40e9e416948c0c8fb339855bd2c0dfeafb244790
chansen424 <chansen424@yahoo.com>
2020-04-27 21:48:51 -0400

    Access Levels

--- server/router/dispatcher.ts
+++ server/router/dispatcher.ts
@@ -8,12 +8,20 @@ const router = express.Router();
 AWS.config.update(config);
 const docClient = new AWS.DynamoDB.DocumentClient();
 
+// Using a String enum allows the enum to be parsed by JSON.parse()
+enum AccessLevel {
+  Admin = "Admin", // only an admin should be able to add another dispatcher
+  SDS = "SDS",
+  Dispatcher = "Dispatcher"
+}
+
 type Dispatcher = {
   id: string,
   firstName: string,
   lastName: string,
   phoneNumber: string,
   email: string,
+  accessLevel: AccessLevel
 };
 
 export default router;
