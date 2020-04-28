6e3ad805f265d162771540f0df886fd11b5790cb
chansen424 <chansen424@yahoo.com>
2020-04-27 22:30:55 -0400

    Factor out deletion logic

---
+++ server/router/common.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+
+export const deleteFromTableByID = (req: Request, res: Response, docClient: DocumentClient, table: string) => {
+  const { id } = req.params;
+  const params = {
+    TableName: table,
+    Key: { id },
+  };
+  docClient.delete(params, (err, _) => {
+    if (err) {
+      res.send({ err });
+    } else {
+      res.send({ id });
+    }
+  });
+}
