8a25e986b48e44f8d985bfbb61326ded0a203c7c
Matthew Guo <mg2239@cornell.edu>
2020-04-30 21:33:28 -0400

    starting to rewrite rider endpoints

--- server/router/location.ts
+++ server/router/location.ts
@@ -1,52 +1,57 @@
 import express from 'express';
 import uuid from 'uuid/v1';
-import AWS from 'aws-sdk';
-import config from '../config';
+import dynamoose from 'dynamoose';
 
 const router = express.Router();
 
-AWS.config.update(config);
-const docClient = new AWS.DynamoDB.DocumentClient();
-
-type Location = {
+type LocationType = {
   id: string,
-  location: string,
+  name: string,
+  address: string,
 };
+
+const schema = new dynamoose.Schema({
+  id: String,
+  name: String,
+  address: String,
+});
+
+export const Locations = dynamoose.model('Locations', schema, { create: false });
 
 // Get a location by ID in Locations table
 router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  const params = {
-    TableName: 'Locations',
-    Key: { id },
-  };
-  docClient.get(params, (err, data) => {
-    if (err) {
-      res.send({ err });
-    } else {
-      res.send(data);
-    }
-  });
+  // const { id } = req.params;
+  // const params = {
+  //   TableName: 'Locations',
+  //   Key: { id },
+  // };
+  // docClient.get(params, (err, data) => {
+  //   if (err) {
+  //     res.send({ err });
+  //   } else {
+  //     res.send(data);
+  //   }
+  // });
 });
 
 // Put a location in Locations table
 router.post('/', (req, res) => {
-  const postBody = req.body;
-  const location: Location = {
-    id: uuid(),
-    location: postBody.location,
-  };
-  const params = {
-    TableName: 'Locations',
-    Item: location,
-  };
-  docClient.put(params, (err, data) => {
-    if (err) {
-      res.send({ err });
-    } else {
-      res.send(location);
-    }
-  });
+  // const postBody = req.body;
+  // const location: Location = {
+  //   id: uuid(),
+  //   location: postBody.location,
+  // };
+  // const params = {
+  //   TableName: 'Locations',
+  //   Item: location,
+  // };
+  // docClient.put(params, (err, data) => {
+  //   if (err) {
+  //     res.send({ err });
+  //   } else {
+  //     res.send(location);
+  //   }
+  // });
 });
 
 // TODO: Update an existing location
--- server/router/rider.ts
+++ server/router/rider.ts
@@ -1,120 +1,134 @@
 import express from 'express';
 import uuid from 'uuid/v1';
-import AWS from 'aws-sdk';
-import * as t from 'io-ts';
+import dynamoose from 'dynamoose';
 import { isRight } from 'fp-ts/lib/Either';
-import config from '../config';
-import { deleteByID } from './common';
+
+import { Locations } from './location';
 
 const router = express.Router();
-
-AWS.config.update(config);
-const docClient = new AWS.DynamoDB.DocumentClient();
 
 type Key = {
   id: string
 };
 
-const Body = t.type({
-  firstName: t.string,
-  lastName: t.string,
-  phoneNumber: t.string,
-  email: t.string,
-  accessibilityNeeds: t.type({
-    needsWheelchair: t.boolean,
-    hasCrutches: t.boolean,
-    needsAssistant: t.boolean,
-  }),
-  description: t.string,
-  joinDate: t.string,
-  pronouns: t.string,
-  address: t.string,
+type RiderType = {
+  id: string,
+  firstName: string,
+  lastName: string,
+  phoneNumber: string,
+  email: string,
+  accessibilityNeeds: {
+    needsWheelchair: boolean,
+    hasCrutches: boolean,
+    needsAssistant: boolean,
+  },
+  description: string,
+  joinDate: string,
+  pronouns: string,
+  address: string,
+  favoriteLocations: string[]
+};
+
+const schema = new dynamoose.Schema({
+  id: String,
+  firstName: String,
+  lastName: String,
+  phoneNumber: String,
+  email: String,
+  accessibilityNeeds: {
+    type: Object,
+    schema: {
+      needsWheelchair: Boolean,
+      hasCrutches: Boolean,
+      needsAssistant: Boolean,
+    },
+  },
+  description: String,
+  joinDate: String,
+  pronouns: String,
+  address: String,
+  favoriteLocations: {
+    type: Array,
+    schema: [String],
+  },
+}, {
+  saveUnknown: true,
 });
+
+export const Riders = dynamoose.model('Riders', schema, { create: false });
 
 // Get a rider by ID in Riders table
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  const params = {
-    TableName: 'Riders',
-    Key: { id },
-  };
-  docClient.get(params, (err, data) => {
+  Riders.get(id, (err, data) => {
     if (err) {
-      res.send({ err });
+      res.send(err);
     } else {
-      res.send(data.Item);
+      res.send(data);
     }
   });
 });
 
 // Get all riders
 router.get('/', (req, res) => {
-  const params = {
-    TableName: 'Riders',
-  };
-  docClient.scan(params, (err, data) => {
+  Riders.scan('', (err, data) => {
     if (err) {
-      res.send({ err });
+      res.send(err);
     } else {
-      res.send({ data: data.Items });
+      res.send(data);
     }
   });
 });
 
 // Get all upcoming rides for a rider
 router.get('/:id/rides', (req, res) => {
-  const { id } = req.params;
-  const params = {
-    TableName: 'Riders',
-    Key: { id },
-  };
-  docClient.get(params, (err, data) => {
-    if (err) {
-      res.send({ err });
-    } else if (!data.Item) {
-      res.send({ err: { message: 'id not found' } });
-    } else {
-      const { requestedRides } = data.Item;
-      if (!requestedRides.length) {
-        res.send({ data: [] });
-      } else {
-        const rideParams = {
-          RequestItems: {
-            ActiveRides: {
-              Keys: requestedRides,
-            },
-          },
-        };
-        docClient.batchGet(rideParams, (rideErr, rideData) => {
-          if (rideErr) {
-            res.send({ err: rideErr });
-          } else {
-            res.send({ data: rideData.Responses!.ActiveRides });
-          }
-        });
-      }
-    }
-  });
+  // const { id } = req.params;
+  // const params = {
+  //   TableName: 'Riders',
+  //   Key: { id },
+  // };
+  // docClient.get(params, (err, data) => {
+  //   if (err) {
+  //     res.send({ err });
+  //   } else if (!data.Item) {
+  //     res.send({ err: { message: 'id not found' } });
+  //   } else {
+  //     const { requestedRides } = data.Item;
+  //     if (!requestedRides.length) {
+  //       res.send({ data: [] });
+  //     } else {
+  //       const rideParams = {
+  //         RequestItems: {
+  //           ActiveRides: {
+  //             Keys: requestedRides,
+  //           },
+  //         },
+  //       };
+  //       docClient.batchGet(rideParams, (rideErr, rideData) => {
+  //         if (rideErr) {
+  //           res.send({ err: rideErr });
+  //         } else {
+  //           res.send({ data: rideData.Responses!.ActiveRides });
+  //         }
+  //       });
+  //     }
+  //   }
+  // });
 });
 
 // Get profile information for a rider
 router.get('/:id/profile', (req, res) => {
   const { id } = req.params;
-  const params = {
-    TableName: 'Riders',
-    Key: { id },
-  };
-  docClient.get(params, (err, data) => {
+  Riders.get(id, (err, data: any) => {
+    const rider: RiderType = data;
     if (err) {
       res.send({ err });
-    } else if (!data.Item) {
-      res.send({ err: { message: 'id not found' } });
     } else {
       const {
-        email, firstName, lastName, phoneNumber, pronouns, picture, joinDate,
-      } = data.Item;
+        email, firstName, lastName, phoneNumber, pronouns, joinDate,
+      } = rider;
       res.send({
-        email, firstName, lastName, phoneNumber, pronouns, picture, joinDate,
+        email, firstName, lastName, phoneNumber, pronouns, joinDate,
       });
     }
   });
@@ -123,17 +137,12 @@ router.get('/:id/profile', (req, res) => {
 // Get accessibility information for a rider
 router.get('/:id/accessibility', (req, res) => {
   const { id } = req.params;
-  const params = {
-    TableName: 'Riders',
-    Key: { id },
-  };
-  docClient.get(params, (err, data) => {
+  Riders.get(id, (err, data: any) => {
+    const rider: RiderType = data;
     if (err) {
       res.send({ err });
-    } else if (!data.Item) {
-      res.send({ err: { message: 'id not found' } });
     } else {
-      const { description, accessibilityNeeds } = data.Item;
+      const { description, accessibilityNeeds } = rider;
       res.send({ description, accessibilityNeeds });
     }
   });
@@ -142,42 +151,62 @@ router.get('/:id/accessibility', (req, res) => {
 // Get all favorite locations for a rider
 router.get('/:id/favorites', (req, res) => {
   const { id } = req.params;
-  const params = {
-    TableName: 'Riders',
-    Key: { id },
-  };
-  docClient.get(params, (err, data) => {
+  Riders.get(id, (err, data: any) => {
     if (err) {
       res.send({ err });
-    } else if (!data.Item) {
-      res.send({ err: { message: 'id not found' } });
     } else {
-      const { favoriteLocations } = data.Item;
+      console.log(data);
+      const rider: RiderType = data;
+      const { favoriteLocations } = rider;
+      console.log(favoriteLocations);
       const keys: Key[] = favoriteLocations.map((locID: string) => ({
         id: locID,
       }));
       if (!keys.length) {
         res.send({ data: [] });
       } else {
-        const locParams = {
-          RequestItems: {
-            Locations: {
-              Keys: keys,
-            },
-          },
-        };
-        docClient.batchGet(locParams, (locErr, locData) => {
+        Locations.batchGet(keys, (locErr, locData) => {
           if (locErr) {
             res.send({ err: locErr });
           } else {
-            res.send({ data: locData.Responses!.Locations });
+            res.send({ data: locData });
           }
         });
       }
     }
   });
+  // const { id } = req.params;
+  // const params = {
+  //   TableName: 'Riders',
+  //   Key: { id },
+  // };
+  // docClient.get(params, (err, data) => {
+  //   if (err) {
+  //     res.send({ err });
+  //   } else if (!data.Item) {
+  //     res.send({ err: { message: 'id not found' } });
+  //   } else {
+  //     const { favoriteLocations } = data.Item;
+  //     const keys: Key[] = favoriteLocations.map((locID: string) => ({
+  //       id: locID,
+  //     }));
+  //     if (!keys.length) {
+  //       res.send({ data: [] });
+  //     } else {
+  //       const locParams = {
+  //         RequestItems: {
+  //           Locations: {
+  //             Keys: keys,
+  //           },
+  //         },
+  //       };
+  //       docClient.batchGet(locParams, (locErr, locData) => {
+  //       });
+  //     }
+  //   }
+  // });
 });
-
+/**
 // Create a rider in Riders table
 router.post('/', (req, res) => {
   if (isRight(Body.decode(req.body))) {
@@ -274,5 +303,6 @@ router.post('/:id/favorites', (req, res) => {
 
 // Delete an existing rider
 router.delete('/:id', (req, res) => deleteByID(req, res, docClient, 'Riders'));
+ */
 
 export default router;
