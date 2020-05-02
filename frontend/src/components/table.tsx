58394f0c81074af841eeb13782575f6cf6aa1e61
chansen424 <chansen424@yahoo.com>
2020-05-02 18:06:58 -0400

    drivers table gets riders for testing purposes

--- frontend/src/components/table.tsx
+++ frontend/src/components/table.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/table.css';
 
-const clientId: string = process.env.REACT_APP_CLIENT_ID!;
 interface Driver {
   name: string;
   netid: string;
@@ -149,6 +148,20 @@ const Table = () => {
     { name: 'Driver3', netid: 'advs12', email: 'advs12@cornell.edu', phone: '333-333-3333' },
     { name: 'Driver4', netid: 'bfd2', email: 'bfd2@cornell.edu', phone: '444-444-4444' },
   ]);
+
+  useEffect(() => {
+    const fetchDrivers = async () => {
+      const data = fetch("/riders", {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+      })
+        .then(res => res.json())
+        .then(data => setDrivers(data['data']))
+        .catch(err => console.log('Error while fetching:', err));
+    }
+
+    fetchDrivers();
+  });
 
   function renderTableData(allDrivers: Driver[]) {
     return allDrivers.map((driver, index) => {
