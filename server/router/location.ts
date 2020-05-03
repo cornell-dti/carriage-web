82fe9ee5590a37357d96db6146e67be5586f02fa
Matthew Guo <mg2239@cornell.edu>
2020-05-02 21:57:15 -0400

    reverted locations

--- server/router/location.ts
+++ server/router/location.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import uuid from 'uuid/v1';
 import dynamoose from 'dynamoose';
+import AWS from 'aws-sdk';
+import config from '../config';
 
 const router = express.Router();
+
+AWS.config.update(config);
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 type LocationType = {
   id: string,
@@ -20,38 +25,39 @@ export const Locations = dynamoose.model('Locations', schema, { create: false })
 
 // Get a location by ID in Locations table
 router.get('/:id', (req, res) => {
-  // const { id } = req.params;
-  // const params = {
-  //   TableName: 'Locations',
-  //   Key: { id },
-  // };
-  // docClient.get(params, (err, data) => {
-  //   if (err) {
-  //     res.send({ err });
-  //   } else {
-  //     res.send(data);
-  //   }
-  // });
+  const { id } = req.params;
+  const params = {
+    TableName: 'Locations',
+    Key: { id },
+  };
+  docClient.get(params, (err, data) => {
+    if (err) {
+      res.send({ err });
+    } else {
+      res.send(data);
+    }
+  });
 });
 
 // Put a location in Locations table
 router.post('/', (req, res) => {
-  // const postBody = req.body;
-  // const location: Location = {
-  //   id: uuid(),
-  //   location: postBody.location,
-  // };
-  // const params = {
-  //   TableName: 'Locations',
-  //   Item: location,
-  // };
-  // docClient.put(params, (err, data) => {
-  //   if (err) {
-  //     res.send({ err });
-  //   } else {
-  //     res.send(location);
-  //   }
-  // });
+  const postBody = req.body;
+  const location: LocationType = {
+    id: uuid(),
+    name: postBody.name,
+    address: postBody.address,
+  };
+  const params = {
+    TableName: 'Locations',
+    Item: location,
+  };
+  docClient.put(params, (err, data) => {
+    if (err) {
+      res.send({ err });
+    } else {
+      res.send(location);
+    }
+  });
 });
 
 // TODO: Update an existing location
