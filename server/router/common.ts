0e072a714f8d4456329972364d04cf5c64728c0a
Matthew Guo <mg2239@cornell.edu>
2020-05-08 15:50:23 -0400

    reformatted common for readability

--- server/router/common.ts
+++ server/router/common.ts
@@ -5,115 +5,118 @@ import { ObjectType } from 'dynamoose/dist/General';
 
 type ModelType = Document & Model<Document>;
 
-export const getByID = (
-  (res: Response,
-    model: ModelType,
-    id: string,
-    table: string,
-    callback?: (value: any) => void) => {
-    model.get(id, (err, data) => {
-      if (err) {
-        res.send({ err });
-      } else if (!data) {
-        res.send({ err: { message: `id not found in ${table}` } });
-      } else if (callback) {
-        callback(data);
-      } else {
-        res.send(data);
-      }
-    });
-  }
-);
-
-export const batchGet = (
-  (res: Response,
-    model: ModelType,
-    keys: ObjectType[],
-    table: string,
-    callback?: (value: any) => void) => {
-    model.batchGet(keys, (err, data) => {
-      if (err) {
-        res.send({ err });
-      } else if (!data) {
-        res.send({ err: { message: `items not found in ${table}` } });
-      } else if (callback) {
-        callback(data);
-      } else {
-        res.send({ data });
-      }
-    });
-  }
-);
-
-export const getAll = (
-  (res: Response,
-    model: ModelType,
-    table: string,
-    callback?: (value: any) => void) => {
-    model.scan().exec((err, data) => {
-      if (err) {
-        res.send({ err });
-      } else if (!data) {
-        res.send({ err: { message: `items not found in ${table}` } });
-      } else if (callback) {
-        callback(data);
-      } else {
-        res.send(data);
-      }
-    });
-  }
-);
-
-export const create = (
-  (res: Response, doc: Document, callback?: (value: any) => void) => {
-    doc.save((err, data) => {
-      if (err) {
-        res.send({ err });
-      } else if (callback) {
-        callback(data);
-      } else {
-        res.send(data);
-      }
-    });
-  }
-);
-
-export const update = (
-  (res: Response,
-    model: ModelType,
-    key: ObjectType,
-    updateObj: ObjectType,
-    table: string,
-    callback?: (value: any) => void) => {
-    model.update(key, updateObj, (err, data) => {
-      if (err) {
-        res.send({ err });
-      } else if (!data) {
-        res.send({ err: { message: `id not found in ${table}` } });
-      } else if (callback) {
-        callback(data);
-      } else {
-        res.send(data);
-      }
-    });
-  }
-);
-
-export const deleteByID = (
-  (res: Response,
-    model: ModelType,
-    id: string,
-    table: string,
-    callback?: (value: any) => void) => {
-    model.get(id, (err, data) => {
-      if (err) {
-        res.send({ err });
-      } else if (!data) {
-        res.send({ err: { message: `id not found in ${table}` } });
-      } else if (callback) {
-        callback(data);
-      } else {
-        data.delete().then(() => res.send({ id }));
-      }
-    });
+export function getByID(
+  res: Response,
+  model: ModelType,
+  id: string,
+  table: string,
+  callback?: (value: any) => void,
+) {
+  model.get(id, (err, data) => {
+    if (err) {
+      res.send({ err });
+    } else if (!data) {
+      res.send({ err: { message: `id not found in ${table}` } });
+    } else if (callback) {
+      callback(data);
+    } else {
+      res.send(data);
+    }
   });
+}
+
+export function batchGet(
+  res: Response,
+  model: ModelType,
+  keys: ObjectType[],
+  table: string,
+  callback?: (value: any) => void,
+) {
+  model.batchGet(keys, (err, data) => {
+    if (err) {
+      res.send({ err });
+    } else if (!data) {
+      res.send({ err: { message: `items not found in ${table}` } });
+    } else if (callback) {
+      callback(data);
+    } else {
+      res.send({ data });
+    }
+  });
+}
+
+export function getAll(
+  res: Response,
+  model: ModelType,
+  table: string,
+  callback?: (value: any) => void,
+) {
+  model.scan().exec((err, data) => {
+    if (err) {
+      res.send({ err });
+    } else if (!data) {
+      res.send({ err: { message: `items not found in ${table}` } });
+    } else if (callback) {
+      callback(data);
+    } else {
+      res.send(data);
+    }
+  });
+}
+
+export function create(
+  res: Response,
+  doc: Document,
+  callback?: (value: any) => void,
+) {
+  doc.save((err, data) => {
+    if (err) {
+      res.send({ err });
+    } else if (callback) {
+      callback(data);
+    } else {
+      res.send(data);
+    }
+  });
+}
+
+export function update(
+  res: Response,
+  model: ModelType,
+  key: ObjectType,
+  updateObj: ObjectType,
+  table: string,
+  callback?: (value: any) => void,
+) {
+  model.update(key, updateObj, (err, data) => {
+    if (err) {
+      res.send({ err });
+    } else if (!data) {
+      res.send({ err: { message: `id not found in ${table}` } });
+    } else if (callback) {
+      callback(data);
+    } else {
+      res.send(data);
+    }
+  });
+}
+
+export function deleteByID(
+  res: Response,
+  model: ModelType,
+  id: string,
+  table: string,
+  callback?: (value: any) => void,
+) {
+  model.get(id, (err, data) => {
+    if (err) {
+      res.send({ err });
+    } else if (!data) {
+      res.send({ err: { message: `id not found in ${table}` } });
+    } else if (callback) {
+      callback(data);
+    } else {
+      data.delete().then(() => res.send({ id }));
+    }
+  });
+}
