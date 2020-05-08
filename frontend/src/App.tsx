97186e2fa61c3b5d5e8d2a94e86dccee059ef7a1
chansen424 <chansen424@yahoo.com>
2020-05-08 13:13:26 -0400

    shared authentication component between all pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LandingPage from './components/landing';
 import DriverTable from './components/driver-table';
 import RiderTable from './components/rider-table';
+import { SignInButton } from './components/signin';
 
 import './App.css';
 
 const App = () => (
   <Router>
+    <SignInButton />
     <Switch>
       <Route exact path="/" component={LandingPage} />
       <Route path="/driver-table" component={DriverTable} />
