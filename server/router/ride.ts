ab437f528e259d2bb248eda60582eeae0ba4d63e
Matthew Guo <mg2239@cornell.edu>
2020-05-13 14:06:16 -0400

    made queries a bit faster

--- server/router/ride.ts
+++ server/router/ride.ts
@@ -6,14 +6,20 @@ import * as db from './common';
 
 const router = express.Router();
 
-enum RideTypes {
+const enum RideType {
   active = 'active',
   past = 'past',
   unscheduled = 'unscheduled',
 }
 
+const enum Index {
+  rider = 'riderIndex',
+  driver = 'driverIndex',
+  time = 'timeIndex',
+}
+
 type Ride = {
-  type: RideTypes,
+  type: RideType,
   id: string,
   startLocation: string,
   endLocation: string,
@@ -29,7 +35,7 @@ const schema = new dynamoose.Schema({
     type: String,
     enum: ['active', 'past', 'unscheduled'],
     index: {
-      name: 'timeIndex',
+      name: Index.time,
       rangeKey: 'startTime',
     } as any,
   },
@@ -44,7 +50,7 @@ const schema = new dynamoose.Schema({
   riderID: {
     type: String,
     index: {
-      name: 'riderIndex',
+      name: Index.rider,
       rangeKey: 'type',
       global: true,
     } as any,
@@ -52,7 +58,7 @@ const schema = new dynamoose.Schema({
   driverID: {
     type: String,
     index: {
-      name: 'driverIndex',
+      name: Index.driver,
       rangeKey: 'type',
       global: true,
     } as any,
@@ -63,8 +69,8 @@ export const Rides = dynamoose.model('Rides', schema, { create: false });
 
 // Get a ride by ID in Rides table
 router.get('/:type/:id', (req, res) => {
-  const { id, type } = req.params;
-  db.getByID(res, Rides, { id, type }, 'Rides');
+  const { type, id } = req.params;
+  db.getByID(res, Rides, { type, id }, 'Rides');
 });
 
 // Get all rides in Rides table
@@ -74,21 +80,31 @@ router.get('/', (req, res) => db.getAll(res, Rides, 'Rides'));
 router.get('/:type', (req, res) => {
   const { type } = req.params;
   const { riderID, driverID, date } = req.query;
+  // default hash key is type, default index is none
   let condition = new Condition('type').eq(type);
   let index;
+  if (riderID) {
+    // if querying rider, must change hash key to riderID and index to riderIndex
+    condition = new Condition('riderID').eq(riderID).where('type').eq(type);
+    index = Index.rider;
+  }
+  if (driverID) {
+    if (index) {
+      // if index already set to riderIndex, use filter operation on driverID
+      condition = condition.where('driverID').eq(driverID);
+    } else {
+      // otherwise change hash key to driverID and index to driverIndex
+      condition = new Condition('driverID').eq(driverID).where('type').eq(type);
+      index = Index.driver;
+    }
+  }
   if (date) {
     const dateStart = new Date(`${date} EST`).toISOString();
     const dateEnd = new Date(`${date} 23:59:59.999 EST`).toISOString();
+    // startTime used as range key or filter operation depending on index
     condition = condition.where('startTime').between(dateStart, dateEnd);
-    index = 'timeIndex';
-  }
-  if (riderID) {
-    condition = condition.where('riderID').eq(riderID);
-    index = 'riderIndex';
-  }
-  if (driverID) {
-    condition = condition.where('driverID').eq(driverID);
-    index = 'driverIndex';
+    // if no index set, use timeIndex to set startTime as range key
+    index = index || Index.time;
   }
   db.query(res, Rides, condition, index);
 });
@@ -98,7 +114,7 @@ router.post('/', (req, res) => {
   const postBody = req.body;
   const rideID = uuid();
   const ride = new Rides({
-    type: RideTypes.unscheduled,
+    type: RideType.unscheduled,
     id: rideID,
     startLocation: postBody.startLocation,
     endLocation: postBody.endLocation,
@@ -109,14 +125,17 @@ router.post('/', (req, res) => {
   db.create(res, ride);
 });
 
-// TODO: Update an existing ride
-router.put('/:id', (req, res) => {
-  res.send();
+// Update an existing ride
+router.put('/:type/:id', (req, res) => {
+  const { type, id } = req.params;
+  const postBody = req.body;
+  db.update(res, Rides, { type, id }, postBody, 'Rides');
 });
 
-// TODO: Delete an existing ride
-router.delete('/:id', (req, res) => {
-  res.send();
+// Delete an existing ride
+router.delete('/:type/:id', (req, res) => {
+  const { type, id } = req.params;
+  db.deleteByID(res, Rides, { type, id }, 'Rides');
 });
 
 export default router;
