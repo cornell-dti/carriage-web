174f1a9db571378ea87622a6c794da81767ab012
Matthew Guo <mg2239@cornell.edu>
2020-09-26 18:38:51 -0400

    Add name and capacity fields to vehicle

--- server/models/vehicle.ts
+++ server/models/vehicle.ts
@@ -2,11 +2,18 @@ import dynamoose from 'dynamoose';
 
 export type VehicleType = {
   id: string,
+  name: string,
+  capacity: number,
   wheelchairAccessible: boolean,
 };
 
 const schema = new dynamoose.Schema({
-  id: String,
+  id: {
+    type: String,
+    hashKey: true,
+  },
+  name: String,
+  capacity: Number,
   wheelchairAccessible: Boolean,
 });
 
