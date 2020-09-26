857468ff109d12eb211c8f1f8f669e86ca1cbcc2
Matthew Guo <mg2239@cornell.edu>
2020-09-26 16:28:55 -0400

    styled drivercards and added heading to drivers page

--- frontend/src/components/DriverCards/DriverCards.tsx
+++ frontend/src/components/DriverCards/DriverCards.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Card from './Card';
 import data from './data';
+import styles from './drivercards.module.css';
 
 const DriverCards = () => (
-  <div>
+  <div className={styles.cardsContainer}>
     {data.map((driver) => <Card key={driver.id} driver={driver} />)}
   </div>
 );
--- frontend/src/pages/Dashboard/Drivers.tsx
+++ frontend/src/pages/Dashboard/Drivers.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import DriverCards from '../../components/DriverCards/DriverCards';
 
 const Drivers = () => (
-  <DriverCards />
+  <>
+    <h1>Drivers</h1>
+    <DriverCards />
+  </>
 );
 
 export default Drivers;
