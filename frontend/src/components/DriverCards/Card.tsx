2b5df03911542f6f926aacfad1db1741586ad05e
Matthew Guo <mg2239@cornell.edu>
2020-09-26 16:28:18 -0400

    finished basic card

--- frontend/src/components/DriverCards/Card.tsx
+++ frontend/src/components/DriverCards/Card.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import styles from './card.module.css';
 import { Driver, BreakType } from '../../types';
+import { clock, phone, wheel } from './icons';
 
 const formatTime = (time: string) => {
   const hours = Number(time.substring(0, 2));
@@ -32,9 +34,30 @@ const parseAvailability = (
   return availability;
 };
 
+const formatPhone = (phoneNumber: string) => {
+  const areaCode = phoneNumber.substring(0, 3);
+  const firstPart = phoneNumber.substring(3, 6);
+  const secondPart = phoneNumber.substring(6, 10);
+  return `${areaCode}-${firstPart}-${secondPart}`;
+};
+
+type CardInfoProps = {
+  icon: string;
+  alt: string;
+  info?: string;
+  children?: JSX.Element;
+};
+
+const CardInfo = ({ icon, alt, info, children }: CardInfoProps) => (
+  <div className={styles.infoContainer}>
+    <img className={styles.icon} src={icon} alt={alt} />
+    {info ? <p className={styles.info}>{info}</p> : children}
+  </div>
+);
+
 type CardProps = {
   driver: Driver;
-}
+};
 
 const Card = ({
   driver: {
@@ -50,18 +73,29 @@ const Card = ({
 }: CardProps) => {
   const fullName = `${firstName} ${lastName}`;
   const netId = email.split('@')[0];
+  const fmtPhone = formatPhone(phoneNumber);
   const availability = parseAvailability(startTime, endTime, breaks);
 
   return (
-    <div>
-      <div>image</div>
-      <p>{fullName}</p>
-      <p>{netId}</p>
-      <p>{phoneNumber}</p>
-      {availability.map(([day, timeRange]) => (
-        <p key={day}><b>{day}:</b> {timeRange}</p>
-      ))}
-      <p>{vehicle}</p>
+    <div className={styles.card}>
+      <div className={styles.image}>image</div>
+      <div className={styles.contentContainer}>
+        <div className={styles.titleContainer}>
+          <p className={styles.name}>{fullName}</p>
+          <p className={styles.netId}>{netId}</p>
+        </div>
+        <CardInfo icon={phone} alt="phone icon" info={fmtPhone} />
+        <CardInfo icon={clock} alt="clock icon">
+          <div className={styles.availabilityContainer}>
+            {availability.map(([day, timeRange]) => (
+              <p key={day} className={styles.info}>
+                <b>{day}:</b> {timeRange}
+              </p>
+            ))}
+          </div>
+        </CardInfo>
+        <CardInfo icon={wheel} alt="wheel icon" info={vehicle} />
+      </div>
     </div>
   );
 };
