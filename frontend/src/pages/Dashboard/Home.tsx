c3009cb925eb374bc0597fe19c0be60a52c9526f
Matthew Guo <mg2239@cornell.edu>
2020-10-24 16:45:17 -0400

    positioned add ride button

--- frontend/src/pages/Dashboard/Home.tsx
+++ frontend/src/pages/Dashboard/Home.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import RideModal from '../../components/RideModal/RideModal';
 import Table from '../../components/UserTables/UnscheduledTable';
+import styles from './home.module.css';
 
 const Home = () => (
   <>
-    <h1>Homepage</h1>
-    <RideModal />
+    <div className={styles.pageTitle}>
+      <h1>Homepage</h1>
+      <RideModal />
+    </div>
     <Table />
   </>
 );
