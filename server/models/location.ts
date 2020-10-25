e388c22b6db2e66a5b7e64d5a43928992ae63ca7
mly32 <mly32@cornell.edu>
2020-10-24 20:26:03 -0400

    Added tags

--- server/models/location.ts
+++ server/models/location.ts
@@ -1,15 +1,27 @@
 import dynamoose from 'dynamoose';
 
 export type LocationType = {
-  id: string,
-  name: string,
-  address: string,
+  id: string;
+  name: string;
+  address: string;
 };
+
+export enum Tag {
+  WEST = 'west',
+  CENTRAL = 'central',
+  NORTH = 'north',
+  CTOWN = 'ctown', // college town
+  DTOWN = 'dtown', // downtown
+}
 
 const schema = new dynamoose.Schema({
   id: String,
   name: String,
   address: String,
+  tag: {
+    type: String,
+    enum: Object.values(Tag),
+  },
 });
 
 export const Location = dynamoose.model('Locations', schema, { create: false });
