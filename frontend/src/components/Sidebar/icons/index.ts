5401c54a0e90bc88a097fdb0486bca64b22522f7
jjc387 <jjc387@cornell.edu>
2020-11-03 12:12:25 -0500

    Replaced menu icons with svgs

--- frontend/src/components/Sidebar/icons/index.ts
+++ frontend/src/components/Sidebar/icons/index.ts
@@ -1,5 +1,5 @@
-export { default as drivers } from './drivers.png';
-export { default as home } from './home.png';
-export { default as riders } from './riders.png';
-export { default as settings } from './settings.png';
-export { default as locations } from './locations.png';
+export { default as drivers } from './drivers.svg';
+export { default as home } from './home.svg';
+export { default as riders } from './riders.svg';
+export { default as settings } from './settings.svg';
+export { default as locations } from './locations.svg';
