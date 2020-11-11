8eb184d55bdd111f0200845ae8cafed0e99c664c
jjc387 <jjc387@cornell.edu>
2020-11-11 11:40:57 -0500

    fixed heading on home page

--- frontend/src/pages/Dashboard/Home.tsx
+++ frontend/src/pages/Dashboard/Home.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import RideModal from '../../components/RideModal/RideModal';
 import Table from '../../components/UserTables/UnscheduledTable';
 import Schedule from '../../components/Schedule/Schedule';
-import styles from './home.module.css';
+import styles from './page.module.css';
 
 const Home = () => (
   <>
     <div className={styles.pageTitle}>
-      <h1>Homepage</h1>
+      <h1 className={styles.header}>Homepage</h1>
       <RideModal />
     </div>
     <Schedule />
