19c1deddc937868170172a395b7746edcf4a94f3
Matthew Guo <mg2239@cornell.edu>
2020-11-28 00:56:43 -0500

    move pageTitle style to page css file

--- frontend/src/pages/Dashboard/Home.tsx
+++ frontend/src/pages/Dashboard/Home.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import RideModal from '../../components/RideModal/RideModal';
 import Table from '../../components/UserTables/UnscheduledTable';
 import Schedule from '../../components/Schedule/Schedule';
-import styles from './home.module.css';
+import styles from './page.module.css';
 import { Driver } from '../../types/index';
 
 const Home = () => {
@@ -19,14 +19,15 @@ const Home = () => {
     fetchDrivers();
   }, []);
 
-  return (<>
-    <div className={styles.pageTitle}>
-      <h1>Homepage</h1>
-      <RideModal />
-    </div>
-    <Schedule />
-    <Table drivers={drivers} />
-  </>
+  return (
+    <>
+      <div className={styles.pageTitle}>
+        <h1>Homepage</h1>
+        <RideModal />
+      </div>
+      <Schedule />
+      <Table drivers={drivers} />
+    </>
   );
 };
 
