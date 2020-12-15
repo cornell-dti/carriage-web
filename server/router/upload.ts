c34688776a0225ca617284577700e00f2a666941
Simran Puri <sp2262@cornell.edu>
2020-12-15 11:53:16 -0500

    added s3 upload api

---
+++ server/router/upload.ts
@@ -0,0 +1,48 @@
+import express from 'express';
+import * as AWS from 'aws-sdk';
+import { Condition } from 'dynamoose';
+import * as db from './common';
+import { Rider } from '../models/rider';
+import { Driver } from '../models/driver';
+import { Dispatcher } from '../models/dispatcher';
+
+const router = express.Router();
+
+const BUCKET_NAME = 'dti-carriage-staging-public';
+const s3bucket = new AWS.S3();
+
+// Uploads base64-encoded {fileBuffer} to S3 with the key {tableName}/{userId}
+// Sets the user's DB photoLink field to the url of the uploaded image, if not set
+router.post('/', (req, res) => {
+  const { body: { userId, tableName, fileBuffer } } = req;
+  const objectKey = `${tableName}/${userId}`;
+
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: objectKey,
+    Body: fileBuffer,
+    ACL: 'public-read',
+  };
+
+  s3bucket.putObject(params, (s3Err, s3data) => {
+    if (s3Err) {
+      res.send(s3Err);
+    } else {
+      res.send(s3data);
+    }
+  });
+
+  const objectLink = `${BUCKET_NAME}.s3.us-east-2.amazonaws.com/${objectKey}`;
+  const operation = { $ADD: { photoLink: [objectLink] } };
+  const condition = new Condition().not().exists('photoLink');
+
+  if (tableName === 'Drivers') {
+    db.conditionalUpdate(res, Driver, { userId }, operation, condition, tableName);
+  } else if (tableName === 'Riders') {
+    db.conditionalUpdate(res, Rider, { userId }, operation, condition, tableName);
+  } else if (tableName === 'Dispatchers') {
+    db.conditionalUpdate(res, Dispatcher, { userId }, operation, condition, tableName);
+  }
+});
+
+export default router;
