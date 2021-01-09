1dae3340e4bcb645d8b4ad667b67637ef9438aa0
Matthew Guo <mg2239@cornell.edu>
2021-01-08 22:43:09 -0500

    add jwt to auth context

--- frontend/src/context/auth.ts
+++ frontend/src/context/auth.ts
@@ -1,5 +1,7 @@
-import { createContext } from 'react';
+import { createContext, useContext } from 'react';
 
-const AuthContext = createContext({ logout: () => { } });
+const AuthContext = createContext({ jwt: '', logout: () => { } });
+
+export const useAuth = () => useContext(AuthContext);
 
 export default AuthContext;
