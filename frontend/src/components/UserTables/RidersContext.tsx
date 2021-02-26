aac3c0749394015826c81b3d1194e866d4ddd612
Laura Sizemore <ls797@cornell.edu>
2021-02-25 21:56:24 -0500

    fixed linter warnings

--- frontend/src/components/UserTables/RidersContext.tsx
+++ frontend/src/components/UserTables/RidersContext.tsx
@@ -3,14 +3,14 @@ import { Rider } from '../../types';
 import { useReq } from '../../context/req';
 
 type ridersState = {
-    getExistingRiders: () => Array<Rider>,
-    refreshRiders: () => Promise<Array<Rider>>
+    getExistingRiders: () => Array<Rider>, 
+    refreshRiders: () => Promise<Array<Rider>> 
 };
 
 const initialState: ridersState = {
-  getExistingRiders: () => [],
-  refreshRiders: async () => [],
-};
+   getExistingRiders: () => [],
+   refreshRiders: async () =>  []
+  };
 const RidersContext = React.createContext(initialState);
 
 export const useRiders = () => React.useContext(RidersContext);
@@ -22,30 +22,35 @@ type RidesProviderProps = {
 type RidersType = Array<Rider>;
 
 export const RidesProvider = ({ children }: RidesProviderProps) => {
-  const [riders, setRiders] = useState<RidersType>([]);
-  const getExistingRiders = () => riders;
+  const [riders, setRiders] = useState<RidersType>([])
+  const getExistingRiders = () => {return riders};
   const { withDefaults } = useReq();
-  const refreshRiders = useCallback(async () => {
+  const refreshRiders = async () => {
     const ridersData:RidersType = await fetch('/api/riders', withDefaults())
-      .then((res) => res.json())
-      .then((data) => data.data);
-
-    setRiders((prev) => ({ ...prev, ridersData }));
-    return riders;
-  });
-    // Initialize the data
-  React.useEffect(() => {
-    refreshRiders();
-  }, [refreshRiders, setRiders, withDefaults]);
-
-  return (
+        .then((res) => res.json())
+        .then((data) => data.data);
+        
+      setRiders((prev) => ({ ...prev, ridersData})); 
+      return riders;
+    };
+    //Initialize the data
+    React.useEffect(()=> {
+      refreshRiders();
+    }, [setRiders, withDefaults]);
+  
+    return (
       <RidersContext.Provider
         value={{
-          getExistingRiders,
-          refreshRiders,
+          getExistingRiders, 
+          refreshRiders
         }}
       >
         {children}
       </RidersContext.Provider>
-  );
+    );
 };
+  
+
+
+
+
