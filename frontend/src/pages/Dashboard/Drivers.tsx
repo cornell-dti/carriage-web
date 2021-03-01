8d1db8fbf4790788e6c0bb83b59f773c16fb63f8
Pinxuan Huang <pinxhuang@gmail.com>
2021-02-28 21:34:30 -0500

    Add Admin section to Drivers page

--- frontend/src/pages/Dashboard/Drivers.tsx
+++ frontend/src/pages/Dashboard/Drivers.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import DriverModal from '../../components/DriverModal/DriverModal';
 import DriverCards from '../../components/DriverCards/DriverCards';
+import AdminCards from '../../components/AdminCards/AdminCards';
 import styles from './page.module.css';
 
 const Drivers = () => (
@@ -11,7 +12,11 @@ const Drivers = () => (
         <DriverModal />
       </div>
     </div>
-    <DriverCards />
+    <DriverCards /> 
+    <div className={styles.pageTitle}>
+      <h1 className={styles.header}>Admins</h1>
+    </div>
+    <AdminCards />
   </>
 );
 
