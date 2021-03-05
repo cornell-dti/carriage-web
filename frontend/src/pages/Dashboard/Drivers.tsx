1c1f94559d7b4892e9ce49e186bc3624e1ae7ed2
Pinxuan Huang <pinxhuang@gmail.com>
2021-03-05 12:40:48 -0500

    Remove AdminCards

--- frontend/src/pages/Dashboard/Drivers.tsx
+++ frontend/src/pages/Dashboard/Drivers.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import DriverModal from '../../components/DriverModal/DriverModal';
 import DriverCards from '../../components/DriverCards/DriverCards';
-import AdminCards from '../../components/AdminCards/AdminCards';
 import styles from './page.module.css';
 
 const Drivers = () => (
@@ -12,11 +11,7 @@ const Drivers = () => (
         <DriverModal />
       </div>
     </div>
-    <DriverCards /> 
-    <div className={styles.pageTitle}>
-      <h1 className={styles.header}>Admins</h1>
-    </div>
-    <AdminCards />
+    <DriverCards />
   </>
 );
 
