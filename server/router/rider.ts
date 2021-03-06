73e65e34069c9b3424630df02aa424081cdba5b5
Matthew Guo <mg2239@cornell.edu>
2021-03-05 22:58:58 -0500

    add current ride endpoint

--- server/router/rider.ts
+++ server/router/rider.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { v4 as uuid } from 'uuid';
 import { Condition } from 'dynamoose';
+import moment from 'moment';
 import * as db from './common';
 import { Rider, RiderType } from '../models/rider';
 import { Location } from '../models/location';
 import { createKeys, validateUser } from '../util';
+import { Ride, Type } from '../models/ride';
 
 const router = express.Router();
 const tableName = 'Riders';
@@ -57,6 +59,21 @@ router.get('/:id/favorites', validateUser('User'), (req, res) => {
   db.getById(res, Rider, id, tableName, ({ favoriteLocations }: RiderType) => {
     const keys = createKeys('id', favoriteLocations);
     db.batchGet(res, Location, keys, 'Locations');
+  });
+});
+
+// Get current ride of rider, if exists
+router.get('/:id/currentride', validateUser('Rider'), (req, res) => {
+  const { params: { id } } = req;
+  db.getById(res, Rider, id, tableName, () => {
+    const now = moment().toISOString();
+    const end = moment().add(30, 'minutes').toISOString();
+    const isRider = new Condition('rider').eq(id);
+    const isActive = new Condition('type').eq(Type.ACTIVE);
+    const isSoon = new Condition('startTime').between(now, end);
+    const isNow = new Condition('startTime').le(now).where('endTime').ge(now);
+    const condition = isRider.group(isActive.group(isSoon.or().group(isNow)));
+    db.scan(res, Ride, condition);
   });
 });
 
