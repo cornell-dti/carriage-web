b342d14491a3d5f8834201853b4e5f78cce2684a
Laura Sizemore <ls797@cornell.edu>
2021-03-11 21:18:29 -0500

    added to export

--- frontend/src/components/ExportPreview/ExportPreview.tsx
+++ frontend/src/components/ExportPreview/ExportPreview.tsx
@@ -1,35 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ScheduledTable from '../UserTables/ScheduledTable';
 import moment from 'moment';
 import { Driver } from '../../types/index';
 import styles from './exportPreview.module.css';
-import { useReq } from '../../context/req';
 import { useDate } from '../../context/date';
+import {useDrivers, DriversProvider} from '../../context/DriversContext';
+
+const Drivers = () => { 
+  const {
+    drivers,
+  } = useDrivers();
+  return(
+    <div id="exportTable">
+    {drivers.map((driver: Driver, index: number) => (
+      <ScheduledTable
+        key={index}
+        driverId={driver.id}
+        driverName={driver.firstName + ' ' + driver.lastName}
+      />
+    ))}
+  </div>
+  );
+};
 
 const ExportPreview = () => {
-  const [drivers, setDrivers] = useState<Driver[]>([]);
-  const { withDefaults } = useReq();
   const { curDate } = useDate();
-
-  useEffect(() => {
-    fetch('/api/drivers', withDefaults())
-      .then((res) => res.json())
-      .then(({ data }) => setDrivers(data));
-  }, [withDefaults]);
-
   return (
     <>
       <p className={styles.date}>{moment(curDate).format('YYYY-MM-DD')}</p>
       <h1 className={styles.header}>Scheduled Rides</h1>
-      <div id="exportTable">
-        {drivers.map((driver: Driver, index: number) => (
-          <ScheduledTable
-            key={index}
-            driverId={driver.id}
-            driverName={driver.firstName + ' ' + driver.lastName}
-          />
-        ))}
-      </div>
+      <DriversProvider>
+        <Drivers/>
+      </DriversProvider>
     </>
   );
 };
--- frontend/src/components/RideModal/Pages/Driver.tsx
+++ frontend/src/components/RideModal/Pages/Driver.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
 import cn from 'classnames';
-import { useReq } from '../../../context/req';
 import { ModalPageProps } from '../../Modal/types';
 import styles from '../ridemodal.module.css';
-import { Driver } from '../../../types/index';
 import { Label, Input, Button } from '../../FormElements/FormElements';
+import {useDrivers, DriversProvider} from '../../../context/DriversContext';
 
 const DriverPage = ({ onBack, onSubmit, formData }: ModalPageProps) => {
   const { register, handleSubmit } = useForm({
@@ -13,17 +12,11 @@ const DriverPage = ({ onBack, onSubmit, formData }: ModalPageProps) => {
       driver: formData?.driver ?? '',
     },
   });
-  const [drivers, setDrivers] = useState<Driver[]>([]);
-  const { withDefaults } = useReq();
-
-  useEffect(() => {
-    fetch('/api/drivers', withDefaults())
-      .then((res) => res.json())
-      .then(({ data }) => setDrivers(data));
-  }, [withDefaults]);
-
-  return (
-    <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+  const  Drivers = () => { 
+    const {
+      drivers,
+    } = useDrivers();
+    return (
       <div className={cn(styles.inputContainer, styles.drivers)}>
         {drivers.map((d) => (
           <div className={styles.driver} key={d.id}>
@@ -40,6 +33,14 @@ const DriverPage = ({ onBack, onSubmit, formData }: ModalPageProps) => {
           </div>
         ))}
       </div>
+    );
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+      <DriversProvider>
+        <Drivers/>
+      </DriversProvider>
       <div className={styles.btnContainer}>
         <Button outline type="button" onClick={onBack}>Back</Button>
         <Button type="submit">Next</Button>
