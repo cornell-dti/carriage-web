c2f7817795b74aef91d39a47bb61f27c5a5153b3
Simran Puri <sp2262@cornell.edu>
2021-03-25 17:19:39 -0400

    added function to generate repeating rides

--- server/util/repeatingRides.ts
+++ server/util/repeatingRides.ts
@@ -2,9 +2,10 @@ import { v4 as uuid } from 'uuid';
 import moment from 'moment';
 import { Condition } from 'dynamoose';
 import schedule from 'node-schedule';
+import { AnyDocument } from 'dynamoose/dist/Document';
 import { Ride, RideType } from '../models/ride';
 
-// run function at 10:05:00 every day
+// run function at 10:05:00am every day
 schedule.scheduleJob('00 05 10 * * *', () => {
   createRepeatingRides();
 });
@@ -12,7 +13,7 @@ schedule.scheduleJob('00 05 10 * * *', () => {
 function createRepeatingRides() {
   let tomorrowDate = moment(new Date());
   tomorrowDate = tomorrowDate.add(1, 'days');
-  let tomorrowDateOnly = tomorrowDate.format('MM/DD/YYYY');
+  const tomorrowDateString = tomorrowDate.format('MM/DD/YYYY');
 
   const tomorrowDay = tomorrowDate.weekday();
 
@@ -22,85 +23,79 @@ function createRepeatingRides() {
     .where('recurringDays')
     .contains(tomorrowDay);
 
-  Ride
-    .scan(condition)
-    .exec((_, data) => {
-      data?.forEach((ride) => {
-        const { rider, driver, startLocation, endLocation,
-          endDate, edits } = ride as unknown as RideType;
+  Ride.scan(condition).exec((_, data) => {
+    data?.forEach(async (masterRide) => {
+      const { rider, driver, startLocation, endLocation,
+        endDate, edits } = masterRide as unknown as RideType;
 
-        if (!endDate || (endDate > tomorrowDateOnly)) {
-          tomorrowDateOnly = moment(tomorrowDateOnly).format('YYYY-MM-DD');
+      if (!endDate || (endDate > tomorrowDateString)) {
+        // const tomorrowDateOnly = moment(new Date()).add(1, 'days').format('YYYY-MM-DD');
+        const tomorrowDateOnly = moment(new Date()).format('YYYY-MM-DD');
 
-          const newStartTimeOnly = moment(ride.startTime).format('HH:mm:ss');
-          const newStartTime = moment(`${tomorrowDateOnly}T${newStartTimeOnly}`).toISOString();
+        const newStartTimeOnly = moment(masterRide.startTime).format('HH:mm:ss');
+        const newStartTime = moment(`${tomorrowDateOnly}T${newStartTimeOnly}`).toISOString();
 
-          const newEndTimeOnly = moment(ride.requestedEndTime).format('HH:mm:ss');
-          const newEndTime = moment(`${tomorrowDateOnly}T${newEndTimeOnly}`).toISOString();
+        const newEndTimeOnly = moment(masterRide.requestedEndTime).format('HH:mm:ss');
+        const newEndTime = moment(`${tomorrowDateOnly}T${newEndTimeOnly}`).toISOString();
 
-          const repeatingRide = new Ride({
-            id: uuid(),
-            rider,
-            startLocation,
-            endLocation,
-            startTime: newStartTime,
-            requestedEndTime: newEndTime,
-            endTime: newEndTime,
-            driver,
-            recurring: false,
-          });
-          repeatingRide.save();
+        const repeatingRide = new Ride({
+          id: uuid(),
+          rider,
+          startLocation,
+          endLocation,
+          startTime: newStartTime,
+          requestedEndTime: newEndTime,
+          endTime: newEndTime,
+          driver,
+          recurring: false,
+        });
 
-          if (edits) {
-            edits.forEach((editId) => {
-              Ride.get(editId, (__, dataEdit) => {
-                const editRide = dataEdit as unknown as RideType;
-                const editDate = moment(editRide.startTime);
-                const editDateOnly = moment(editDate).format('YYYY-MM-DD');
-
-                // only look at edits that match tomorrow's date
-                if (editDateOnly === tomorrowDateOnly) {
-                  if (editRide.deleted === true) {
-                    // find and remove the ride instance that matches this edit
-                    // this condition excludes the edit itself (deleted field must not exist)
-                    // if there are multiple matches, only one is deleted
-                    const conditionToDelete = new Condition()
-                      .where('recurring').eq(false)
-                      .and()
-                      .where('deleted')
-                      .not()
-                      .exists()
-                      .where('startTime')
-                      .eq(editRide.startTime)
-                      .and()
-                      .where('requestedEndTime')
-                      .eq(editRide.requestedEndTime)
-                      .and()
-                      .where('rider')
-                      .eq(editRide.rider.id)
-                      .and()
-                      .where('startLocation')
-                      .eq(editRide.startLocation.id)
-                      .and()
-                      .where('endLocation')
-                      .eq(editRide.endLocation.id);
-
-                    Ride
-                      .scan(conditionToDelete)
-                      .exec((___, dataDelete) => {
-                        const rideToRemove = dataDelete[0] as unknown as RideType;
-
-                        Ride.get(rideToRemove.id, (____, dataGetToDelete) => {
-                          dataGetToDelete?.delete();
-                        });
-                      });
-                  }
-                  // if deleted = false, this ride instance should be kept
-                }
-              });
-            });
-          }
+        if (edits) {
+          handleEdits(edits, tomorrowDateOnly, repeatingRide, masterRide);
         }
-      });
+      }
     });
+  });
+}
+
+function handleEdits(edits: string[], tomorrowDateOnly: string,
+  repeatingRide: AnyDocument, masterRide: AnyDocument) {
+  const seenEdits: string[] = [];
+  let editCount = 0;
+  const numEdits = edits.length;
+
+  edits.forEach(async (editId) => {
+    Ride.get(editId).then(async (editRide) => {
+      const editDate = moment(editRide.startTime);
+      const editDateOnly = moment(editDate).add(1, 'days').format('YYYY-MM-DD');
+
+      // only look at edits that match tomorrow's date
+      if (editDateOnly === tomorrowDateOnly) {
+        seenEdits.push(editId);
+        // if deleted = true
+        if (editRide.deleted === true) {
+          // create repeating ride only if it does NOT match the delete edit
+          if (!(repeatingRide.startTime === editRide.startTime
+            && repeatingRide.requestedEndTime === editRide.requestedEndTime
+            && repeatingRide.rider.id === editRide.rider.id
+            && repeatingRide.startLocation.id === editRide.startLocation.id
+            && repeatingRide.endLocation.id === editRide.endLocation.id)) {
+            repeatingRide.save();
+          }
+          // remove this delete edit
+          Ride.get(editId).then((dataGetToDelete) => {
+            dataGetToDelete?.delete();
+          });
+        }
+        // if deleted = false, the instance is kept as a valid ride
+      }
+      editCount += 1;
+      if (editCount === numEdits) {
+        const newEdits = await masterRide.edits.filter((id: string) => !(seenEdits.includes(id)));
+        const operation = { $SET: { edits: newEdits } };
+        const keyId = masterRide.id;
+        Ride.update({ id: keyId }, operation);
+      }
+    });
+  });
 }
