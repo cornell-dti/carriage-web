4a406a4aaf9ba420e6d8528f46eab25183f5fcf5
Matthew Guo <mg2239@cornell.edu>
2021-03-25 21:07:03 -0400

    add id to riderData state

--- frontend/src/components/UserTables/RidersTable.tsx
+++ frontend/src/components/UserTables/RidersTable.tsx
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router-dom';
 import TableRow from '../TableComponents/TableRow';
 import { NewRider } from '../../types';
 import styles from './table.module.css';
-import {useRiders} from '../../context/RidersContext';
+import { useRiders } from '../../context/RidersContext';
 
 function renderTableHeader() {
   return (
@@ -33,63 +33,65 @@ function renderAccessNeeds(accessNeeds: Array<string>) {
   }
   return null;
 }
-  const RidersTable = () => { 
-    const history = useHistory();
-    const {riders} = useRiders();
-    function renderTableData(allRiders: NewRider[]) {
-      return allRiders.map((rider, index) => {
-        const {
-          firstName,
-          lastName,
-          phoneNumber,
-          address,
-          joinDate,
-          email,
-          accessibility,
-        } = rider;
-        const valuePhone = { data: phoneNumber };
-        const valueAddress = { data: address };
-        const valueJoinDate = { data: joinDate };
-        const valueAccessbility = { data: renderAccessNeeds(accessibility) };
-        const netId = email.split('@')[0];
-        const valueNameNetid = {
-          data:
-            <span>
-              <span style={{ fontWeight: 'bold' }}>
-                {`${firstName} ${lastName}`}
-              </span>
-              {` ${netId}`}
-            </span>,
-        };
-        const inputValues = [
-          valueNameNetid,
-          valuePhone,
-          valueAddress,
-          valueJoinDate,
-          valueAccessbility,
-        ];
-        const riderData = {
-          firstName,
-          lastName,
-          netID: netId,
-          phone: phoneNumber,
-          accessibility: renderAccessNeeds(accessibility),
-        };
-        const location = {
-          pathname: '/riders/rider',
-          state: riderData,
-          search: `?name=${`${firstName}_${lastName}`}`,
-        };
-        const goToDetail = () => {
-          history.push(location);
-        };
-        return (
-          <tr key={index} onClick={goToDetail} className={styles.tableRow}>
-            <TableRow values={inputValues} />
-          </tr>
-        );
-      });
-    }
+const RidersTable = () => {
+  const history = useHistory();
+  const { riders } = useRiders();
+  function renderTableData(allRiders: NewRider[]) {
+    return allRiders.map((rider, index) => {
+      const {
+        id,
+        firstName,
+        lastName,
+        phoneNumber,
+        address,
+        joinDate,
+        email,
+        accessibility,
+      } = rider;
+      const valuePhone = { data: phoneNumber };
+      const valueAddress = { data: address };
+      const valueJoinDate = { data: joinDate };
+      const valueAccessbility = { data: renderAccessNeeds(accessibility) };
+      const netId = email.split('@')[0];
+      const valueNameNetid = {
+        data:
+          <span>
+            <span style={{ fontWeight: 'bold' }}>
+              {`${firstName} ${lastName}`}
+            </span>
+            {` ${netId}`}
+          </span>,
+      };
+      const inputValues = [
+        valueNameNetid,
+        valuePhone,
+        valueAddress,
+        valueJoinDate,
+        valueAccessbility,
+      ];
+      const riderData = {
+        id,
+        firstName,
+        lastName,
+        netID: netId,
+        phone: phoneNumber,
+        accessibility: renderAccessNeeds(accessibility),
+      };
+      const location = {
+        pathname: '/riders/rider',
+        state: riderData,
+        search: `?name=${`${firstName}_${lastName}`}`,
+      };
+      const goToDetail = () => {
+        history.push(location);
+      };
+      return (
+        <tr key={index} onClick={goToDetail} className={styles.tableRow}>
+          <TableRow values={inputValues} />
+        </tr>
+      );
+    });
+  }
   return (
     <>
       <div>
