523413ef7912c267f1e762fdbe5b431bcdbdbf7b
Matthew Guo <mg2239@cornell.edu>
2021-04-01 18:39:40 -0400

    Update Students.tsx

--- frontend/src/pages/Dashboard/Students.tsx
+++ frontend/src/pages/Dashboard/Students.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-// import RidersTable from '../../components/UserTables/RidersTable';
 import StudentsTable from '../../components/UserTables/StudentsTable';
 import RiderModal from '../../components/Modal/RiderModal';
 import styles from './page.module.css';
