5cc8b87ac515123932344b36f5364f929a7febc4
Pinxuan Huang <pinxhuang@gmail.com>
2021-04-10 17:13:07 -0400

    Prefill working hours

--- frontend/src/components/EmployeeModal/WorkingHours.tsx
+++ frontend/src/components/EmployeeModal/WorkingHours.tsx
@@ -7,9 +7,15 @@ import { WeekProvider, useWeek } from './WeekContext';
 
 type AvailabilityInputProps = {
   index: number;
+  existingTimeRange?: string;
+  existingDayArray?: string[];
 }
 
-const AvailabilityInput = ({ index }: AvailabilityInputProps) => {
+const AvailabilityInput = ({ 
+  index, 
+  existingTimeRange, 
+  existingDayArray 
+}: AvailabilityInputProps) => {
   const {
     selectDay,
     deselectDay,
@@ -27,6 +33,7 @@ const AvailabilityInput = ({ index }: AvailabilityInputProps) => {
     Fri: 'F',
     Sat: 'S',
   };
+  const [existingTime, setExisingTime] = useState<string[]>();
   // All data for this AvailabilityInput instance will be saved in the form's
   // data in an array 'availability' at index 'index'
   const instance = `availability[${index}]`;
@@ -40,6 +47,18 @@ const AvailabilityInput = ({ index }: AvailabilityInputProps) => {
     }
   };
 
+  const prefillDays = (): void => {
+    existingDayArray?.forEach(day => {
+      selectDay(day, index);
+    });
+  };
+
+  useEffect(() => {
+    // Prefill time range and days once
+    prefillTimeRange();
+    prefillDays();
+  }, []);
+
   useEffect(() => {
     // Register day selector as custom form input
     register(`${instance}.days`, { required: true });
@@ -50,12 +69,51 @@ const AvailabilityInput = ({ index }: AvailabilityInputProps) => {
     setValue(`${instance}.days`, days);
   }, [instance, days, setValue]);
 
+  const formatTime = (time: string): string => {
+    if (time.includes('am')) {
+      // remove 'am' from time
+      let [timeAM] = time.split('am');
+      if (timeAM === '12') { //12am edge case
+        timeAM = '00'; //00:00 translates to 12am
+      } else if (timeAM.length === 1) { 
+        // pad time to be in the format HH:MM
+        timeAM = `0${timeAM}`;
+      }
+      return `${timeAM}:00`;
+    } else {
+      // remove 'pm' from time
+      let [timePM] = time.split('pm');
+      // Add 12 to the hour
+      let timePMInt = parseInt(timePM);
+      if (timePMInt !== 12) { //check for edge case, 12:00 translates to 12pm
+        timePMInt += 12;
+      }
+      timePM = timePMInt.toString();
+      // pad time to be in the format HH:mm
+      if (timePM.length === 1) {
+        timePM = `0${timePM}`;
+      }
+      return `${timePM}:00`
+    }
+  }
+
+  const prefillTimeRange = (): void => {
+    if (existingTimeRange) {
+      //extract start and end times
+      let [startTime, endTime] = existingTimeRange.split('-');
+      startTime = formatTime(startTime);
+      endTime = formatTime(endTime);
+      setExisingTime([startTime, endTime]);
+    }
+  };
+
   return (
     <div className={styles.availabilityInput}>
       <Input
         name={`${instance}.startTime`}
         type='time'
         className={styles.timeInput}
+        defaultValue={existingTime?.[0]}
         ref={register({ required: true })}
       />
       <p className={styles.toText}>to</p>
@@ -63,6 +121,7 @@ const AvailabilityInput = ({ index }: AvailabilityInputProps) => {
         name={`${instance}.endTime`}
         type='time'
         className={styles.timeInput}
+        defaultValue={existingTime?.[1]}
         ref={register({
           required: true,
           validate: (endTime) => {
@@ -93,22 +152,84 @@ type WorkingHoursProps = {
   existingAvailability?: string[][];
 }
 
+type availabilityPair = [string, string[]];
+
 const WorkingHours = ({ existingAvailability }: WorkingHoursProps) => {
-  const [numAvailability, setNumAvailability] = useState(1);
+  const [numAvailability, setNumAvailability] = useState(existingAvailability? 0:1);
+  const [availabilityArray, setAvailabilityArray] = useState<availabilityPair[]>([]);
 
   const addAvailabilityInput = () => setNumAvailability((n) => n + 1);
+
+  const getAvailabilityMap = (): Map<string, string[]> => {
+    let availabilityMap = new Map();
+    existingAvailability?.forEach(availability => {
+      const [day, timeRange] = availability;
+      const dayArray: string[] | undefined = availabilityMap.get(timeRange);
+      if (dayArray) {
+        dayArray.push(day);
+        availabilityMap.set(timeRange, dayArray);
+      } else { //create new array
+        availabilityMap.set(timeRange, [day]);
+      }
+    });
+    return availabilityMap;
+  };
+
+  const availabilityMapToArray = (map: Map<string, string[]>) => {
+    const timeRangeArray = Array.from(map.keys());
+    const dayArray = Array.from(map.values());
+    if (timeRangeArray.length === dayArray.length) {
+      // Each element of newAvailabilityArray is a tuple of (string, string[])
+      let newAvailabilityArray: availabilityPair[] = [];
+      for (let i = 0; i < timeRangeArray.length; i++) {
+        const pair: availabilityPair = [timeRangeArray[i], dayArray[i]]; //auto concat issue
+        newAvailabilityArray.push(pair);
+      }
+      setAvailabilityArray(newAvailabilityArray);
+    } else {
+      console.log("Error: Sizes of time and day arrays are not equal. \
+      This error should be impossible.");
+    }
+  }
+
+  useEffect(() => {
+    availabilityMapToArray(getAvailabilityMap());
+  }, []);
 
   return (
     <div className={styles.workingHours}>
       <p className={styles.workingHoursTitle}>Working Hours</p>
+      <p>Num Avail: { numAvailability }</p>
+      { existingAvailability ? 
       <WeekProvider>
+        {availabilityArray.map(([timeRange, dayArray], index) => (
+            <AvailabilityInput 
+            key={index} 
+            index={index} 
+            existingTimeRange={timeRange}
+            existingDayArray={dayArray}
+            />
+        ))}
+        {[...new Array(numAvailability)].map((_, index) => (
+            <AvailabilityInput 
+              key={index + availabilityArray.length} 
+              index={index + availabilityArray.length} 
+            />
+        ))}
+      </WeekProvider>
+      : <WeekProvider>
         {[...new Array(numAvailability)].map((_, index) => (
           <AvailabilityInput key={index} index={index} />
         ))}
       </WeekProvider>
+      }
       <p className={styles.addAvailabilityInput} onClick={addAvailabilityInput}>
         + Add more
-        </p>
+      </p>
+
+      <p className={styles.addAvailabilityInput} onClick={getAvailabilityMap}>
+      <br></br>getAvailabilityMap
+      </p>
     </div>
   );
 };
