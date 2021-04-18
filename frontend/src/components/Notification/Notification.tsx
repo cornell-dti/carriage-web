3094d7372983cf93be5a5470f855eb4c672674a8
Michael Ye <mly32@cornell.edu>
2021-04-18 17:40:15 -0400

    updated styling

--- frontend/src/components/Notification/Notification.tsx
+++ frontend/src/components/Notification/Notification.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import moment from 'moment';
 import Popup from 'reactjs-popup';
+import cn from 'classnames';
 import { useReq } from '../../context/req';
 import subscribeUser from './subscribeUser';
 import styles from './notification.module.css';
@@ -14,6 +16,7 @@ type Message = {
 
 const Notification = () => {
   const [availability, setAvailability] = useState(true); // TODO
+  const [newMessages, setNewMessages] = useState<Message[]>([]);
   const [messages, setMessages] = useState<Message[]>([]);
   const [notify, setNotify] = useState(false);
   const { withDefaults } = useReq();
@@ -35,21 +38,31 @@ const Notification = () => {
         body: event.data.body,
       };
 
-      setMessages([newMsg, ...messages]);
+      setNewMessages([newMsg, ...newMessages]);
       setNotify(true);
     });
   });
 
-  const onClick = () => {
-    console.log('clik');
-  };
+  const mapMessages = (msgs: Message[]) => msgs.map(({ time, title, body }, i) => (
+    <div key={i} className={styles.body}>
+      <div className={styles.user}>
+        <div className={styles.avatar}>
+          <span className={styles.initials}>T</span>
+        </div>
+      </div>
+      <div className={styles.msg}>
+        <p className={styles.date}>{moment(time).format('MMMM Do')}</p>
+        <p>{title} - {body}</p>
+      </div>
+      <div className={styles.link}>View</div>
+    </div>
+  ));
 
   return (
     <Popup
       trigger={
-        <button className={styles.bell} onClick={onClick}>
-          <span className={styles.span} />
-          <img src={notificationBell} alt="notification bell icon" />
+        <button className={styles.bell}>
+          <img src={notificationBell} alt='notification bell icon' />
           {availability && notify && (
             <img
               src={notificationBadge}
@@ -57,25 +70,31 @@ const Notification = () => {
               alt="notification badge icon"
             />
           )}
-          {notify || <span className={styles.span} />}
         </button>
       }
-      onOpen={() => setNotify(false)}
-      onClose={() => setNotify(false)}
+      onClose={() => {
+        setMessages([...newMessages, ...messages]);
+        setNewMessages([]);
+        setNotify(false);
+      }}
       position={['bottom right']}
       contentStyle={{
         margin: '10px',
+        width: '333px',
       }}
     >
       <div className={styles.content}>
-        <h6 className={styles.heading}>Message History</h6>
-        {messages.map(({ time, title, body }, i) => (
-          <div key={i} className={styles.body}>
-            <p className={styles.date}>{time.toLocaleDateString('en-US')}</p>
-            <p>{title}</p>
-            <p>{body}</p>
-          </div>
-        ))}
+        {newMessages.length === 0
+          || <h6 className={cn(styles.heading, styles.divider)}>
+            You have {newMessages.length} new message{newMessages.length === 1 || 's'}
+          </h6>
+        }
+        {newMessages.length === 0
+          || mapMessages(newMessages)}
+        {messages.length === 0
+          || <h6 className={styles.heading}>Message History</h6>}
+        {messages.length === 0
+          || mapMessages(messages)}
       </div>
     </Popup>
   );
