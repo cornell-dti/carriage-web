8a8a1fde8b74c4455711c9a3a87c3c34d0fb3013
Pinxuan Huang <pinxhuang@gmail.com>
2021-04-24 18:00:43 -0400

    Style Time header for UnscheduledTable

--- frontend/src/components/UserTables/RidesTable.tsx
+++ frontend/src/components/UserTables/RidesTable.tsx
@@ -14,22 +14,24 @@ type RidesTableProps = {
 const RidesTable = ({ rides, drivers, hasAssignButton }: RidesTableProps) => {
   const [openModal, setOpenModal] = useState(-1);
 
-  //0.65fr 1fr .65fr 0.65fr 0.65fr .5fr
-  const unscheduledDataColSizes = [1, 1, 1, 1, 1];
-  const unscheduledHeaderColSizes = [1, 1, 1, 1, 1];//[1, 0.8, 1, 1, 1, 1];
-  const unscheduledHeaders = ['Time', 'Passenger', 'Pickup Location', 'Dropoff Location', 'Needs'];
+  const unscheduledColSizes = [0.8, 1, 1, 0.8, 1];
+  const unscheduledHeaders = ['Passenger', 'Pickup Location', 'Dropoff Location', 'Needs'];
  
   const scheduledColSizes = [1, 1, 1, 1, 1, 1];
   const scheduledHeaders = ['Time', 'Pickup Location', 'Dropoff Location', 'Needs', 'Driver'];
   
   return (
     <Table>
-      <Row
-        header
-        colSizes={hasAssignButton ? unscheduledHeaderColSizes : scheduledColSizes}
-        data={hasAssignButton ? unscheduledHeaders.map((h) => ({ data: h })) 
-          : scheduledHeaders.map((h) => ({ data: h }))}
-      />
+      <div className={styles.ridesTableHeader}>
+        {hasAssignButton && <span className={styles.ridesEmptyHeader}></span>}
+        {hasAssignButton && <th className={styles.ridesTimeHeader}>Time</th>}
+        <Row
+          header
+          colSizes={hasAssignButton ? unscheduledColSizes : scheduledColSizes}
+          data={hasAssignButton ? unscheduledHeaders.map((h) => ({ data: h })) 
+            : scheduledHeaders.map((h) => ({ data: h }))}
+        />
+      </div>
       {rides.filter((r) => r.startLocation !== undefined
       && r.endLocation !== undefined).map((ride, index) => {
         const startTime = new Date(ride.startTime).toLocaleTimeString([], {
@@ -115,25 +117,15 @@ const RidesTable = ({ rides, drivers, hasAssignButton }: RidesTableProps) => {
                 <span className={styles.bold}>{startTime}</span> <br></br>
                 <span className={styles.gray}>-- {endTime}</span>
               </td>
-              <Row data={unscheduledRideData} colSizes={unscheduledDataColSizes} />
+              <Row data={unscheduledRideData} colSizes={unscheduledColSizes} />
             </tr>
           );
         };
 
         return (
-          <div className={styles.divRow}>
+          <div>
             {hasAssignButton ? unscheduledRow() : scheduledRow()}
           </div>
-          // <tr key={index}>
-          //   <td className={styles.cell}>{timeframe}</td>
-          //   <td className={styles.cell}>
-          //     <span className={styles.bold}>{startTime}</span> <br></br>
-          //     <span className={styles.gray}>-- {endTime}</span>
-          //   </td>
-          //   {hasAssignButton
-          //     ? <Row data={unscheduledRideData} colSizes={unscheduledDataColSizes} />
-          //     : <Row data={scheduledRideData} colSizes={scheduledDataColSizes} />}
-          // </tr>
         );
       })}
     </Table>
