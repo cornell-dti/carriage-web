e483319cc84563c92fca603135f5593026569e37
Matthew Guo <mg2239@cornell.edu>
2021-04-26 00:53:04 -0400

    style and formatting fixes

--- frontend/src/pages/Dashboard/Home.tsx
+++ frontend/src/pages/Dashboard/Home.tsx
@@ -1,35 +1,35 @@
-import React, { useState, useRef } from "react";
-import { CSVLink } from "react-csv";
-import moment from "moment";
-import RideModal from "../../components/RideModal/RideModal";
-import UnscheduledTable from "../../components/UserTables/UnscheduledTable";
-import Schedule from "../../components/Schedule/Schedule";
-import MiniCal from "../../components/MiniCal/MiniCal";
-import Notification from "../../components/Notification/Notification";
-import styles from "./page.module.css";
-import { useEmployees } from "../../context/EmployeesContext";
-import ExportButton from "../../components/ExportButton/ExportButton";
-import { useReq } from "../../context/req";
-import { useDate } from "../../context/date";
-import Collapsible from "../../components/Collapsible/Collapsible";
+import React, { useState, useRef } from 'react';
+import { CSVLink } from 'react-csv';
+import moment from 'moment';
+import RideModal from '../../components/RideModal/RideModal';
+import UnscheduledTable from '../../components/UserTables/UnscheduledTable';
+import Schedule from '../../components/Schedule/Schedule';
+import MiniCal from '../../components/MiniCal/MiniCal';
+import Notification from '../../components/Notification/Notification';
+import styles from './page.module.css';
+import { useEmployees } from '../../context/EmployeesContext';
+import ExportButton from '../../components/ExportButton/ExportButton';
+import { useReq } from '../../context/req';
+import { useDate } from '../../context/date';
+import Collapsible from '../../components/Collapsible/Collapsible';
 
 const Home = () => {
   const { drivers } = useEmployees();
   const { withDefaults } = useReq();
 
-  const [downloadData, setDownloadData] = useState<string>("");
+  const [downloadData, setDownloadData] = useState<string>('');
   const csvLink = useRef<
     CSVLink & HTMLAnchorElement & { link: HTMLAnchorElement }
   >(null);
   const { curDate } = useDate();
-  const today = moment(curDate).format("YYYY-MM-DD");
+  const today = moment(curDate).format('YYYY-MM-DD');
 
   const downloadCSV = () => {
     fetch(`/api/rides/download?date=${today}`, withDefaults())
       .then((res) => res.text())
       .then((data) => {
-        if (data === "") {
-          setDownloadData("Name,Pick Up,From,To,Drop Off,Needs,Driver");
+        if (data === '') {
+          setDownloadData('Name,Pick Up,From,To,Drop Off,Needs,Driver');
         } else {
           setDownloadData(data);
         }
@@ -58,7 +58,7 @@ const Home = () => {
       </div>
       <MiniCal />
       <Schedule />
-      <Collapsible title={"Unscheduled Rides"}>
+      <Collapsible title={'Unscheduled Rides'}>
         <UnscheduledTable drivers={drivers} />
       </Collapsible>
     </div>
