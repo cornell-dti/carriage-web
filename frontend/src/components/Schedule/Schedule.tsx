d06e56299f3199f99cab07a6df3e1e8bf94b21e6
Laura Sizemore <ls797@cornell.edu>
2021-04-26 23:01:55 -0400

    fix icon for real and update page title

--- frontend/src/components/Schedule/Schedule.tsx
+++ frontend/src/components/Schedule/Schedule.tsx
@@ -210,11 +210,11 @@ Rider: ${ride.rider.firstName} ${ride.rider.lastName}`,
         <div className={cn(styles.right, { [styles.long]: viewState })}>
           <div>
             <button className={styles.btn} onClick={goUp} disabled={okHr(0)}>
-              <div className={styles.uparrow}></div>
+              <span className={styles.uparrow} role="img" aria-label={"up arrow"}></span>
             </button>
             <span className={styles.pad}></span>
             <button className={styles.btn} onClick={goDown} disabled={okHr(22)}>
-              <div className={styles.downarrow}></div>
+              <span className={styles.downarrow} role="img" aria-label={"down arrow"}></span>
             </button>
           </div>
         </div>
