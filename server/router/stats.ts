d552badf53864d8033727f99a6c53a5fb926ff6d
Matthew Guo <mg2239@cornell.edu>
2021-04-28 23:00:33 -0400

    fixed response errors

--- server/router/stats.ts
+++ server/router/stats.ts
@@ -1,12 +1,10 @@
 import express, { Response } from 'express';
 import moment from 'moment-timezone';
 import { Condition } from 'dynamoose/dist/Condition';
-import { Stats } from '../models/stats';
-import { Ride, RideType } from '../models/ride';
+import { Stats, StatsType } from '../models/stats';
+import { Ride, RideType, Status } from '../models/ride';
 import * as db from './common';
 import { validateUser } from '../util';
-import { Driver, DriverType } from '../models/driver';
-import { RiderType } from '../models/rider';
 
 const router = express.Router();
 const tableName = 'Stats';
@@ -15,25 +13,20 @@ router.get('/', validateUser('User'), (req, res) => {
   const { query: { from, to } } = req;
 
   let date = moment.tz(from, 'America/New_York').format('YYYY-MM-DD');
-  let endDate = date;
   const dates = [date];
   if (to) {
     date = moment.tz(date, 'America/New_York').add(1, 'days').format('YYYY-MM-DD');
-    endDate = moment.tz(to, 'America/New_York').format('YYYY-MM-DD');
     while (date <= to) {
       dates.push(date);
       date = moment.tz(date, 'America/New_York').add(1, 'days').format('YYYY-MM-DD');
     }
   }
+
+  const statsAcc: StatsType[] = [];
+
   dates.forEach((currDate) => {
     const year = moment.tz(currDate, 'YYYY-MM-DD', 'America/New_York').format('YYYY');
     const monthDay = moment.tz(currDate, 'YYYY-MM-DD', 'America/New_York').format('MMDD');
-    const condition = new Condition()
-      .where('year')
-      .eq(year)
-      .and()
-      .where('monthDay')
-      .eq(monthDay);
 
     const dateMoment = moment.tz(currDate, 'America/New_York');
     // day = 12am to 5:00pm
@@ -43,30 +36,38 @@ router.get('/', validateUser('User'), (req, res) => {
     const nightStart = moment.tz(dayEnd, 'America/New_York').add(1, 'seconds').toISOString();
     const nightEnd = moment.tz(currDate as string, 'America/New_York').endOf('day').toISOString();
 
-    computeStats(res, condition, dayStart, dayEnd, nightStart, nightEnd, year,
-      monthDay, currDate, endDate);
+    computeStats(
+      res, statsAcc, dates.length, dayStart, dayEnd, nightStart, nightEnd, year, monthDay,
+    );
   });
 });
 
+function checkSend(
+  res: Response,
+  statsAcc: StatsType[],
+  numDays: number,
+) {
+  if (statsAcc.length === numDays) {
+    res.send(statsAcc);
+  }
+}
+
 function computeStats(
   res: Response,
-  condition: Condition,
+  statsAcc: StatsType[],
+  numDays: number,
   dayStart: string,
   dayEnd: string,
   nightStart: string,
   nightEnd: string,
   year: string,
   monthDay: string,
-  date: string,
-  endDate: string,
 ) {
-  db.scan(res, Stats, condition, (data: Document[]) => {
-    if (data.length) {
-      res.write(JSON.stringify(data));
-      if (date === endDate) {
-        res.end();
-      }
-    } else {
+  Stats.get({ year, monthDay }, (err, data) => {
+    if (data) {
+      statsAcc.push(data.toJSON() as StatsType);
+      checkSend(res, statsAcc, numDays);
+    } else if (err || !data) {
       const conditionRidesDate = new Condition()
         .where('startTime')
         .between(dayStart, nightEnd)
@@ -83,22 +84,22 @@ function computeStats(
 
         dataDay.forEach((rideData: RideType) => {
           const driverName = `${rideData.driver?.firstName} ${rideData.driver?.lastName}`;
-          if (driversStat[driverName]) {
-            driversStat[driverName] += 1;
-          } else {
-            driversStat[driverName] = 1;
-          }
-          if (rideData.status === 'no_show') {
+          if (rideData.status === Status.NO_SHOW) {
             if (rideData.startTime <= dayEnd) {
               dayNoShowStat += 1;
             } else {
               nightNoShowStat += 1;
             }
-          } else if (rideData.status === 'completed') {
+          } else if (rideData.status === Status.COMPLETED) {
             if (rideData.startTime <= dayEnd) {
               dayCountStat += 1;
             } else {
               nightCountStat += 1;
+            }
+            if (driversStat[driverName]) {
+              driversStat[driverName] += 1;
+            } else {
+              driversStat[driverName] = 1;
             }
           }
         });
@@ -113,12 +114,13 @@ function computeStats(
           nightCancel: 0,
           drivers: driversStat,
         });
-        Stats.create(stats);
-        res.write(JSON.stringify(stats));
-        if (date === endDate) {
-          res.end();
-        }
+        Stats.create(stats).then((doc) => {
+          statsAcc.push(doc.toJSON() as StatsType);
+          checkSend(res, statsAcc, numDays);
+        });
       });
+    } else {
+      console.log('oh no');
     }
   });
 }
