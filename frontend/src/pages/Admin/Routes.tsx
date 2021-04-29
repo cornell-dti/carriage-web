7c0feebb316a70451ec8ff7628a306b35534da0c
Matthew Guo <mg2239@cornell.edu>
2021-04-29 17:57:16 -0400

    create skip main hook, add to admin and rider routes

--- frontend/src/pages/Admin/Routes.tsx
+++ frontend/src/pages/Admin/Routes.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   Redirect,
-  useHistory,
 } from 'react-router-dom';
+import useSkipMain from '../../hooks/useSkipMain';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import Employees from './Employees';
 import Students from './Students';
@@ -18,66 +18,63 @@ import ExportPreview from '../../components/ExportPreview/ExportPreview';
 import DateContext from '../../context/date';
 
 const Routes = () => {
+  const skipRef = useSkipMain();
+  return (
+    <>
+      <div tabIndex={-1} ref={skipRef}></div>
+      <a className='skip-main' href='#main'>Skip to main content</a>
+      <Sidebar type="admin">
+        <Switch>
+          <Route
+            path="/home"
+            render={({ match: { url } }) => (
+              <>
+                <Route path={`${url}/`} component={Home} exact />
+                <Route path={`${url}/export`} component={ExportPreview} />
+              </>
+            )}
+          />
+          <Route
+            path="/employees"
+            render={({ match: { url } }) => (
+              <>
+                <Route path={`${url}/`} component={Employees} exact />
+                <Route path={`${url}/employee`} component={EmployeeDetail} />
+              </>
+            )}
+          />
+          <Route
+            path="/riders"
+            render={({ match: { url } }) => (
+              <>
+                <Route path={`${url}/`} component={Students} exact />
+                <Route path={`${url}/rider`} component={RiderDetail} />
+              </>
+            )}
+          />
+          <Route path="/locations" component={Locations} />
+          <Route path="/analytics" component={Analytics} />
+          <Route path="*">
+            <Redirect to="/home" />
+          </Route>
+        </Switch>
+      </Sidebar>
+    </>
+  );
+};
+
+const AdminRoutes = () => {
   const [curDate, setCurDate] = useState(new Date());
   const defaultVal = { curDate, setCurDate };
-  const history = useHistory();
-  const ref = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    const unlisten = history.listen(() => {
-      if (ref) {
-        ref.current?.focus();
-      }
-    });
-    return unlisten;
-  }, [history]);
 
   return (
     <DateContext.Provider value={defaultVal}>
       <Router basename="/admin">
-      <>
-      <div tabIndex={-1} ref={ref}></div>
-      <a className='skip-main' href='#main'>Skip to main content</a>
-        <Sidebar type="admin">
-          <Switch>
-            <Route
-              path="/home"
-              render={({ match: { url } }) => (
-                <>
-                  <Route path={`${url}/`} component={Home} exact />
-                  <Route path={`${url}/export`} component={ExportPreview} />
-                </>
-              )}
-            />
-            <Route
-              path="/employees"
-              render={({ match: { url } }) => (
-                <>
-                  <Route path={`${url}/`} component={Employees} exact />
-                  <Route path={`${url}/employee`} component={EmployeeDetail} />
-                </>
-              )}
-            />
-            <Route
-              path="/riders"
-              render={({ match: { url } }) => (
-                <>
-                  <Route path={`${url}/`} component={Students} exact />
-                  <Route path={`${url}/rider`} component={RiderDetail} />
-                </>
-              )}
-            />
-            <Route path="/locations" component={Locations} />
-            <Route path="/analytics" component={Analytics} />
-            <Route path="*">
-              <Redirect to="/home" />
-            </Route>
-          </Switch>
-        </Sidebar>
-      </>
+        <Routes />
       </Router>
     </DateContext.Provider >
 
   );
 };
 
-export default Routes;
+export default AdminRoutes;
--- frontend/src/pages/Rider/Routes.tsx
+++ frontend/src/pages/Rider/Routes.tsx
@@ -5,34 +5,46 @@ import {
   Switch,
   Redirect,
 } from 'react-router-dom';
+import useSkipMain from '../../hooks/useSkipMain';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import DateContext from '../../context/date';
 
-const Dashboard = () => {
+const Routes = () => {
+  const skipRef = useSkipMain();
+  return (
+    <>
+      <div tabIndex={-1} ref={skipRef}></div>
+      <a className='skip-main' href='#main'>Skip to main content</a>
+      <Sidebar type="rider">
+        <Switch>
+          <Route
+            path="/schedule"
+            component={() => null}
+          />
+          <Route
+            path="/settings"
+            component={() => null}
+          />
+          <Route path="*">
+            <Redirect to="/schedule" />
+          </Route>
+        </Switch>
+      </Sidebar>
+    </>
+  );
+};
+
+const RiderRoutes = () => {
   const [curDate, setCurDate] = useState(new Date());
   const defaultVal = { curDate, setCurDate };
 
   return (
     <DateContext.Provider value={defaultVal}>
       <Router basename="/rider">
-        <Sidebar type="rider">
-          <Switch>
-            <Route
-              path="/schedule"
-              component={() => null}
-            />
-            <Route
-              path="/settings"
-              component={() => null}
-            />
-            <Route path="*">
-              <Redirect to="/schedule" />
-            </Route>
-          </Switch>
-        </Sidebar>
+        <Routes />
       </Router>
     </DateContext.Provider>
   );
 };
 
-export default Dashboard;
+export default RiderRoutes;
