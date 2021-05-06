cddf5b39eac4b120078d96fee3fa55a6b5ea70d3
Laura Sizemore <ls797@cornell.edu>
2021-05-06 00:22:09 -0400

    create barebone setup for request ride modal

---
+++ frontend/src/components/RequestRideModal/RequestRideModal.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import { useReq } from '../../context/req';
+import Modal from '../Modal/Modal';
+import { Button } from '../FormElements/FormElements';
+import { ObjectType } from '../../types/index';
+import Toast from '../ConfirmationToast/ConfirmationToast';
+import styles from './requestridemodal.module.css';
+
+const RequestRideModal = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [showingToast, setToast] = useState(false);
+    const methods = useForm();
+
+    const openModal = () => {
+        setIsOpen(true);
+        setToast(false);
+      };
+    
+      const closeModal = () => {
+        methods.clearErrors();
+        setIsOpen(false);
+      };
+
+      return(
+          <>
+            {showingToast ? <Toast message='The employee has been added.' /> : null}
+            <Modal
+                title={"Request a Ride"}
+                isOpen={isOpen}
+                onClose={closeModal}
+            >
+            </Modal>
+          </>
+      );
+};
+export default RequestRideModal;
