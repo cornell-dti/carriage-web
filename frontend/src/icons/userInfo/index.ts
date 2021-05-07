ca29f3729e02e99b2b80e3295b43bd7e00cd44ab
bungaepyo <bungaepyo@gmail.com>
2021-05-06 21:43:13 -0400

    add mail icon

--- frontend/src/icons/userInfo/index.ts
+++ frontend/src/icons/userInfo/index.ts
@@ -3,3 +3,4 @@ export { default as phone } from './phone.svg';
 export { default as wheel } from './wheel.svg';
 export { default as capacity } from './capacity.svg';
 export { default as user } from './user.svg';
+export { default as mail } from './mail.svg';
