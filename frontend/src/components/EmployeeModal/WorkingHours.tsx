2b61601556231fda16049096089ef2cfc97d0e7c
Laura Sizemore <ls797@cornell.edu>
2021-05-08 16:52:59 -0400

    maybe fixed the bug

--- frontend/src/components/EmployeeModal/WorkingHours.tsx
+++ frontend/src/components/EmployeeModal/WorkingHours.tsx
@@ -10,12 +10,14 @@ type AvailabilityInputProps = {
   index: number;
   existingTimeRange?: string;
   existingDayArray?: string[];
+  hide: boolean;
 }
 
 const AvailabilityInput = ({
   index,
   existingTimeRange,
   existingDayArray,
+  hide
 }: AvailabilityInputProps) => {
   const {
     selectDay,
@@ -71,13 +73,13 @@ const AvailabilityInput = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // useEffect(() => {
-  //   // Register day selector as custom form input. 
-  //   //Not putting error message here since there is no default behavior to override
-  //   register(`${instance}.days`, { 
-  //     required: true, 
-  //     validate: () => {return days.length > 0}});
-  // }, [instance, register, days]);
+  useEffect(() => {
+    // Register day selector as custom form input. 
+    //Not putting error message here since there is no default behavior to override
+    register(`${instance}.days`, { 
+      required: !hide, 
+      validate: () => {return hide? true : days.length > 0}});
+  }, [instance, register, days, hide]);
 
   useEffect(() => {
     // When selected days changes, update days value
@@ -96,7 +98,7 @@ const AvailabilityInput = ({
           type='time'
           className={styles.timeInput}
           defaultValue={existingTime?.[0]}
-          ref={register({ required: true})}
+          ref={register({ required: !hide})}
         />
         {errors.availability && errors.availability[index] && 
           errors.availability[index].startTime &&  
@@ -111,10 +113,10 @@ const AvailabilityInput = ({
         className={styles.timeInput}
         defaultValue={existingTime?.[1]}
         ref={register({
-          required: true,
+          required: !hide,
           validate: (endTime) => {
             const startTime = getValues(`${instance}.startTime`);
-            return startTime < endTime;
+            return hide? true : startTime < endTime;
           },
         })}
       />
@@ -132,10 +134,6 @@ const AvailabilityInput = ({
               name={`${instance}.days`}
               type="button"
               value={label}
-              ref={register({
-                required: true,
-                validate: () => {return days.length > 0;}
-              })}
               className={cn(
                 styles.day,
                 { [styles.daySelected]: isDaySelectedByInstance(day, index) },
@@ -155,9 +153,10 @@ const AvailabilityInput = ({
 
 type WorkingHoursProps = {
   existingAvailability?: string[][];
+  hide: boolean;
 }
 
-const WorkingHours = ({ existingAvailability }: WorkingHoursProps) => {
+const WorkingHours = ({ existingAvailability, hide }: WorkingHoursProps) => {
   const [numAvailability, setNumAvailability] = useState(existingAvailability ? 0 : 1);
   const [availabilityArray, setAvailabilityArray] = useState<any[]>([]);
 
@@ -202,7 +201,7 @@ const WorkingHours = ({ existingAvailability }: WorkingHoursProps) => {
   }, []);
 
   return (
-    <div className={styles.workingHours}>
+    <div className={cn(styles.workingHours, { [styles.hidden]: hide })}>
       <p className={styles.workingHoursTitle}>Working Hours</p>
       <WeekProvider>
         {existingAvailability ? (
@@ -212,11 +211,12 @@ const WorkingHours = ({ existingAvailability }: WorkingHoursProps) => {
               index={index}
               existingTimeRange={timeRange}
               existingDayArray={dayArray}
+              hide={hide}
             />
           ))
         ) : (
           [...new Array(numAvailability)].map((_, index) => (
-            <AvailabilityInput key={index} index={index} />
+            <AvailabilityInput key={index} index={index} hide={hide} />
           ))
         )}
       </WeekProvider>
