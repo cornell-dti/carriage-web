1b2da429a76dc5c843920858eda52d0e54acecbf
Laura Sizemore <ls797@cornell.edu>
2021-05-10 11:47:18 -0400

    add buttons

--- frontend/src/components/RequestRideModal/RequestRideInfo.tsx
+++ frontend/src/components/RequestRideModal/RequestRideInfo.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import cn from 'classnames';
 import moment from 'moment';
 import {useFormContext } from 'react-hook-form';
 import styles from './requestridemodal.module.css';
-import { Label, Input } from '../FormElements/FormElements';
+import { Label, Input, SRLabel } from '../FormElements/FormElements';
 
 type RequestRideInfoProps = {
     startLocation?: string; 
@@ -14,10 +14,35 @@ type RequestRideInfoProps = {
     startDate?: string; 
     endDate?: string; 
 }
+const dayLabels = {
+    Sun: 'S',
+    Mon: 'M',
+    Tue: 'T',
+    Wed: 'W',
+    Thu: 'T',
+    Fri: 'F',
+    Sat: 'S',
+  };
+
+  type WeekType = {
+    [day: string]: boolean;
+  };
 
 const RequestRideInfo = () => {
     const { register, formState } = useFormContext();
   const {errors} = formState;
+  const [week, setWeek] = useState<WeekType>({
+    Sun: false,
+    Mon: false,
+    Tue: false,
+    Wed: false,
+    Thu: false,
+    Fri: false,
+    Sat: false,
+  });
+  const handleClick = (day: string) => {
+    setWeek((prev) => ({ ...prev, [day]: !week[day] }));
+  };
   return (
     <div className={styles.inputContainer}>
         <div className = {styles.col1}>
@@ -59,6 +84,26 @@ const RequestRideInfo = () => {
                 className={cn(styles.input)}
                 ref={register({ required: true })}
             />
+        </div>
+        <div className={styles.dayBox}>
+            <label id = {"repeatDays"}>Repeat On</label>
+          {Object.entries(dayLabels).map(([day, label]) => (
+             <div> 
+                <SRLabel id = {label}>{day}</SRLabel>
+                <Input
+                aria-labelledby={`${label} repeatDays`}
+                key={day}
+                name={"days"}
+                type="button"
+                value={label}
+                className={cn(
+                    styles.day,
+                    { [styles.daySelected]: week[day] },
+                )}
+                onClick={() => handleClick(day)}
+                />
+            </div>
+          ))}
         </div>
     </div>
   ); 
