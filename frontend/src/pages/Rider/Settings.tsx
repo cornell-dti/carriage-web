750786a84179dfd044ec865af55bb9913a7ebf7a
bungaepyo <bungaepyo@gmail.com>
2021-05-10 20:55:14 -0400

    adjust tab space & add notification

--- frontend/src/pages/Rider/Settings.tsx
+++ frontend/src/pages/Rider/Settings.tsx
@@ -3,39 +3,48 @@ import UserDetail, { UserContactInfo } from '../../components/UserDetail/UserDet
 import { phone, mail } from '../../icons/userInfo/index';
 import AuthContext from '../../context/auth';
 import { useReq } from '../../context/req';
+import settingsStyles from './settings.module.css';
+import styles from "../Admin/page.module.css"
+import Notification from '../../components/Notification/Notification';
 
 type RiderProfile = {
-    email: string;
-    firstName: string;
-    joinDate: string;
-    lastName: string;
-    phoneNumber: string;
-    pronouns: string;
+  email: string;
+  firstName: string;
+  joinDate: string;
+  lastName: string;
+  phoneNumber: string;
+  pronouns: string;
 }
 
 const Settings = () => {
-    const { id } = useContext(AuthContext);
-    const { withDefaults } = useReq();
-    const [rider, setRider] = useState<RiderProfile>();
-    const netId = rider?.email.split('@')[0] || ""
+  const { id } = useContext(AuthContext);
+  const { withDefaults } = useReq();
+  const [rider, setRider] = useState<RiderProfile>();
+  const netId = rider?.email.split('@')[0] || ""
 
-    useEffect(() => {
-        fetch(`/api/riders/${id}/profile`, withDefaults())
-            .then((res) => res.json())
-            .then((data) => {setRider(data)})
-    }, [withDefaults, id]);
+  useEffect(() => {
+      fetch(`/api/riders/${id}/profile`, withDefaults())
+        .then((res) => res.json())
+        .then((data) => {setRider(data)})
+  }, [withDefaults, id]);
 
-    return (
-        <>
-          <UserDetail
-            firstName={rider?.firstName || ""}
-            lastName={rider?.lastName || ""}
-            netId={netId}
-          >
-            <UserContactInfo icon={phone} alt="" text={rider?.phoneNumber || ""} />
-            <UserContactInfo icon={mail} alt="" text={rider?.email || ""} />
-          </UserDetail>
-        </>
+  return (
+      <>
+      <div className={styles.pageTitle}>
+        <h1 className={styles.header}>Settings</h1>
+        <div className={styles.rightSection}>
+          <Notification />
+        </div>
+      </div>
+      <UserDetail
+          firstName={rider?.firstName || ""}
+          lastName={rider?.lastName || ""}
+          netId={netId}
+      >
+          <UserContactInfo icon={phone} alt="" text={rider?.phoneNumber || ""} />
+          <UserContactInfo icon={mail} alt="" text={rider?.email || ""} />
+      </UserDetail>
+      </>
     );
 };
 export default Settings;
