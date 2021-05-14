f71f79cb0cb3c19434db0e8dfa9332b22071c157
Matthew Guo <mg2239@cornell.edu>
2021-05-14 00:07:08 -0400

    moved analytics specific components out of tab switcher

--- frontend/src/pages/Admin/Analytics.tsx
+++ frontend/src/pages/Admin/Analytics.tsx
@@ -3,10 +3,15 @@ import moment from 'moment';
 import { useReq } from '../../context/req';
 import AnalyticsTable, { TableData } from '../../components/AnalyticsTable/AnalyticsTable';
 import TabSwitcher from '../../components/TabSwitcher/TabSwitcher';
+import { useEmployees } from '../../context/EmployeesContext';
+import { Driver } from '../../types';
+import ExportButton from '../../components/ExportButton/ExportButton';
+import Notification from '../../components/Notification/Notification';
 
 const Analytics = () => {
   const [analyticsData, setData] = useState<TableData[]>([]);
   const { withDefaults } = useReq();
+  const { drivers } = useEmployees();
   const today = new Date();
   const to = moment(today).format('YYYY-MM-DD');
   const from = moment(today.setDate(today.getDate() - 30)).format('YYYY-MM-DD');
@@ -22,9 +27,29 @@ const Analytics = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const generateCols = () => {
+    const cols = 'Date,Daily Total,Daily Ride Count,Day No Shows,Day Cancels,Night Ride Count, Night No Shows, Night Cancels';
+    const finalCols = drivers.reduce((acc: string, curr: Driver) => (
+      `${acc},${curr.firstName} ${curr.lastName.substring(0, 1)}.`
+    ), cols);
+    return finalCols;
+  };
+
+  const renderRight = () => (
+    <>
+      <ExportButton
+        toastMsg={`${from} to ${to} data has been downloaded.`}
+        endpoint={`/api/stats/download?from=${from}&to=${to}`}
+        csvCols={generateCols()}
+        filename={`${from}_${to}_analytics.csv`}
+      />
+      <Notification />
+    </>
+  );
+
 
   return (
-    <TabSwitcher labels={['Ride Data', 'Driver Data']}>
+    <TabSwitcher labels={['Ride Data', 'Driver Data']} renderRight={renderRight}>
       <AnalyticsTable type="ride" data={analyticsData} refreshTable={refreshTable} />
       <AnalyticsTable type="driver" data={analyticsData} refreshTable={refreshTable} />
     </TabSwitcher>
