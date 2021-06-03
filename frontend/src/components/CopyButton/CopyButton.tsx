1dbdbd072ac5330d908302eddac50f41ec339f0e
Aaron Kang <aaronkang612@gmail.com>
2021-06-03 12:11:41 -1000

    add toast and style copy button

--- frontend/src/components/CopyButton/CopyButton.tsx
+++ frontend/src/components/CopyButton/CopyButton.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from '../FormElements/FormElements'
 import { useRiders } from '../../context/RidersContext';
+import Toast from '../ConfirmationToast/ConfirmationToast';
+import styles from './copyButton.module.css';
 
 const CopyButton = () => {
+  const [showingToast, setToast] = useState(false);
   const { riders } = useRiders();
   const getRidersEmail = () => {
     let concat = '';
@@ -10,11 +14,17 @@ const CopyButton = () => {
   };
 
   const handleClick = () => {
-    navigator.clipboard.writeText(getRidersEmail());
+    navigator.clipboard.writeText(getRidersEmail())
+      .then(() => { setToast(true); });
+    console.log(getRidersEmail());
   };
 
   return (
-    <button onClick={() => { handleClick(); }}>Copy Emails</button>
+    <>
+      {showingToast ? <Toast message={'Rider e-mails copied to clipboard.'} /> : null}
+      <Button onClick={() => { handleClick(); }} outline={true}
+        className={styles.copyButton}>Copy Emails</Button>
+    </>
   );
 };
 
