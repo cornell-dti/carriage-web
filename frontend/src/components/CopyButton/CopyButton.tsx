05efd4feec013bd185cd790308ae195ed2cb836e
Matthew Guo <mg2239@cornell.edu>
2021-06-09 21:00:29 -0400

    cleaned up implementation

--- frontend/src/components/CopyButton/CopyButton.tsx
+++ frontend/src/components/CopyButton/CopyButton.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button } from '../FormElements/FormElements'
+import { Button } from '../FormElements/FormElements';
 import { useRiders } from '../../context/RidersContext';
 import Toast from '../ConfirmationToast/ConfirmationToast';
 import styles from './copyButton.module.css';
@@ -7,23 +7,18 @@ import styles from './copyButton.module.css';
 const CopyButton = () => {
   const [showingToast, setToast] = useState(false);
   const { riders } = useRiders();
-  const getRidersEmail = () => {
-    let concat = '';
-    riders.forEach((rider) => { concat = `${concat + rider.email}, `; });
-    return concat;
-  };
+  const emails = riders.filter((r) => r.active).map((r) => r.email).join(',');
 
   const handleClick = () => {
-    navigator.clipboard.writeText(getRidersEmail())
-      .then(() => { setToast(true); });
-    console.log(getRidersEmail());
+    navigator.clipboard.writeText(emails).then(() => setToast(true));
   };
 
   return (
     <>
-      {showingToast ? <Toast message={'Rider e-mails copied to clipboard.'} /> : null}
-      <Button onClick={() => { handleClick(); }} outline={true}
-        className={styles.copyButton}>Copy Emails</Button>
+      {showingToast ? <Toast message={'Student e-mails copied to clipboard.'} /> : null}
+      <Button onClick={handleClick} outline className={styles.copyButton}>
+        Copy Emails
+      </Button>
     </>
   );
 };
