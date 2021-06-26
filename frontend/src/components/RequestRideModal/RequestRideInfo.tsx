68b133bf2d94f340dc2a938f80fe1101ef45109a
Matthew Guo <mg2239@cornell.edu>
2021-06-25 20:23:39 -0400

    format RequestRideInfo

--- frontend/src/components/RequestRideModal/RequestRideInfo.tsx
+++ frontend/src/components/RequestRideModal/RequestRideInfo.tsx
@@ -14,7 +14,7 @@ type RequestRideInfoProps = {
   showRepeatingCheckbox: boolean;
   showRepeatingInfo: boolean;
   modalType: RideModalType;
-}
+};
 
 const RequestRideInfo = ({
   ride,
@@ -33,7 +33,10 @@ const RequestRideInfo = ({
 
   useEffect(() => {
     const getExistingLocations = async () => {
-      const locationsData = await fetch('/api/locations?active=true', withDefaults())
+      const locationsData = await fetch(
+        '/api/locations?active=true',
+        withDefaults(),
+      )
         .then((res) => res.json())
         .then((data) => data.data);
       const sortedLocations = locationsData.sort((a: Location, b: Location) => {
@@ -42,7 +45,11 @@ const RequestRideInfo = ({
         return 0;
       });
       // Logic to prevent the other from being the default value
-      sortedLocations.push({ id: 'Other', name: 'Other', address: 'custom, do not use' });
+      sortedLocations.push({
+        id: 'Other',
+        name: 'Other',
+        address: 'custom, do not use',
+      });
       setLocations(sortedLocations);
     };
     getExistingLocations();
@@ -76,33 +83,53 @@ const RequestRideInfo = ({
 
   return (
     <div>
-      {((modalType === 'CREATE' && watchRepeating) || modalType === 'EDIT_REGULAR' || modalType === 'EDIT_SINGLE_RECURRING') && <Label htmlFor={'startDate'} className={styles.largeLabel}>Date</Label>}
+      {((modalType === 'CREATE' && watchRepeating)
+        || modalType === 'EDIT_REGULAR'
+        || modalType === 'EDIT_SINGLE_RECURRING')
+        && (
+          <Label htmlFor={'startDate'} className={styles.largeLabel}>
+            Date
+          </Label>
+        )}
       <div className={styles.box}>
-        {(modalType === 'EDIT_ALL_RECURRING') && <Label className={styles.boldLabel} htmlFor="startDate">Starts</Label>}
+        {modalType === 'EDIT_ALL_RECURRING' && (
+          <Label className={styles.boldLabel} htmlFor="startDate">
+            Starts
+          </Label>
+        )}
         <div className={styles.errorBox}>
           <Input
-            id='startDate'
-            name='startDate'
-            type='date'
+            id="startDate"
+            name="startDate"
+            type="date"
             className={cn(styles.input)}
             ref={register({ required: true })}
           />
-          {errors.startDate && <p className={styles.error}>
-            Please enter a valid start date</p>}
+          {errors.startDate && (
+            <p className={styles.error}>Please enter a valid start date</p>
+          )}
         </div>
-        {showRepeatingCheckbox && <Label className={styles.boldLabel}
-          htmlFor={'recurring'}>Repeating?</Label>}
-        {showRepeatingCheckbox && <Input
-          className={styles.recurring}
-          type="checkbox"
-          id="recurring"
-          name="recurring"
-          ref={register({ required: false })} />}
+        {showRepeatingCheckbox && (
+          <Label className={styles.boldLabel} htmlFor={'recurring'}>
+            Repeating?
+          </Label>
+        )}
+        {showRepeatingCheckbox && (
+          <Input
+            className={styles.recurring}
+            type="checkbox"
+            id="recurring"
+            name="recurring"
+            ref={register({ required: false })}
+          />
+        )}
       </div>
-      {showRepeatingInfo && watchRepeating
-        ? <div>
+      {showRepeatingInfo && watchRepeating ? (
+        <div>
           <div className={styles.box}>
-            <Label className={styles.boldLabel} id="repeats">Repeats</Label>
+            <Label className={styles.boldLabel} id="repeats">
+              Repeats
+            </Label>
             <Input
               className={styles.whenRepeat}
               name="whenRepeat"
@@ -110,8 +137,11 @@ const RequestRideInfo = ({
               ref={register({ required: watchRepeating })}
               type="radio"
               value="daily"
-              onChange={() => setCustom(false)} />
-            <Label className={styles.label} htmlFor="daily">Daily</Label>
+              onChange={() => setCustom(false)}
+            />
+            <Label className={styles.label} htmlFor="daily">
+              Daily
+            </Label>
             <input
               className={styles.whenRepeat}
               name="whenRepeat"
@@ -119,8 +149,11 @@ const RequestRideInfo = ({
               ref={register({ required: watchRepeating })}
               type="radio"
               value="weekly"
-              onChange={() => setCustom(false)} />
-            <Label className={styles.label} htmlFor="weekly">Weekly</Label>
+              onChange={() => setCustom(false)}
+            />
+            <Label className={styles.label} htmlFor="weekly">
+              Weekly
+            </Label>
             <input
               className={styles.whenRepeat}
               name="whenRepeat"
@@ -128,41 +161,67 @@ const RequestRideInfo = ({
               ref={register({ required: watchRepeating })}
               type="radio"
               value="custom"
-              onChange={() => setCustom(true)} />
-            <Label className={styles.label} htmlFor="custom">Custom</Label>
-            {errors.whenRepeat && <p className={styles.error}>
-              Please select a value</p>}
+              onChange={() => setCustom(true)}
+            />
+            <Label className={styles.label} htmlFor="custom">
+              Custom
+            </Label>
+            {errors.whenRepeat && (
+              <p className={styles.error}>Please select a value</p>
+            )}
           </div>
-          {custom && watchRepeating ? <CustomRepeatingRides ride={ride} /> : null}
-          <Label className={styles.boldLabel} htmlFor="endDate">Ends</Label>
-          <Input className={styles.input} type={'date'} name="endDate" id="endDate"
-            ref=
-            {register({
+          {custom && watchRepeating ? (
+            <CustomRepeatingRides ride={ride} />
+          ) : null}
+          <Label className={styles.boldLabel} htmlFor="endDate">
+            Ends
+          </Label>
+          <Input
+            className={styles.input}
+            type={'date'}
+            name="endDate"
+            id="endDate"
+            ref={register({
               required: getValues('repeating'),
               validate: (endDate: any) => {
                 const startDate = getValues('startDate');
                 return startDate < endDate;
               },
-            })} />
-          {errors.endDate && <p className={styles.error}>
-            Please select a valid end date</p>}
-        </div> : null}
-      <Label className={styles.largeLabel} id="pickupLabel">Pickup</Label>
+            })}
+          />
+          {errors.endDate && (
+            <p className={styles.error}>Please select a valid end date</p>
+          )}
+        </div>
+      ) : null}
+      <Label className={styles.largeLabel} id="pickupLabel">
+        Pickup
+      </Label>
       <div className={styles.box}>
         <div className={styles.errorBox}>
-          <Label className={styles.label} id="pickupLocation">Location</Label>
-          <select className={styles.input} name="startLocation" aria-labelledby="pickupLabel pickupLocations"
+          <Label className={styles.label} id="pickupLocation">
+            Location
+          </Label>
+          <select
+            className={styles.input}
+            name="startLocation"
+            aria-labelledby="pickupLabel pickupLocations"
             ref={register({ required: true })}
           >
-            {locations.map((location) => (<option key={location.id}
-              value={location.id}>{location.name}</option>))
-            }
+            {locations.map((location) => (
+              <option key={location.id} value={location.id}>
+                {location.name}
+              </option>
+            ))}
           </select>
-          {errors.startLocation && <p className={styles.error}>
-            Please select a valid location</p>}
+          {errors.startLocation && (
+            <p className={styles.error}>Please select a valid location</p>
+          )}
         </div>
         <div className={styles.errorBox}>
-          <Label className={styles.label} id="pickupTime">Time</Label>
+          <Label className={styles.label} id="pickupTime">
+            Time
+          </Label>
           <Input
             type="time"
             name="pickupTime"
@@ -170,22 +229,30 @@ const RequestRideInfo = ({
             aria-labelledby="pickupLabel pickupTime"
             ref={register({ required: true })}
           />
-          {errors.pickupTime && <p className={styles.error}>
-            Please choose a valid pickup time</p>}
+          {errors.pickupTime && (
+            <p className={styles.error}>Please choose a valid pickup time</p>
+          )}
         </div>
       </div>
-      {watchPickupCustom === 'Other'
-        ? <div className={styles.box}>
-          <Label className={styles.boldLabel} id="customPickup">Enter Pickup Location</Label>
-          <Input className={cn(styles.input, styles.flexGrow)}
+      {watchPickupCustom === 'Other' ? (
+        <div className={styles.box}>
+          <Label className={styles.boldLabel} id="customPickup">
+            Enter Pickup Location
+          </Label>
+          <Input
+            className={cn(styles.input, styles.flexGrow)}
             aria-labelledby="customPickup"
             name="customPickup"
             type="text"
             ref={register({
               required: watchPickupCustom === 'Other',
-            })} />
-          <Label className={styles.label} id="pickupCity">City</Label>
-          <Input className={styles.input}
+            })}
+          />
+          <Label className={styles.label} id="pickupCity">
+            City
+          </Label>
+          <Input
+            className={styles.input}
             aria-labelledby="customPickup pickupCity"
             name="pickupCity"
             type="text"
@@ -193,9 +260,13 @@ const RequestRideInfo = ({
             maxLength={32}
             ref={register({
               required: watchPickupCustom === 'Other',
-            })} />
-          <Label className={styles.label} id="pickupZip">Zip Code</Label>
-          <Input className={styles.input}
+            })}
+          />
+          <Label className={styles.label} id="pickupZip">
+            Zip Code
+          </Label>
+          <Input
+            className={styles.input}
             aria-labelledby="customPickup pickupZip"
             name="pickupZip"
             type="text"
@@ -204,30 +275,49 @@ const RequestRideInfo = ({
             maxLength={10}
             ref={register({
               required: watchDropoffCustom === 'Other',
-            })} />
+            })}
+          />
         </div>
-        : null}
-      <Label className={styles.largeLabel} id="dropoffLabel">Dropoff</Label>
+      ) : null}
+      <Label className={styles.largeLabel} id="dropoffLabel">
+        Dropoff
+      </Label>
       <div className={styles.box}>
         <div className={styles.errorBox}>
-          <Label className={styles.label} id="dropoffLocation">Location</Label>
-          <select className={styles.input} name="endLocation"
+          <Label className={styles.label} id="dropoffLocation">
+            Location
+          </Label>
+          <select
+            className={styles.input}
+            name="endLocation"
             aria-labelledby="dropoffLabel dropoffLocations"
             ref={register({
               required: true,
               validate: (endLocation: string) => {
                 const startLoc = getValues('startLocation');
-                return endLocation !== startLoc || (endLocation === 'Other' && startLoc === 'Other');
+                return (
+                  endLocation !== startLoc
+                  || (endLocation === 'Other' && startLoc === 'Other')
+                );
               },
-            })}>
-            {locations.map((location) => (<option key={location.id}
-              value={location.id}>{location.name}</option>))}
+            })}
+          >
+            {locations.map((location) => (
+              <option key={location.id} value={location.id}>
+                {location.name}
+              </option>
+            ))}
           </select>
-          {errors.endLocation && <p className={styles.error}>
-            Please select a valid dropoff location</p>}
+          {errors.endLocation && (
+            <p className={styles.error}>
+              Please select a valid dropoff location
+            </p>
+          )}
         </div>
         <div className={styles.errorBox}>
-          <Label className={styles.label} id="dropoffTime">Time</Label>
+          <Label className={styles.label} id="dropoffTime">
+            Time
+          </Label>
           <Input
             type="time"
             name="dropoffTime"
@@ -240,23 +330,32 @@ const RequestRideInfo = ({
                 const pickupTi = getValues('pickupTime');
                 return dropoffTi > pickupTi;
               },
-            })} />
-          {errors.dropoffTime && <p className={styles.error}>
-            Please choose a valid dropoff time</p>}
+            })}
+          />
+          {errors.dropoffTime && (
+            <p className={styles.error}>Please choose a valid dropoff time</p>
+          )}
         </div>
       </div>
-      {watchDropoffCustom === 'Other'
-        ? <div className={styles.box}>
-          <Label className={styles.boldLabel} id="customDropoff">Enter Dropoff Location</Label>
-          <Input className={cn(styles.input, styles.flexGrow)}
+      {watchDropoffCustom === 'Other' ? (
+        <div className={styles.box}>
+          <Label className={styles.boldLabel} id="customDropoff">
+            Enter Dropoff Location
+          </Label>
+          <Input
+            className={cn(styles.input, styles.flexGrow)}
             aria-labelledby="customDropoff"
             name="customDropoff"
             type="text"
             ref={register({
               required: watchDropoffCustom === 'Other',
-            })} />
-          <Label className={styles.label} id="dropoffCity">City</Label>
-          <Input className={styles.input}
+            })}
+          />
+          <Label className={styles.label} id="dropoffCity">
+            City
+          </Label>
+          <Input
+            className={styles.input}
             aria-labelledby="customDropoff dropoffCity"
             name="dropoffCity"
             type="text"
@@ -264,9 +363,13 @@ const RequestRideInfo = ({
             maxLength={32}
             ref={register({
               required: watchDropoffCustom === 'Other',
-            })} />
-          <Label className={styles.label} id="dropoffZip">Zip Code</Label>
-          <Input className={styles.input}
+            })}
+          />
+          <Label className={styles.label} id="dropoffZip">
+            Zip Code
+          </Label>
+          <Input
+            className={styles.input}
             aria-labelledby="customDropoff dropoffZip"
             name="dropoffZip"
             type="text"
@@ -275,9 +378,10 @@ const RequestRideInfo = ({
             maxLength={10}
             ref={register({
               required: watchDropoffCustom === 'Other',
-            })} />
+            })}
+          />
         </div>
-        : null}
+      ) : null}
     </div>
   );
 };
