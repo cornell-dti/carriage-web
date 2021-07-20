10c67e1637e990076078a2cc0a5987fcd7f7f44f
Matthew Guo <mg2239@cornell.edu>
2021-07-19 22:40:59 -0400

    implement getChangeType func, replace change w/ changeType

--- server/util/notification.ts
+++ server/util/notification.ts
@@ -8,7 +8,8 @@ import {
   UserType,
   PlatformType,
 } from '../models/subscription';
-import { RideType } from '../models/ride';
+import { RideType, Status, Type } from '../models/ride';
+import { Change } from './types';
 
 AWS.config.update({ ...config, region: 'us-east-1' });
 const sns = new AWS.SNS();
@@ -168,19 +169,37 @@ export const sendToUsers = (
   });
 });
 
+const getChangeType = (
+  change: Partial<RideType>,
+): Change | Status => {
+  const { status, late, type, driver } = change;
+  if (status) {
+    return status;
+  }
+  if (late) {
+    return Change.LATE;
+  }
+  if (type === Type.ACTIVE && driver) {
+    return Change.SCHEDULED;
+  }
+  return Change.EDITED;
+};
+
 export const notifyEdit = (
   updatedRide: RideType,
-  change: any,
+  body: Partial<RideType>,
   userType: UserType,
   userId: string,
+  change?: Change,
 ) => new Promise((resolve, reject) => {
   const riderId = updatedRide.rider.id;
   const hasDriver = updatedRide.driver;
   const driverId = hasDriver ? updatedRide.driver!.id : '';
+  const changeType = change || getChangeType(body);
 
   const info = JSON.stringify({
     ride: updatedRide,
-    change,
+    changeType,
     changedBy: { userType, userId },
   });
 
