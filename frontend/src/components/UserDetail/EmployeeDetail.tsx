28a551f4f433ebcdce3100bbb38f8857feeebc2e
20austinw <75195643+20austinw@users.noreply.github.com>
2021-10-21 19:32:00 -0400

    Formatted

--- frontend/src/components/UserDetail/EmployeeDetail.tsx
+++ frontend/src/components/UserDetail/EmployeeDetail.tsx
@@ -187,13 +187,13 @@ const EmployeeDetail = () => {
             alt="availability"
             text={avail === '' ? 'N/A' : avail}
           />
-          {employee.startDate &&
+          {employee.startDate && (
             <UserContactInfo
               icon={calender_dark}
               alt="join date"
               text={employee.startDate}
             />
-          }
+          )}
         </UserDetail>
         <EmployeeStatistics rideCount={rideCount} hours={workingHours} />
         <PastRides isStudent={false} rides={rides} />
