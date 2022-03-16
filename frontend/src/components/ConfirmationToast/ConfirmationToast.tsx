3859dc5011c7c286d85a2dd9f7bfd5e0d5106b56
michaeltorku <michaeltorku5@gmail.com>
2022-03-16 02:31:47 -0400

    error toast 40%

--- frontend/src/components/ConfirmationToast/ConfirmationToast.tsx
+++ frontend/src/components/ConfirmationToast/ConfirmationToast.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { check } from '../../icons/other/index';
+import { check, block } from '../../icons/other/index';
 import styles from './confirmationtoast.module.css';
 
 type toastProps = {
   message: string;
+  toastType?: string;
 };
 
-const Toast = ({ message }: toastProps) => {
-  return (
-    <div className={styles.toast}>
-      <img alt="toast check" src={check} />
+const Toast = ({ message, toastType }: toastProps) => {
+  return typeof toastType === 'undefined' || toastType == 'Success' ? (
+    <div className={`${styles.toast} ${styles.successToast}`}>
+      <img alt="toast blocked" src={check} />
+      <p className={styles.toasttext}>{message}</p>
+    </div>
+  ) : (
+    <div className={`${styles.toast} ${styles.errorToast}`}>
+      <img alt="toast check" src={block} />
       <p className={styles.toasttext}>{message}</p>
     </div>
   );
