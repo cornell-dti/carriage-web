6bbc97356d75ebcb435826215b8fc48301a85207
andrewschoi <89707989+andrewschoi@users.noreply.github.com>
2022-03-24 20:19:01 -0400

    bug fix

--- frontend/src/components/UserDetail/UserDetail.tsx
+++ frontend/src/components/UserDetail/UserDetail.tsx
@@ -127,9 +127,14 @@ const UserDetail = ({
           </div>
           <div className={styles.userEditContainer}>
             {rider && !isRider ? (
-              <Button onClick={toggleActive}>
-                {rider.active ? 'Deactivate' : 'Activate'}
-              </Button>
+              <label className={styles.switch}>
+                <input
+                  type="checkbox"
+                  onClick={toggleActive}
+                  checked={!rider.active}
+                />
+                <span className={styles.slider}></span>
+              </label>
             ) : null}
             {employee ? (
               <EmployeeModal
