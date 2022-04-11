7e389670141449033db70d219024a536cf8c7567
Matthew Guo <mguo22@gmail.com>
2022-04-11 16:59:11 -0400

    move refreshride call into createoreditridemodal

--- frontend/src/pages/Rider/Schedule.tsx
+++ frontend/src/pages/Rider/Schedule.tsx
@@ -1,12 +1,4 @@
-import React, {
-  useContext,
-  useState,
-  useEffect,
-  useCallback,
-  useRef,
-} from 'react';
-import { Ride } from '../../types';
-import { useReq } from '../../context/req';
+import React, { useContext } from 'react';
 import RiderScheduleTable from '../../components/UserTables/RiderScheduleTable';
 import Collapsible from '../../components/Collapsible/Collapsible';
 import AuthContext from '../../context/auth';
@@ -17,25 +9,16 @@ import styles from './page.module.css';
 import { useRides } from '../../../src/context/RidesContext';
 
 const Schedule = () => {
-  const componentMounted = useRef(true);
-  const [rides, setRides] = useState<Ride[]>();
-  const { upcomingRides, pastRides, refreshRides } = useRides();
-  const { id, user } = useContext(AuthContext);
-  const { withDefaults } = useReq();
-
-  useEffect(() => {
-    setRides([...upcomingRides, ...pastRides]);
-    return () => {
-      componentMounted.current = false;
-    };
-  }, [upcomingRides, pastRides, refreshRides]);
+  const { upcomingRides, pastRides } = useRides();
+  const { user } = useContext(AuthContext);
+  const rides = [...upcomingRides, ...pastRides];
 
   return (
     <main id="main">
       <div className={styles.pageTitle}>
         <h1 className={styles.header}>Hi {user?.firstName ?? ''}</h1>
         <div className={styles.rightSection}>
-          <RequestRideModal onSubmit={refreshRides} />
+          <RequestRideModal />
           <Notification />
         </div>
       </div>
