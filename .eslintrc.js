b5e345afe3f1f46ecfa41649dee0d7dc8551eb9e
Pratyush Sudhakar <pratyushsudhakar03@gmail.com>
2022-11-22 04:46:28 -0500

    run prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -49,11 +49,5 @@ module.exports = {
     react: {
       version: 'detect',
     },
-    "import/resolver": {
-      "node": {
-        "extensions": [".js", ".jsx", ".ts", ".tsx"]
-      },
-      "caseSensitive": false
-    }
   },
 };
