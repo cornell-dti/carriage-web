6cc8f7b72542f7b7e3bb82fe0bd4241695e9c137
andrew <asc269@cornell.edu>
2022-11-30 20:10:50 -0500

    Format code

--- frontend/src/components/Modal/RiderModalInfo.tsx
+++ frontend/src/components/Modal/RiderModalInfo.tsx
@@ -64,7 +64,7 @@ const RiderModalInfo = ({
   const isEditing = rider !== undefined;
   const isStudentEditing = isEditing && localUserType === 'Rider';
 
-  console.log()
+  console.log();
   return (
     <form onSubmit={handleSubmit(beforeSubmit)} className={styles.form}>
       <div className={cn(styles.inputContainer, styles.rideTime)}>
