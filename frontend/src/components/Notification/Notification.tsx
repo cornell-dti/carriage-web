34f52d1d4c16a1fbebe67d1ce1e400d037f2d33e
Pratyush Sudhakar <pratyushsudhakar03@gmail.com>
2023-03-08 01:39:54 -0500

    included a aria id generator package

--- frontend/src/components/Notification/Notification.tsx
+++ frontend/src/components/Notification/Notification.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useId } from '@react-aria/utils';
 import moment from 'moment';
 import Popup from 'reactjs-popup';
 import cn from 'classnames';
@@ -31,6 +32,7 @@ const Notification = () => {
   const [newMessages, setNewMessages] = useState<Message[]>([]);
   const [messages, setMessages] = useState<Message[]>([]);
   const [notify, setNotify] = useState(false);
+  const popupId = useId();
 
   useEffect(() => {
     navigator.serviceWorker.addEventListener('message', (event) => {
@@ -63,14 +65,14 @@ const Notification = () => {
     ));
 
   useEffect(() => {
-    const element = document.getElementById('popup');
+    const element = document.getElementById(popupId);
     element?.removeAttribute('aria-describedby');
   }, []);
 
   return (
     <Popup
       trigger={
-        <button id="popup" className={styles.bell}>
+        <button id={popupId} className={styles.bell}>
           <img src={notificationBell} alt="Notifications" />
           {notify && (
             <img
