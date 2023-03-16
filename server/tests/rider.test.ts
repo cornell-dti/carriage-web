55788196b792dce1e07a36f3ee36eda7366a7b55
harrisonchin2523 <harrisonchin2523@gmail.com>
2023-03-16 19:39:10 -0400

    reformmated tests and removed extraneous ones

--- server/tests/rider.test.ts
+++ server/tests/rider.test.ts
@@ -109,18 +109,16 @@ describe('Testing Functionality of Riders Endpoints', () => {
   after(clearDB);
 
   // testing retrieval of specific rider by ID
-  const generateGetRiderTest = async (authToken: string) => {
-    const res = await request(app)
-      .get('/api/riders/abc-10')
-      .auth(authToken, { type: 'bearer' })
-      .expect(200)
-      .expect('Content-Type', 'application/json; charset=utf-8');
-    expect(res.status).to.be.equal(200);
-    expect(res.body).to.have.property('data');
-    expect(res.body.data).to.deep.equal(testRiders[0]);
-  };
-
   describe('Testing Get Rider by Id', () => {
+    const generateGetRiderTest = async (authToken: string) => {
+      const res = await request(app)
+        .get('/api/riders/abc-10')
+        .auth(authToken, { type: 'bearer' })
+        .expect('Content-Type', 'application/json; charset=utf-8');
+      expect(res.status).to.be.equal(200);
+      expect(res.body).to.have.property('data');
+      expect(res.body.data).to.deep.equal(testRiders[0]);
+    };
     it('should return correct response for Admin account', async () =>
       await generateGetRiderTest(adminToken));
     it('should return correct response for Driver account', async () =>
@@ -134,34 +132,24 @@ describe('Testing Functionality of Riders Endpoints', () => {
   });
 
   // testing retrieval of all riders
-  const generateGetAllRidersTest = async (authToken: string) => {
-    const res = await request(app)
-      .get('/api/riders/')
-      .auth(authToken, { type: 'bearer' })
-      .expect(200)
-      .expect('Content-Type', 'application/json; charset=utf-8');
-    expect(res.status).to.be.equal(200);
-    expect(res.body).to.have.property('data');
-    expect(res.body.data.reverse()).to.deep.equal(testRiders);
-  };
-
-  // testing retrieval of all riders without admin privileges
-  const generateGetAllRidersErrorTest = async (authToken: string) => {
-    const res = await request(app)
-      .get('/api/riders/')
-      .auth(authToken, { type: 'bearer' })
-      .expect(400)
-      .expect('Content-Type', 'application/json; charset=utf-8');
-    expect(res.status).to.be.equal(400);
-  };
-
   describe('Testing fetching all riders', () => {
+    const generateGetAllRidersTest = async (authToken: string) => {
+      const res = await request(app)
+        .get('/api/riders/')
+        .auth(authToken, { type: 'bearer' })
+        .expect('Content-Type', 'application/json; charset=utf-8');
+      res.status === 200
+        ? (expect(res.status).to.be.equal(200),
+          expect(res.body).to.have.property('data'),
+          expect(res.body.data.reverse()).to.deep.equal(testRiders))
+        : expect(res.status).to.be.equal(400);
+    };
     it('should return correct response for Admin account', async () =>
       await generateGetAllRidersTest(adminToken));
     it('should fail with 400 given Driver account', async () =>
-      await generateGetAllRidersErrorTest(driverToken));
+      await generateGetAllRidersTest(driverToken));
     it('should fail with 400 given Rider account', async () =>
-      await generateGetAllRidersErrorTest(riderToken));
+      await generateGetAllRidersTest(riderToken));
     it('should fail with 400 given no authorization header', async () => {
       const res = await request(app).get('/api/riders/').expect(400);
       expect(res.body).have.property('err');
@@ -169,26 +157,24 @@ describe('Testing Functionality of Riders Endpoints', () => {
   });
 
   // testing retrieval of rider profile
-  const generateGetRiderProfileTest = async (authToken: string) => {
-    const riderProfile = {
-      email: 'test-email@test.com',
-      phoneNumber: '1234567890',
-      firstName: 'Test',
-      lastName: 'Testing',
-      pronouns: 'he/him/his',
-      joinDate: '2023-03-09',
-      endDate: '2024-03-09',
-    };
-    const res = await request(app)
-      .get('/api/riders/abc-10/profile')
-      .auth(authToken, { type: 'bearer' })
-      .expect(200)
-      .expect('Content-Type', 'application/json; charset=utf-8');
-    expect(res.status).to.be.equal(200);
-    expect(res.body).to.deep.equal(riderProfile);
-  };
-
   describe('GET a rider profile by ID', () => {
+    const generateGetRiderProfileTest = async (authToken: string) => {
+      const riderProfile = {
+        email: 'test-email@test.com',
+        phoneNumber: '1234567890',
+        firstName: 'Test',
+        lastName: 'Testing',
+        pronouns: 'he/him/his',
+        joinDate: '2023-03-09',
+        endDate: '2024-03-09',
+      };
+      const res = await request(app)
+        .get('/api/riders/abc-10/profile')
+        .auth(authToken, { type: 'bearer' })
+        .expect('Content-Type', 'application/json; charset=utf-8');
+      expect(res.status).to.be.equal(200);
+      expect(res.body).to.deep.equal(riderProfile);
+    };
     it('should return correct response for Admin account', async () =>
       await generateGetRiderProfileTest(adminToken));
     it('should return correct response for Driver account', async () =>
@@ -204,21 +190,20 @@ describe('Testing Functionality of Riders Endpoints', () => {
   });
 
   // testing retrieval of org info
-  const generateGetRiderOrgTest = async (authToken: string) => {
-    const riderOrganization = {
-      organization: testRiders[0].organization,
-      description: testRiders[0].description,
-    };
-    const res = await request(app)
-      .get('/api/riders/abc-10/organization')
-      .auth(authToken, { type: 'bearer' })
-      .expect(200)
-      .expect('Content-Type', 'application/json; charset=utf-8');
-    expect(res.status).to.be.equal(200);
-    expect(res.body).to.deep.equal(riderOrganization);
-  };
-
   describe('GET a rider organization by ID', () => {
+    const generateGetRiderOrgTest = async (authToken: string) => {
+      const riderOrganization = {
+        organization: testRiders[0].organization,
+        description: testRiders[0].description,
+      };
+      const res = await request(app)
+        .get('/api/riders/abc-10/organization')
+        .auth(authToken, { type: 'bearer' })
+        .expect('Content-Type', 'application/json; charset=utf-8');
+      expect(res.status).to.be.equal(200);
+      expect(res.body).to.deep.equal(riderOrganization);
+    };
+
     it('should return correct response for Admin account', async () =>
       await generateGetRiderOrgTest(adminToken));
     it('should return correct response for Driver account', async () =>
@@ -234,20 +219,20 @@ describe('Testing Functionality of Riders Endpoints', () => {
   });
 
   // testing retrieval of accessibility info
-  const generateGetRiderAccessibilityTest = async (authToken: string) => {
-    const riderAccessibility = {
-      accessibility: testRiders[0].accessibility,
-      description: testRiders[0].description,
-    };
-    const res = await request(app)
-      .get('/api/riders/abc-10/accessibility')
-      .auth(authToken, { type: 'bearer' })
-      .expect(200)
-      .expect('Content-Type', 'application/json; charset=utf-8');
-    expect(res.status).to.be.equal(200);
-    expect(res.body).to.deep.equal(riderAccessibility);
-  };
   describe("GET a rider's accessiblity by the rider's ID", () => {
+    const generateGetRiderAccessibilityTest = async (authToken: string) => {
+      const riderAccessibility = {
+        accessibility: testRiders[0].accessibility,
+        description: testRiders[0].description,
+      };
+      const res = await request(app)
+        .get('/api/riders/abc-10/accessibility')
+        .auth(authToken, { type: 'bearer' })
+        .expect(200)
+        .expect('Content-Type', 'application/json; charset=utf-8');
+      expect(res.status).to.be.equal(200);
+      expect(res.body).to.deep.equal(riderAccessibility);
+    };
     it('should return correct response for Admin account', async () =>
       await generateGetRiderAccessibilityTest(adminToken));
     it('should return correct response for Driver account', async () =>
@@ -262,65 +247,53 @@ describe('Testing Functionality of Riders Endpoints', () => {
     });
   });
 
-  const newRiderData = {
-    email: 'test-email2@test.com',
-    phoneNumber: '1234567892',
-    firstName: 'Test',
-    lastName: 'Testing2',
-    pronouns: 'he/him/his',
-    accessibility: 'Crutches',
-    description: '',
-    joinDate: '2023-03-09',
-    endDate: '2024-03-09',
-    address: '3 Colonial Ln, Ithaca, NY 14850',
-    favoriteLocations: ['Test-Location 1'],
-    organization: Organization.CULIFT,
-    photoLink: '',
-    active: true,
-  };
-
   // testing the addition of a new rider
-  const generateCreateNewRiderTest = async (authToken: string) => {
-    const res = await request(app)
-      .post('/api/riders/')
-      .send(newRiderData)
-      .auth(authToken, { type: 'bearer' })
-      .expect(200)
-      .expect('Content-Type', 'application/json; charset=utf-8');
-
-    const sentData = { ...res.body.data };
-    // this is randomly generated and cannot be tested for,
-    // but the accuracy of the rest of the data can be
-    delete sentData.id;
-    expect(sentData).to.deep.equal(newRiderData);
-    // retrieve this new rider
-    const res2 = await request(app)
-      .get(`/api/riders/${res.body.data.id}`)
-      .auth(authToken, { type: 'bearer' })
-      .expect(200)
-      .expect('content-type', 'application/json; charset=utf-8');
-    const retrievedData = res2.body.data;
-    delete retrievedData.id;
-    expect(retrievedData).to.deep.equal(newRiderData);
-  };
-
-  const generateUnauthorizedCreateNewRiderTest = async (authToken: string) => {
-    const res = await request(app)
-      .post('/api/riders/')
-      .send(newRiderData)
-      .auth(authToken, { type: 'bearer' })
-      .expect(400)
-      .expect('Content-Type', 'application/json; charset=utf-8');
-    expect(res.status).to.be.equal(400);
-  };
-
   describe('Create a new rider', () => {
+    const generateCreateNewRiderTest = async (authToken: string) => {
+      const newRiderData = {
+        email: 'test-email2@test.com',
+        phoneNumber: '1234567892',
+        firstName: 'Test',
+        lastName: 'Testing2',
+        pronouns: 'he/him/his',
+        accessibility: 'Crutches',
+        description: '',
+        joinDate: '2023-03-09',
+        endDate: '2024-03-09',
+        address: '3 Colonial Ln, Ithaca, NY 14850',
+        favoriteLocations: ['Test-Location 1'],
+        organization: Organization.CULIFT,
+        photoLink: '',
+        active: true,
+      };
+      const res = await request(app)
+        .post('/api/riders/')
+        .send(newRiderData)
+        .auth(authToken, { type: 'bearer' })
+        .expect('Content-Type', 'application/json; charset=utf-8');
+
+      res.status === 200
+        ? async () => {
+            const sentData = { ...res.body.data };
+            delete sentData.id;
+            expect(sentData).to.deep.equal(newRiderData);
+            const res2 = await request(app)
+              .get(`/api/riders/${res.body.data.id}`)
+              .auth(authToken, { type: 'bearer' })
+              .expect(200)
+              .expect('content-type', 'application/json; charset=utf-8');
+            const retrievedData = res2.body.data;
+            delete retrievedData.id;
+            expect(retrievedData).to.deep.equal(newRiderData);
+          }
+        : expect(res.status).to.be.equal(400);
+    };
     it('should return correct response for Admin account', async () =>
       await generateCreateNewRiderTest(adminToken));
     it('should fail with 400 given Driver account', async () =>
-      await generateUnauthorizedCreateNewRiderTest(driverToken));
+      await generateCreateNewRiderTest(driverToken));
     it('should fail with 400 given Rider account', async () =>
-      await generateUnauthorizedCreateNewRiderTest(riderToken));
+      await generateCreateNewRiderTest(riderToken));
     it('should fail with 400 given no authorization header', async () => {
       const res = await request(app).post('/api/riders/').expect(400);
       expect(res.body).have.property('err');
@@ -328,35 +301,30 @@ describe('Testing Functionality of Riders Endpoints', () => {
   });
 
   // testing the updating of info of a rider
-  const generateUpdateRiderTest = async (authToken: string) => {
-    const res = await request(app)
-      .put('/api/riders/abc-10')
-      .send({ firstName: 'NewName' })
-      .auth(authToken, { type: 'bearer' })
-      .expect(200)
-      .expect('Content-Type', 'application/json; charset=utf-8');
-    expect(res.body.data.firstName).to.be.equal('NewName');
-    // retrieve rider and see if there is a new name
-    const res2 = await request(app)
-      .get(`/api/riders/abc-10`)
-      .auth(authToken, { type: 'bearer' })
-      .expect(200)
-      .expect('content-type', 'application/json; charset=utf-8');
-    expect(res2.body.data.firstName).to.be.equal('NewName');
-  };
-  const generateErrorUpdateRiderTest = async (authToken: string) => {
-    const res = await request(app)
-      .put('/api/riders/abc-10')
-      .send({ firstName: 'NewName' })
-      .auth(authToken, { type: 'bearer' })
-      .expect(400)
-      .expect('Content-Type', 'application/json; charset=utf-8');
-  };
   describe('PUT new information for a rider by id abc-10', () => {
+    const generateUpdateRiderTest = async (authToken: string) => {
+      const res = await request(app)
+        .put('/api/riders/abc-10')
+        .send({ firstName: 'NewName' })
+        .auth(authToken, { type: 'bearer' })
+        .expect('Content-Type', 'application/json; charset=utf-8');
+      res.status === 200
+        ? async () => {
+            expect(res.body.data.firstName).to.be.equal('NewName');
+            // retrieve rider and see if there is a new name
+            const res2 = await request(app)
+              .get(`/api/riders/abc-10`)
+              .auth(authToken, { type: 'bearer' })
+              .expect(200)
+              .expect('content-type', 'application/json; charset=utf-8');
+            expect(res2.body.data.firstName).to.be.equal('NewName');
+          }
+        : expect(res.status).to.be.equal(400);
+    };
     it('should return correct response for Admin account', async () =>
       await generateUpdateRiderTest(adminToken));
     it('should fail with 400 given Driver account', async () =>
-      await generateErrorUpdateRiderTest(driverToken));
+      await generateUpdateRiderTest(driverToken));
     it('should return correct response for Rider account', async () =>
       await generateUpdateRiderTest(riderToken));
     it('should fail with 400 given no authorization header', async () => {
@@ -373,34 +341,29 @@ describe('Testing Functionality of Riders Endpoints', () => {
     const res = await request(app)
       .delete('/api/riders/abc-10')
       .auth(authToken, { type: 'bearer' })
-      .expect(200)
       .expect('Content-Type', 'application/json; charset=utf-8');
-    expect(res.body).to.deep.equal({ id: 'abc-10' });
+    res.status === 200
+      ? async () => {
+          expect(res.body).to.deep.equal({ id: 'abc-10' });
 
-    // try to fetch the deleted rider's information; should return 400 error
-    const res2 = await request(app)
-      .get('/api/riders/abc-10')
-      .auth(authToken, { type: 'bearer' })
-      .expect(400)
-      .expect('content-type', 'application/json; charset=utf-8');
-    expect(res2.body).have.property('err');
-  };
-
-  const generateErrorDeleteRiderTest = async (authToken: string) => {
-    const res = await request(app)
-      .delete('/api/riders/abc-10')
-      .auth(authToken, { type: 'bearer' })
-      .expect(400)
-      .expect('Content-Type', 'application/json; charset=utf-8');
+          // try to fetch the deleted rider's information; should return 400 error
+          const res2 = await request(app)
+            .get('/api/riders/abc-10')
+            .auth(authToken, { type: 'bearer' })
+            .expect(400)
+            .expect('content-type', 'application/json; charset=utf-8');
+          expect(res2.body).have.property('err');
+        }
+      : expect(res.status).to.be.equal(400);
   };
 
   describe('Delete a rider by id abc-10', () => {
     it('should return correct response for Admin account', async () =>
       await generateDeleteRiderTest(adminToken));
     it('should fail with 400 given Driver account', async () =>
-      await generateErrorDeleteRiderTest(driverToken));
+      await generateDeleteRiderTest(driverToken));
     it('should fail with 400 given Rider account', async () =>
-      await generateErrorDeleteRiderTest(riderToken));
+      await generateDeleteRiderTest(riderToken));
     it('should fail with 400 given no authorization header', async () => {
       const res = await request(app).delete('/api/riders/abc-10').expect(400);
       expect(res.body).have.property('err');
