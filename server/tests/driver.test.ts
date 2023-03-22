0fb957ad75bd023c9c8b59018d93522e5b59bd9e
harrisonchin2523 <harrisonchin2523@gmail.com>
2023-03-22 01:41:50 -0400

    drivers now store vehicle ids

--- server/tests/driver.test.ts
+++ server/tests/driver.test.ts
@@ -7,7 +7,10 @@ import { clearDB, populateDB } from './utils/db';
 import { Vehicle, VehicleType } from '../src/models/vehicle';
 import { DriverType } from '../src/models/driver';
 import { AdminType } from '../src/models/admin';
-import { Organization, RiderType } from '../src/models/rider';
+import { Organization, Rider, RiderType } from '../src/models/rider';
+import { Ride, RideType, Status, Type } from '../src/models/ride';
+import { Location, LocationType, Tag } from '../src/models/location';
+import moment from 'moment';
 
 const testAdmin: Omit<AdminType, 'id'> = {
   firstName: 'Test-Admin',
@@ -33,7 +36,25 @@ const testRider: Omit<RiderType, 'id'> = {
   active: true,
 };
 
-const testVehicles: VehicleType[] | string = [
+const testStatRider: RiderType = {
+  id: 'abc-10',
+  email: 'test-email@test.com',
+  phoneNumber: '1234567890',
+  firstName: 'Test',
+  lastName: 'Testing',
+  pronouns: 'he/him/his',
+  accessibility: 'Crutches',
+  description: '',
+  joinDate: '2023-03-09',
+  endDate: '2024-03-09',
+  address: '36 Colonial Ln, Ithaca, NY 14850',
+  favoriteLocations: ['1'],
+  organization: Organization.REDRUNNER,
+  photoLink: '',
+  active: true,
+};
+
+const testVehicles: VehicleType[] = [
   {
     id: 'vehicle1',
     name: 'Toyota',
@@ -57,7 +78,7 @@ const testDrivers: DriverType[] = [
     phoneNumber: '1234567890',
     firstName: 'Test',
     lastName: 'Testing',
-    vehicle: testVehicles[0],
+    vehicle: testVehicles[0].id,
     startDate: '2023-03-09',
     availability: {
       Mon: {
@@ -82,7 +103,7 @@ const testDrivers: DriverType[] = [
       },
     },
     photoLink: '',
-    admin: true,
+    admin: false,
   },
   {
     id: 'driver1',
@@ -90,7 +111,7 @@ const testDrivers: DriverType[] = [
     phoneNumber: '1234567891',
     firstName: 'Test',
     lastName: 'Testing1',
-    vehicle: testVehicles[1],
+    vehicle: testVehicles[1].id,
     startDate: '2023-03-10',
     availability: {
       Mon: {
@@ -119,6 +140,110 @@ const testDrivers: DriverType[] = [
   },
 ];
 
+const testLocations: LocationType[] = [
+  {
+    id: '1',
+    name: 'Test-Location 1',
+    address: '123 Test Location',
+    tag: Tag.WEST,
+    info: 'Test Info 1',
+  },
+  {
+    id: '2',
+    name: 'Test-Location 2',
+    address: '321 Test Drive',
+    tag: Tag.NORTH,
+    info: 'Test Info 2',
+  },
+];
+
+const testRides: RideType[] = [
+  {
+    id: 'test-ride0',
+    type: Type.PAST,
+    status: Status.COMPLETED,
+    late: false,
+    startLocation: {
+      id: '1',
+      name: 'Test-Location 1',
+      address: '123 Test Location',
+      tag: Tag.WEST,
+    },
+    endLocation: {
+      id: '2',
+      name: 'Test-Location 2',
+      address: '321 Test Drive',
+      tag: Tag.NORTH,
+    },
+    startTime: moment().subtract({ days: 1, hours: 6 }).toISOString(),
+    endTime: moment().subtract({ days: 1, hours: 5 }).toISOString(),
+    rider: testStatRider,
+    driver: testDrivers[0],
+    recurring: false,
+    recurringDays: undefined,
+    endDate: undefined,
+    deleted: undefined,
+    edits: undefined,
+    parentRide: undefined,
+  },
+  {
+    id: 'test-ride1',
+    type: Type.PAST,
+    status: Status.COMPLETED,
+    late: false,
+    startLocation: {
+      id: '2',
+      name: 'Test-Location 2',
+      address: '321 Test Drive',
+      tag: Tag.NORTH,
+    },
+    endLocation: {
+      id: '1',
+      name: 'Test-Location 1',
+      address: '123 Test Location',
+      tag: Tag.WEST,
+    },
+    startTime: moment().subtract({ days: 2, hours: 6 }).toISOString(),
+    endTime: moment().subtract({ days: 2, hours: 5 }).toISOString(),
+    rider: testStatRider,
+    driver: testDrivers[0],
+    recurring: false,
+    recurringDays: undefined,
+    endDate: undefined,
+    deleted: undefined,
+    edits: undefined,
+    parentRide: undefined,
+  },
+  {
+    id: 'test-ride2',
+    type: Type.PAST,
+    status: Status.NO_SHOW,
+    late: false,
+    startLocation: {
+      id: '2',
+      name: 'Test-Location 2',
+      address: '321 Test Drive',
+      tag: Tag.NORTH,
+    },
+    endLocation: {
+      id: '1',
+      name: 'Test-Location 1',
+      address: '123 Test Location',
+      tag: Tag.WEST,
+    },
+    startTime: moment().subtract({ days: 2, hours: 4 }).toISOString(),
+    endTime: moment().subtract({ days: 2, hours: 3 }).toISOString(),
+    rider: testStatRider,
+    driver: testDrivers[0],
+    recurring: false,
+    recurringDays: undefined,
+    endDate: undefined,
+    deleted: undefined,
+    edits: undefined,
+    parentRide: undefined,
+  },
+];
+
 describe('Testing Functionality of Drivers Endpoints', () => {
   let adminToken: string;
   let riderToken: string;
@@ -126,12 +251,21 @@ describe('Testing Functionality of Drivers Endpoints', () => {
   before(async () => {
     adminToken = await authorize('Admin', testAdmin);
     riderToken = await authorize('Rider', testRider);
+    await Promise.all(
+      testVehicles.map((vehicle) => populateDB(Vehicle, vehicle))
+    );
     driverToken = await authorize('Driver', testDrivers[0]);
     await Promise.all(
       testDrivers.slice(1).map((driver) => populateDB(Driver, driver))
     );
+    await populateDB(Rider, testStatRider);
     await Promise.all(
-      testVehicles.map((vehicle) => populateDB(Vehicle, vehicle))
+      testLocations.map((location) => populateDB(Location, location))
+    );
+    await Promise.all(
+      testRides.map(
+        (ride) => (console.log(ride.endTime), populateDB(Ride, ride))
+      )
     );
   });
 
@@ -140,6 +274,10 @@ describe('Testing Functionality of Drivers Endpoints', () => {
   // testing retrieval of specific driver
   describe('Testing get driver by Id', () => {
     const generateGetDriverTest = async (authToken: string) => {
+      // testDrivers is stored with drivers where the vehicle field contains the vehicle ID
+      // we need the vehicle field to contain the entire vehicle object
+      const { vehicle, ...driverObj } = testDrivers[0];
+      const driverWithVehicleObj = { ...driverObj, vehicle: testVehicles[0] };
       const res = await request(app)
         .get('/api/drivers/driver0')
         .auth(authToken, { type: 'bearer' })
@@ -147,7 +285,7 @@ describe('Testing Functionality of Drivers Endpoints', () => {
         .expect('Content-Type', 'application/json; charset=utf-8');
       expect(res.status).to.be.equal(200);
       expect(res.body).to.have.property('data');
-      expect(res.body.data).to.deep.equal(testDrivers[0]);
+      expect(res.body.data).to.deep.equal(driverWithVehicleObj);
     };
     it('should return correct response for Admin account', async () =>
       await generateGetDriverTest(adminToken));
@@ -162,19 +300,30 @@ describe('Testing Functionality of Drivers Endpoints', () => {
   });
 
   // testing retrieval of all drivers
-  const generateAllDriversTest = async (authToken: string) => {
-    const res = await request(app)
-      .get('/api/drivers/')
-      .auth(authToken, { type: 'bearer' })
-      .expect('Content-Type', 'application/json; charset=utf-8');
-    res.status === 200
-      ? (expect(res.status).to.be.equal(200),
-        expect(res.body).to.have.property('data'),
-        expect(res.body.data).to.deep.equal(testDrivers))
-      : expect(res.status).to.be.equal(400);
-  };
-
   describe('GET all drivers', () => {
+    const generateAllDriversTest = async (authToken: string) => {
+      // testDrivers is stored with drivers where the vehicle field contains the vehicle ID
+      // we need the vehicle field to contain the entire vehicle object
+      const driversWithVehicles = testDrivers.map((driver) => {
+        const { vehicle, ...driverObj } = driver;
+        const driverWithVehicleObj = {
+          ...driverObj,
+          vehicle: testVehicles.filter(
+            (testVehicle) => testVehicle.id == vehicle
+          )[0],
+        };
+        return driverWithVehicleObj;
+      });
+      const res = await request(app)
+        .get('/api/drivers/')
+        .auth(authToken, { type: 'bearer' })
+        .expect('Content-Type', 'application/json; charset=utf-8');
+      res.status === 200
+        ? (expect(res.status).to.be.equal(200),
+          expect(res.body).to.have.property('data'),
+          expect(res.body.data).to.deep.equal(driversWithVehicles))
+        : expect(res.status).to.be.equal(400);
+    };
     it('should return correct response for Admin account', async () =>
       await generateAllDriversTest(adminToken));
     it('should fail with 400 given Driver account', async () =>
@@ -190,33 +339,12 @@ describe('Testing Functionality of Drivers Endpoints', () => {
   // testing retrieval of driver profile
   describe('GET a driver profile by ID', () => {
     const generateGetDriverProfileTest = async (authToken: string) => {
-      const driverProfile = {
-        email: 'drivertest-email@test.com',
-        phoneNumber: '1234567890',
-        firstName: 'Test',
-        lastName: 'Testing',
-        availability: {
-          Mon: {
-            startTime: '8:00',
-            endTime: '12:00',
-          },
-          Tue: {
-            startTime: '8:00',
-            endTime: '12:00',
-          },
-          Wed: {
-            startTime: '8:00',
-            endTime: '12:00',
-          },
-          Thu: {
-            startTime: '8:00',
-            endTime: '12:00',
-          },
-          Fri: {
-            startTime: '8:00',
-            endTime: '12:00',
-          },
-        },
+      // testDrivers is stored with drivers where the vehicle field contains the vehicle ID
+      // we need the vehicle field to contain the entire vehicle object
+      const { id, admin, photoLink, startDate, vehicle, ...driverProfile } =
+        testDrivers[0];
+      const driverProfileWithVehicle = {
+        ...driverProfile,
         vehicle: testVehicles[0],
       };
       const res = await request(app)
@@ -225,7 +353,7 @@ describe('Testing Functionality of Drivers Endpoints', () => {
         .expect(200)
         .expect('Content-Type', 'application/json; charset=utf-8');
       expect(res.status).to.be.equal(200);
-      expect(res.body).to.deep.equal(driverProfile);
+      expect(res.body).to.deep.equal(driverProfileWithVehicle);
     };
     it('should return correct response for Admin account', async () =>
       await generateGetDriverProfileTest(adminToken));
@@ -241,16 +369,48 @@ describe('Testing Functionality of Drivers Endpoints', () => {
     });
   });
 
-  // testing the addition of a new driver
+  // **********************************
+  // currently returning 0 rides for some reason, is something wrong with how rides are being populated?
+  //************************************ */
+  // testing the retrieval of a driver's stats
+  describe("GET a driver's stats", () => {
+    const driverStats = {
+      rides: 2,
+      workingHours: 20,
+    };
+    const generateGetDriverStatsTest = async (authToken: string) => {
+      const res = await request(app)
+        .get('/api/drivers/driver0/stats')
+        .auth(authToken, { type: 'bearer' })
+        .expect('Content-Type', 'application/json; charset=utf-8');
+      res.status === 200
+        ? expect(res.body).to.deep.equal(driverStats)
+        : expect(res.status).to.be.equal(400);
+    };
+    // completed 2 of 3 rides (one no show) and completed 20 hours of work
+    it('should return correct response for Admin account', async () =>
+      await generateGetDriverStatsTest(adminToken));
+    it('should fail with 400 given Driver account', async () =>
+      await generateGetDriverStatsTest(driverToken));
+    it('should fail with 400 given Rider account', async () =>
+      await generateGetDriverStatsTest(riderToken));
+    it('should fail with 400 given no authorization header', async () => {
+      const res = await request(app)
+        .get('/api/drivers/driver0/stats')
+        .expect(400);
+      expect(res.body).have.property('err');
+    });
+  });
 
+  // testing the addition of a new driver
   describe('Create a new driver', () => {
     const generateCreateDriverTest = async (authToken: string) => {
-      const newDriverData = {
+      const newDriverData: Omit<DriverType, 'id'> = {
         email: 'drivertest3-email@test.com',
         phoneNumber: '1234567890',
         firstName: 'Test',
         lastName: 'Testing',
-        vehicle: testVehicles[2],
+        vehicle: testVehicles[2].id,
         startDate: '2023-03-09',
         availability: {
           Mon: {
@@ -277,6 +437,8 @@ describe('Testing Functionality of Drivers Endpoints', () => {
         photoLink: '',
         admin: true,
       };
+      const { vehicle, ...driverObj } = newDriverData;
+      const driverWithVehicleObj = { ...driverObj, vehicle: testVehicles[2] };
       const res = await request(app)
         .post('/api/drivers/')
         .send(newDriverData)
@@ -288,7 +450,7 @@ describe('Testing Functionality of Drivers Endpoints', () => {
             // this is randomly generated and cannot be tested for,
             // but the accuracy of the rest of the data can be
             delete sentData.id;
-            expect(sentData).to.deep.equal(newDriverData);
+            expect(sentData).to.deep.equal(driverWithVehicleObj);
             // retrieve this new rider
             const res2 = await request(app)
               .get(`/api/drivers/${res.body.data.id}`)
@@ -297,7 +459,7 @@ describe('Testing Functionality of Drivers Endpoints', () => {
               .expect('content-type', 'application/json; charset=utf-8');
             const retrievedData = res2.body.data;
             delete retrievedData.id;
-            expect(retrievedData).to.deep.equal(newDriverData);
+            expect(retrievedData).to.deep.equal(driverWithVehicleObj);
           }
         : expect(res.status).to.be.equal(400);
     };
