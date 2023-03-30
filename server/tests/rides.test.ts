cde7d685b65ff7ab5b82bb88b8e944809174e145
Rohit Valiveti <rvv6@cornell.edu>
2023-03-29 22:35:25 -0400

    Post Request with existing locations as start/end locations

--- server/tests/rides.test.ts
+++ server/tests/rides.test.ts
@@ -49,6 +49,7 @@ const testRideResponse2 = {
   late: false,
 };
 
+// Updated data for put request
 const putReq1 = {
   startLocation: '678 Test Lane',
   endLocation: '12 NewTest Road',
@@ -91,6 +92,55 @@ const testRideReq4 = {
   endTime: '2022-01-31T23:55:00.000Z',
 };
 
+// Start Location POST Requst Data
+const startLoc = {
+  name: 'Toni Morrison Hall',
+  address: '18 Sisson Pl',
+  tag: 'north',
+  info: 'In front of dining hall',
+};
+
+// End Location POST Request Data
+const endLoc = {
+  name: '7/11',
+  address: '409 College Ave',
+  tag: 'ctown',
+  info: 'In front of building',
+};
+
+// POST reqeust data with existing locations
+const testRideRequest5 = {
+  startLocation: '',
+  endLocation: '',
+  startTime: '2022-01-31T23:50:00.000Z',
+  endTime: '2022-01-31T23:55:00.000Z',
+};
+
+// GET response for testRideRequest5 after adding locations
+const testRideResponse5 = {
+  startLocation: {
+    name: 'Toni Morrison Hall',
+    address: '18 Sisson Pl',
+    tag: 'north',
+    info: 'In front of dining hall',
+    id: '',
+  },
+  endLocation: {
+    name: '7/11',
+    address: '409 College Ave',
+    tag: 'ctown',
+    info: 'In front of building',
+    id: '',
+  },
+  recurring: false,
+  startTime: '2022-01-31T23:50:00.000Z',
+  endTime: '2022-01-31T23:55:00.000Z',
+  id: '',
+  type: 'unscheduled',
+  status: 'not_started',
+  late: false,
+};
+
 describe('Rides Tests', () => {
   let adminToken: string;
 
@@ -131,7 +181,7 @@ describe('Rides Tests', () => {
     });
   });
 
-  describe('DELETE Request with an id that does not exist in db', () => {
+  describe('DELETE /api/rides with an id that does not exist in db', () => {
     it('Should return an error that the id is not found in the rides db', async () => {
       const randomId = 'nonexistant-id';
       const delRes = await request(app)
@@ -156,7 +206,7 @@ describe('Rides Tests', () => {
     });
   });
 
-  describe('POST & GET with recurring days', () => {
+  describe('POST & GET /api/rides with recurring days', () => {
     it('Creates a recurring ride and', async () => {
       const postRes = await request(app)
         .post('/api/rides')
@@ -194,6 +244,47 @@ describe('Rides Tests', () => {
         .expect(200)
         .expect('Content-Type', 'application/json; charset=utf-8');
       expect(putRes.body).to.deep.equal(testRideResponse3);
+    });
+  });
+
+  describe('POST /api/rides with exiting locations', () => {
+    it('Creates start and end location and uses these to create a new ride', async () => {
+      const startLocRes = await request(app)
+        .post('/api/locations')
+        .send(startLoc)
+        .set('Authorization', `Bearer ${adminToken}`)
+        .expect(200)
+        .expect('Content-Type', 'application/json; charset=utf-8');
+      const startLocResId = startLocRes.body.data['id'];
+      const endLocRes = await request(app)
+        .post('/api/locations')
+        .send(endLoc)
+        .set('Authorization', `Bearer ${adminToken}`)
+        .expect(200)
+        .expect('Content-Type', 'application/json; charset=utf-8');
+      const endLocResId = endLocRes.body.data['id'];
+
+      testRideRequest5.startLocation = startLocResId;
+      testRideRequest5.endLocation = endLocResId;
+
+      const postRes = await request(app)
+        .post('/api/rides')
+        .send(testRideRequest5)
+        .set('Accept', 'application/json')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${adminToken}`)
+        .expect(200);
+      const rideId = postRes.body['id'];
+      testRideResponse5.id = rideId;
+      testRideResponse5.startLocation.id = startLocResId;
+      testRideResponse5.endLocation.id = endLocResId;
+
+      const getRes = await request(app)
+        .get(`/api/rides/${rideId}`)
+        .set('Authorization', `Bearer ${adminToken}`)
+        .expect(200)
+        .expect('Content-Type', 'application/json; charset=utf-8');
+      expect(getRes.body.data).to.deep.equal(testRideResponse5);
     });
   });
 });
