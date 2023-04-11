6ded11d84aa14540fd12d146b6c020220b3ab198
harrisonchin2523 <harrisonchin2523@gmail.com>
2023-04-11 19:04:07 -0400

    current progress

--- frontend/src/components/EmployeeCards/EmployeeCards.tsx
+++ frontend/src/components/EmployeeCards/EmployeeCards.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import Card, { CardInfo } from '../Card/Card';
 import styles from './employeecards.module.css';
 import { clock, phone, wheel, user } from '../../icons/userInfo/index';
 import { Employee, Admin } from '../../types';
 import { useEmployees } from '../../context/EmployeesContext';
 import formatAvailability from '../../util/employee';
+import { AdminType } from '../../../../server/src/models/admin';
+import { DriverType } from '../../../../server/src/models/driver';
+import { time } from 'console';
 
 const formatPhone = (phoneNumber: string) => {
   const areaCode = phoneNumber.substring(0, 3);
@@ -18,7 +21,59 @@ type EmployeeCardProps = {
   id: string;
   employee: Employee;
 };
+type EmployeeDetailProps = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  netId: string;
+  phone: string;
+  availability?: string[][];
+  admin?: boolean;
+  photoLink?: string;
+  startDate?: string;
+};
+//Convert DriverType to EmployeeType
+const DriverToEmployees = (drivers: DriverType[]): EmployeeDetailProps[] => {
+  return drivers.map((driver) => ({
+    id: driver.id,
+    firstName: driver.firstName,
+    lastName: driver.lastName,
+    availability: formatAvailability(driver.availability)!,
+    netId: driver.email.split('@')[0],
+    phone: driver.phoneNumber,
+    admin: driver.admin,
+    photoLink: driver.photoLink,
+    startDate: driver.startDate,
+  }));
+};
 
+//Convert AdminType to EmployeeType
+const AdminToEmployees = (admins: AdminType[]): EmployeeDetailProps[] => {
+  return admins.map((admin) => ({
+    id: admin.id,
+    firstName: admin.firstName,
+    lastName: admin.lastName,
+    netId: admin.email.split('@')[0],
+    phone: admin.phoneNumber,
+    admin: true,
+    photoLink: admin.photoLink,
+  }));
+};
+
+const findEmployee = (
+  drivers: DriverType[],
+  admins: AdminType[],
+  employeeId: string
+): EmployeeDetailProps => {
+  const employee = DriverToEmployees(drivers).find(
+    (employee) => employee.id === employeeId
+  );
+  if (!employee)
+    return AdminToEmployees(admins).find(
+      (employee) => employee.id === employeeId
+    )!;
+  return employee;
+};
 const EmployeeCard = ({
   id,
   employee: {
@@ -32,9 +87,35 @@ const EmployeeCard = ({
     startDate,
   },
 }: EmployeeCardProps) => {
+  const { drivers, admins } = useEmployees();
+  const [employee, setEmployee] = useState(findEmployee(drivers, admins, id));
   const netId = email.split('@')[0];
   const fmtPhone = formatPhone(phoneNumber);
-  const fmtAvailability = formatAvailability(availability);
+  let prevTimeRange =
+    employee.availability && employee.availability[0]
+      ? employee.availability[0][1]
+      : '';
+
+  const availToString = (acc: string, [day, timeRange]: string[]) =>
+    // need to account for only one time range
+    timeRange !== prevTimeRange
+      ? acc.charAt(acc.length - 1) === '•'
+        ? `${acc + day}: ${timeRange} •`
+        : `${acc}: ${prevTimeRange} • ${day}: ${timeRange} `
+      : acc !== ''
+      ? ((prevTimeRange = timeRange), `${acc + '/' + day}`)
+      : ((prevTimeRange = timeRange), `${acc + day}`);
+
+  const parsedAvail = employee.availability
+    ? employee.availability.reduce(availToString, '')
+    : '';
+  let avail = parsedAvail.substring(0, parsedAvail.length);
+  avail =
+    avail === ''
+      ? 'N/A'
+      : avail.charAt(avail.length - 1) !== 'm'
+      ? avail + ': ' + prevTimeRange
+      : avail;
 
   const isAdmin = !availability;
   const isBoth = !isAdmin && admin; // admin and driver
@@ -50,7 +131,7 @@ const EmployeeCard = ({
     lastName,
     netId,
     phone: fmtPhone,
-    availability: fmtAvailability,
+    availability: avail,
     admin,
     photoLink,
     startDate,
@@ -77,17 +158,7 @@ const EmployeeCard = ({
         </CardInfo>
 
         <CardInfo icon={clock} alt="clock">
-          <div>
-            {fmtAvailability ? (
-              fmtAvailability.map(([day, timeRange]) => (
-                <p key={day}>
-                  <span className={styles.dayText}>{day}:</span> {timeRange}
-                </p>
-              ))
-            ) : (
-              <p>N/A</p>
-            )}
-          </div>
+          {avail ? <p className={styles.timeText}>{avail}</p> : <p>N/A</p>}
         </CardInfo>
 
         <CardInfo
