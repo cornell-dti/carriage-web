3fd902cd695e66d38ab918a2a8aeb3c6bf9f963a
Stuti <sg2265@cornell.edu>
2023-05-12 23:28:12 -0400

    Started modifying login based on new design

--- frontend/src/pages/Landing/Footer.tsx
+++ frontend/src/pages/Landing/Footer.tsx
@@ -5,8 +5,43 @@ import { dti_logo } from '../../icons/other';
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <div className={styles.blackTop}></div>
-      <div className={styles.white}></div>
+      <div className={styles.blackTop}>
+        <p
+          style={{
+            color: 'white',
+            fontSize: 25,
+            textAlign: 'left',
+            paddingLeft: 100,
+            paddingTop: 30,
+            paddingBottom: 30,
+          }}
+        >
+          Students schedule, <br />
+          edit, and cancel rides <br />
+          on rider web <br />
+          and mobile.
+        </p>
+        <img src="frontend/src/pages/Landing/Group525.png" />
+      </div>
+
+      <div className={styles.white}>
+        <p
+          style={{
+            color: 'black',
+            fontSize: 25,
+            textAlign: 'right',
+            paddingRight: 200,
+            paddingTop: 30,
+            paddingBottom: 30,
+          }}
+        >
+          Administrators add <br />
+          students and <br />
+          employees and <br />
+          dispatch rides.
+        </p>
+        <img src="frontend/src/pages/Landing/Group525.png" />
+      </div>
       <div className={styles.blackBottom}>
         <div
           className={styles.dti_container}
