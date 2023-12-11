fc343cb83ace8961b8cdf2c051cb9fe9ebe6ce01
Stuti <sg2265@cornell.edu>
2023-12-10 22:49:39 -0500

    added documentation for location modal

--- frontend/src/components/LocationModal/LocationModal.tsx
+++ frontend/src/components/LocationModal/LocationModal.tsx
@@ -48,39 +48,54 @@ const isAddress = (address: string) => {
   return true;
 };
 
+/**
+ * `LocationModal` component for adding or editing a location.
+ */
+
 const LocationModal = ({
   existingLocation,
   onAddLocation,
   onEditLocation,
 }: LocationModalProps) => {
+  // State to manage the modal open/closed status
   const [isOpen, setIsOpen] = useState(false);
+  // Toast context for displaying success messages
   const { showToast } = useToast();
+  // Form handling with react-hook-form
   const { register, handleSubmit, errors, reset } = useForm();
+  // Destructure specific error types for clarity
   const { name, address, info } = errors;
+  // State for managing form data
   const [formData, setFormData] = useState({
     name: existingLocation?.name || '',
     address: existingLocation?.address || '',
     info: existingLocation?.info || '',
     tag: existingLocation?.tag || 'default', // Replace 'default' with your desired default tag
   });
-
+  // Modal title based on whether editing or adding a location
   const modalTitle = existingLocation ? 'Edit Location' : 'Add a Location';
+  // Button text for submit based on whether editing or adding a location
   const submitButtonText = existingLocation ? 'Save' : 'Save';
 
+  // Open the modal
   const openModal = () => {
     setIsOpen(true);
   };
 
+  // Close the modal
   const closeModal = () => setIsOpen(false);
 
   const onSubmit = async (data: ObjectType) => {
+    // Define the API endpoint based on whether editing or adding a location
     const url = existingLocation
       ? `/api/locations/${existingLocation!.id}`
       : '/api/locations';
+    // Determine the HTTP method for the API request
     const method = existingLocation ? axios.put : axios.post;
-
+    // Make the API request and obtain the new location data
     const newLocation = await method(url, data).then((res) => res.data);
 
+    // Trigger the appropriate callback and display a success message
     if (!existingLocation && onAddLocation) {
       onAddLocation(newLocation.data);
       showToast('Location has been added.', ToastStatus.SUCCESS);
@@ -88,17 +103,23 @@ const LocationModal = ({
       onEditLocation(newLocation);
       showToast('Location has been updated.', ToastStatus.SUCCESS);
     }
+    // Close the modal after submission
     closeModal();
   };
 
-  // Clear function to reset form data and errors
+  /**
+   * Clear form data and reset the form.
+   */
   const clearForm = () => {
+    // Reset the local state for form data
     setFormData({
       name: '',
       address: '',
       info: '',
       tag: 'default',
     });
+
+    // Reset the form using react-hook-form's reset function
     reset();
   };
 
