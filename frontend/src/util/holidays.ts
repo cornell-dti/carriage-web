9cceb7425351abc99080812acd6265bef28fa31e
Selena Liu <sl2663@cornell.edu>
2024-11-13 19:17:11 -0500

    delete console.log

--- frontend/src/util/holidays.ts
+++ frontend/src/util/holidays.ts
@@ -50,7 +50,6 @@ function parseHoliday(holiday: Holidays): holiday {
   const [start, end] = dates.split(' to ');
   const startDate = new Date(`${start}T00:00:00`);
   const endDate = new Date(`${end}T23:59:59`);
-  console.log(name, startDate, endDate);
   return { name, startDate, endDate };
 }
 
