69a9c9ef020d8c1d24bb25c0099b4365e1b3212e
Dwain-Anderson <dka36@cornell.edu>
2024-12-02 14:14:47 -0500

    Removed dubug methods

--- server/src/app.ts
+++ server/src/app.ts
@@ -36,17 +36,14 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '500kb' }));
 app.use(express.urlencoded({ extended: false }));
-app.use((req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-  next();
-});
 
-app.use((req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-  console.log('Headers:', req.headers);
-  console.log('Body:', req.body); // Requires body-parser or express.json()
-  next();
-});
+// Very useful API Debugger Method. 
+//app.use((req, res, next) => {
+  //console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  //console.log('Headers:', req.headers);
+  //console.log('Body:', req.body); // Requires body-parser or express.json()
+  //next();
+//});
 
 app.use('/api/riders', rider);
 app.use('/api/drivers', driver);
@@ -58,9 +55,6 @@ app.use('/api/auth', auth);
 app.use('/api/upload', upload);
 app.use('/api/notification', notification);
 app.use('/api/stats', stats);
-// Increase JSON payload limit
-app.use(express.json({ limit: '5000mb' }));
-app.use(express.urlencoded({ extended: true, limit: '5000mb' }));
 app.get('/api/health-check', (_, response) => response.status(200).send('OK'));
 
 // Serve static files from frontend
