2eaa85751c9d35aa4312a06708879e4c0f4cf19b
Dwain-Anderson <dka36@cornell.edu>
2025-03-22 23:01:30 -0400

    Favorites model, needs Postman testing

---
+++ server/src/models/favorite.ts
@@ -0,0 +1,11 @@
+import dynamoose from 'dynamoose';
+import defaultModelConfig from '../util/modelConfig';
+
+const schema = new dynamoose.Schema({
+  // we store references because storing the ride duplicates the data, comp key for efficiency and better normalization
+  userId: { type: String, required: true, hashKey: true },
+  rideId: { type: String, required: true, rangeKey: true },
+  favoritedAt: { type: Date, default: () => new Date() },
+});
+
+export const Favorite = dynamoose.model('Favorites', schema, defaultModelConfig);
