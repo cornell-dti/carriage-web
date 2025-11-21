3ac9f028fd5ad7495506159f80bb8577594ba784
yottayocta <chubbean.rq@gmail.com>
2025-11-21 01:08:47 -0500

    task: stub for new schedule card

--- frontend/src/pages/Rider/Schedule.tsx
+++ frontend/src/pages/Rider/Schedule.tsx
@@ -1,6 +1,6 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { FC, useContext, useEffect, useState } from 'react';
 import { Button } from '@mui/material';
-import { Ride } from '../../types';
+import { Ride, SchedulingState, Status } from '../../types';
 import AuthContext from '../../context/auth';
 import NoRidesView from '../../components/NoRidesView/NoRidesView';
 import Notification from '../../components/Notification/Notification';
@@ -15,6 +15,7 @@ import { Driver, DayOfWeek } from 'types';
 import { useLocations } from '../../context/LocationsContext';
 import { useRides } from '../../context/RidesContext';
 import axios from '../../util/axios';
+import { Place, SubdirectoryArrowRight, WatchLater } from '@mui/icons-material';
 
 // Favorite ride type
 interface FavoriteRide {
@@ -30,6 +31,261 @@ interface FavoriteRide {
   };
   preferredTime: string;
 }
+
+interface RideCardProps {
+  ride: Ride;
+  handleEdit: (rideToEdit: Ride) => any;
+}
+
+const RideCardComponent: FC<RideCardProps> = ({ ride, handleEdit }) => {
+  const [expanded, setExpanded] = useState<boolean>(false);
+
+  console.log(ride);
+  return (
+    <button
+      onClick={() => setExpanded(!expanded)}
+      style={{
+        width: '100%',
+        height: 'min-content',
+        maxWidth: '32rem',
+        background: 'white',
+        borderRadius: '0.25rem',
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '1.5rem',
+        border: '#dddddd 1px solid',
+        cursor: 'pointer',
+      }}
+    >
+      {/* ride status, check if scheduled. If not, display card indicating */}
+      {ride.schedulingState === SchedulingState.UNSCHEDULED ? (
+        <div
+          style={{
+            width: '100%',
+            height: 'min-content',
+            borderRadius: '0.1275rem',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            color: '#707070',
+          }}
+        >
+          <p>Requested</p>
+        </div>
+      ) : ride.status === Status.CANCELLED ? (
+        <div
+          style={{
+            width: '100%',
+            height: 'min-content',
+            borderRadius: '0.1275rem',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#fff0f0',
+            color: '#c10000',
+          }}
+        >
+          <p>Canceled</p>
+        </div>
+      ) : (
+        <></>
+      )}
+      <div
+        style={{
+          width: '100%',
+          height: 'min-content',
+          display: 'flex',
+          flexDirection: expanded ? 'column' : 'row',
+          justifyContent: expanded ? 'normal' : 'space-between',
+          gap: expanded ? 'auto' : '1.5rem',
+          textWrap: 'nowrap',
+        }}
+      >
+        {/* time-related */}
+        <div
+          style={{
+            width: 'min-content',
+            height: 'min-content',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '0.5rem',
+          }}
+        >
+          <span
+            style={{
+              display: 'flex',
+              gap: '0.5rem',
+              alignItems: 'center',
+            }}
+          >
+            <WatchLater></WatchLater>
+            <p>{ride.startTime}</p>
+          </span>
+          <span
+            style={{
+              display: 'flex',
+              gap: '0.5rem',
+              alignItems: 'center',
+              color: '#707070',
+            }}
+          >
+            <SubdirectoryArrowRight></SubdirectoryArrowRight>
+            <p>{ride.endTime}</p>
+          </span>
+        </div>
+        {/* location-related */}
+        {!expanded && (
+          <div
+            style={{
+              width: '8rem',
+              height: 'min-content',
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '0.5rem',
+            }}
+          >
+            <span
+              style={{
+                display: 'flex',
+                gap: '0.5rem',
+                alignItems: 'center',
+              }}
+            >
+              <Place></Place>
+              <p>{ride.startLocation.shortName}</p>
+            </span>
+            <span
+              style={{
+                display: 'flex',
+                gap: '0.5rem',
+                alignItems: 'center',
+                color: '#707070',
+              }}
+            >
+              <SubdirectoryArrowRight></SubdirectoryArrowRight>
+              <p>{ride.endLocation.shortName}</p>
+            </span>
+          </div>
+        )}
+        {/* expanded location view */}
+        {expanded && (
+          <div
+            style={{
+              width: '100%',
+              height: 'min-content',
+              display: 'flex',
+              gap: '0.5rem',
+            }}
+          >
+            {/* start location + map */}
+            <div
+              style={{
+                width: 'flex-1',
+                height: 'min-content',
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '0.5rem',
+              }}
+            >
+              <span
+                style={{
+                  display: 'flex',
+                  gap: '0.5rem',
+                  alignItems: 'center',
+                }}
+              >
+                <Place></Place>
+                <p>{ride.startLocation.shortName}</p>
+              </span>
+
+              {/* map placeholder */}
+              <div
+                style={{
+                  width: '100%',
+                  height: '32rem',
+                  background: '#8888ff',
+                }}
+              ></div>
+            </div>
+            {/* end location + map */}
+            <div
+              style={{
+                width: 'flex-1',
+                height: 'min-content',
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '0.5rem',
+              }}
+            >
+              <span
+                style={{
+                  display: 'flex',
+                  gap: '0.5rem',
+                  alignItems: 'center',
+                  color: '#707070',
+                }}
+              >
+                <SubdirectoryArrowRight></SubdirectoryArrowRight>
+                <p>{ride.endLocation.shortName}</p>
+              </span>
+
+              {/* map placeholder */}
+              <div
+                style={{
+                  width: '100%',
+                  height: '32rem',
+                  background: '#8888ff',
+                }}
+              ></div>
+            </div>
+          </div>
+        )}
+      </div>
+      {/* expanded buttons */}
+      {expanded && (
+        <div
+          style={{
+            width: '100%',
+            height: 'min-content',
+            display: 'flex',
+            gap: '0.5rem',
+          }}
+        >
+          <button
+            style={{
+              width: '100%',
+              height: '1.5rem',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              cursor: 'pointer',
+              border: '#707070 1px solid',
+              borderRadius: '0.25rem',
+            }}
+          >
+            Close
+          </button>
+          <button
+            style={{
+              width: '100%',
+              height: '1.5rem',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              cursor: 'pointer',
+              border: '#303030 1px solid',
+              borderRadius: '0.25rem',
+              backgroundColor: '#000',
+              color: '#fff',
+            }}
+          >
+            Edit
+          </button>
+        </div>
+      )}
+    </button>
+  );
+};
 
 const Schedule: React.FC = () => {
   const { user, id } = useContext(AuthContext);
@@ -225,9 +481,29 @@ const Schedule: React.FC = () => {
             />
           </div>
         </div>
-        <div className={styles.tableSection}>
-          <RideTable rides={allRides} userRole="rider" />
+        <div
+          style={{
+            width: '100%',
+            height: 'min-content',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'start',
+            alignItems: 'center',
+            gap: '0.5rem',
+          }}
+        >
+          {allRides.map((ride, idx) => (
+            <RideCardComponent
+              ride={ride}
+              handleEdit={() => {}}
+              key={idx}
+            ></RideCardComponent>
+          ))}
         </div>
+
+        {/* <div className={styles.tableSection}>
+          <RideTable rides={allRides} userRole="rider" />
+        </div> */}
         <RequestRideDialog
           open={isDialogOpen}
           onClose={handleDialogClose}
