8d77aab4f4fd0e6030afb5fa782e57bb8459b4fd
Anika <asc344@cornell.edu>
2025-11-30 14:28:02 -0500

    User created <custom> locations do not display

--- frontend/src/pages/Rider/Schedule.tsx
+++ frontend/src/pages/Rider/Schedule.tsx
@@ -183,9 +183,6 @@ const Schedule: React.FC = () => {
 
   const hasUpcomingRide = nextUpcomingRide !== undefined;
 
-  const { refreshLocations } = useLocations();
-
-
   return (
     <APIProvider
       apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string}
@@ -249,7 +246,6 @@ const Schedule: React.FC = () => {
               images: l.images,
             }))
             .filter((l) => Number.isFinite(l.lat) && Number.isFinite(l.lng))}
-          refreshLocations={refreshLocations}
         />
       </main>
     </APIProvider>
