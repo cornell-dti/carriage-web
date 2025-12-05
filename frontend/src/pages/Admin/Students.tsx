b3ae722d50c53e681ec4b3ad21277bbd0aed79c0
yottayocta <chubbean.rq@gmail.com>
2025-12-05 01:19:47 -0500

    style: students page

--- frontend/src/pages/Admin/Students.tsx
+++ frontend/src/pages/Admin/Students.tsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import StudentsTable from '../../components/UserTables/StudentsTable';
 import RiderModal from '../../components/Modal/RiderModal';
 import CopyButton from '../../components/CopyButton/CopyButton';
 import SearchAndFilter from 'components/FormElements/SearchAndFilter';
 import styles from './page.module.css';
-import { Button } from '../../components/FormElements/FormElements';
 import { useRiders } from '../../context/RidersContext';
 import { Rider, Accessibility } from '../../types';
 import StatsBox from 'components/AnalyticsOverview/StatsBox';
 import { active, inactive } from '../../icons/other/index';
+import buttonStyles from '../../styles/button.module.css';
 
 const Riders = () => {
   const { riders } = useRiders();
@@ -47,55 +47,67 @@ const Riders = () => {
 
   return (
     <main id="main">
-      <div className={styles.pageTitle}>
-        <h1 className={styles.header}>Students</h1>
-        <div className={styles.rightSection}>
-          <CopyButton />
-          <Button
-            className={styles.addRiderButton}
-            onClick={() => setIsOpen(true)}
-          >
-            + Add Student
-          </Button>
-          <RiderModal isOpen={isOpen} setIsOpen={setIsOpen} />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '2rem',
+          padding: '2rem 4rem',
+        }}
+      >
+        <div className={styles.pageTitle}>
+          <h1 className={styles.header}>Students</h1>
+          <div className={styles.rightSection}>
+            <CopyButton />
+            <button
+              style={{
+                width: '10rem',
+              }}
+              className={`${buttonStyles.button} ${buttonStyles.buttonPrimary} ${buttonStyles.buttonLarge}`}
+              onClick={() => setIsOpen(true)}
+            >
+              + Add Student
+            </button>
+            <RiderModal isOpen={isOpen} setIsOpen={setIsOpen} />
+          </div>
         </div>
-      </div>
 
-      <div className={styles.statsAndSearch}>
-        <div className={styles.searchFilter}>
-          <SearchAndFilter
-            items={riders}
-            searchFields={['firstName', 'lastName']}
-            filterOptions={[
-              {
-                field: 'active',
-                label: 'Status',
-                options: [
-                  { value: 'true', label: 'Active' },
-                  { value: 'false', label: 'Inactive' },
-                ],
-              },
-              {
-                field: 'accessibility',
-                label: 'Disability',
-                options: Object.values(Accessibility).map((value) => ({
-                  value,
-                  label: value,
-                })),
-              },
-            ]}
-            onFilterApply={handleFilterApply}
-          />
+        <div className={styles.statsAndSearch}>
+          <div className={styles.searchFilter}>
+            <SearchAndFilter
+              items={riders}
+              searchFields={['firstName', 'lastName']}
+              filterOptions={[
+                {
+                  field: 'active',
+                  label: 'Status',
+                  options: [
+                    { value: 'true', label: 'Active' },
+                    { value: 'false', label: 'Inactive' },
+                  ],
+                },
+                {
+                  field: 'accessibility',
+                  label: 'Disability',
+                  options: Object.values(Accessibility).map((value) => ({
+                    value,
+                    label: value,
+                  })),
+                },
+              ]}
+              onFilterApply={handleFilterApply}
+            />
+          </div>
+          <div className={styles.statsBoxContainer}>
+            {studentStats.map((stat, idx) => (
+              <StatsBox key={idx} {...stat} />
+            ))}
+          </div>
         </div>
-        <div className={styles.statsBoxContainer}>
-          {studentStats.map((stat, idx) => (
-            <StatsBox key={idx} {...stat} />
-          ))}
-        </div>
-      </div>
 
-      <div className={styles.studentTable}>
-        <StudentsTable students={filteredStudents} />
+        <div className={styles.studentTable}>
+          <StudentsTable students={filteredStudents} />
+        </div>
       </div>
     </main>
   );
